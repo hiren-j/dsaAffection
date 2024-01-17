fafb88bee6faec9916a6ecc448ee8c1db70fc89f
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-17 13:48:07 +0530

    Check If Bitwise OR Has Trailing Zeros.cpp

--- Bit Manipulation/Check If Bitwise OR Has Trailing Zeros.cpp
+++ Bit Manipulation/Check If Bitwise OR Has Trailing Zeros.cpp
@@ -1,30 +1,14 @@
 // Program to check if it is possible to select two or more elements whose bitwise OR has trailing zeros or not ~ coded by Hiren
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
 using namespace std;
-
 #define loop(i,n,k) for(int i=(k?k:0); i<n; i++)
-#define itr(it,arr) for(auto it = begin(arr); it != end(arr); it++)
-#define sortThe(arr) sort(begin(arr), end(arr));
-#define ct cout
-#define cn cin
-#define pb push_back
-#define pp pop_back
-#define F first
-#define S second
-#define ll long long
-#define even(n) if(!(n % 2))
-#define odd(n) if(n % 2)
-typedef pair<int, int> p;
-typedef vector<int> vec;
-typedef vector<vector<int>> vec2d;
-typedef map<int, int> omap;
-typedef unordered_map<int, int> umap;
-typedef set<int> oset;
-typedef unordered_set<int> uset;
-typedef queue<int> q;
+#define even(n) (!(n % 2))
 
 // #1 Method to check if bitwise OR has trailing zeros or not, using brute force - O(N*N) & O(1)
-bool hasTrailingZeros(vector<int>& nums) {
+bool hasTrailingZeros_V1(vector<int>& nums) {
     int n = nums.size();
     
     loop(i, n, 0) 
@@ -38,7 +22,7 @@ bool hasTrailingZeros(vector<int>& nums) {
 }
 
 // #2 Method to check if bitwise OR has trailing zeros or not, using constant auxiliary space - O(N) & O(1)
-bool hasTrailingZeros_(vector<int>& nums) {
+bool hasTrailingZeros_V2(vector<int>& nums) {
     int n = nums.size();
 
     // Stores any "even element" with its index
@@ -47,7 +31,7 @@ bool hasTrailingZeros_(vector<int>& nums) {
     // Iterate the array
     loop(i, n, 0) {
         // If the ith element is "even" then mark the element with its index
-        if(!(nums[i] % 2)) {
+        if(even(nums[i])) {
             evenNum = nums[i], evenNumIdx = i; 
             break;
         }
@@ -56,7 +40,7 @@ bool hasTrailingZeros_(vector<int>& nums) {
     // Iterate the array and look for an another "even element"
     loop(i, n, 0) {
         // If the ith element is "even" and its at unique index then check for the trailing zeros by OR operation
-        if((i != evenNumIdx) && !(nums[i] % 2) && (evenNum | nums[i]) % 2 == 0) {
+        if((i != evenNumIdx) && (even(nums[i])) && (evenNum | nums[i]) % 2 == 0) {
             return true;
         }
     }
@@ -67,17 +51,38 @@ bool hasTrailingZeros_(vector<int>& nums) {
 
 // Driver code
 int main() {
-    vector<int> nums = {2,4,8,16};
+    cout<<"Enter the number of testcases you want: ";
+    int testCases; cin>>testCases;
 
-    // Print values
-    for(int num : nums)
-        cout<<num<<' ';
+    while(testCases--) {
+        // Handles console clearence for both "windows" and "linux" user
+        system("clear || cls");
+        
+        cout<<"Enter the size of the array: ";
+        int n; cin>>n;
 
-    // Verification call
-    if(hasTrailingZeros_(nums))
-        cout<<"\nIt is possible to select two or more elements whose bitwise OR has trailing zeros!";
-    else
-        cout<<"\nIt is not possible to select two or more elements whose bitwise OR has trailing zeros!";
+        vector<int> nums(n);
+
+        cout<<"\nEnter the array elements: ";
+
+        for(int i=0; i<n; i++) {
+            cin>>nums[i];
+        }
+
+        // Verification call
+        if(hasTrailingZeros_V2(nums))
+            cout<<"\nIt is possible to select two or more elements whose bitwise OR has trailing zeros!";
+        else
+            cout<<"\nIt is not possible to select two or more elements whose bitwise OR has trailing zeros!";
+        
+        if(testCases != 0)
+            cout<<"\n\nApplication will restart in 10 seconds!";
+        else
+            cout<<"\n\nApplication will close in 10 seconds!";
+            
+        // Add a 10-second delay before the next iteration
+        this_thread::sleep_for(chrono::seconds(10));
+    }
 
     return 0;
 }
