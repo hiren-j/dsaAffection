b53d5090bb9dca602e6cca907d04ba693886f2a7
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-22 22:06:46 +0530

    Add Two Numbers.cpp

--- Linked List/Add Two Numbers.cpp
+++ Linked List/Add Two Numbers.cpp
@@ -24,21 +24,21 @@ public:
 class Solution_V1 {
 public:
     // Method to add two non-negative lists, using dynamic node creation - O(N+M) & O(N+M) : Where N is the size of "list1" and M of "list2"
-    ListNode* addTwoNumbers(ListNode* headNode1, ListNode* headNode2) {
+    ListNode* addTwoNumbers(ListNode* headNodeA, ListNode* headNodeB) {
         ListNode* sumListHead    = nullptr; // Tracks the head node of the new list (sum list)
         ListNode* sumListCurrent = nullptr; // Tracks the nodes of the new list (sum list)
         int carry = 0;                      // Stores the "carry value" get by adding the value of two nodes
 
-        while(headNode1 || headNode2 || carry) {
+        while(headNodeA || headNodeB || carry) {
             // At the beginning, update the "sum value" by "carry value"
             int sum = carry;
 
             // If the node of "list1" exists then add its value to the sum
-            if(headNode1)
-                sum += headNode1->val, headNode1 = headNode1->next;
+            if(headNodeA)
+                sum += headNodeA->val, headNodeA = headNodeA->next;
             // If the node of "list2" exists then add its value to the sum
-            if(headNode2)
-                sum += headNode2->val, headNode2 = headNode2->next;
+            if(headNodeB)
+                sum += headNodeB->val, headNodeB = headNodeB->next;
             
             // Calculate and store the "remainder" and "carry" value
             int rem = sum % 10; carry = sum / 10;
@@ -64,35 +64,35 @@ public:
 class Solution_V2 {
 public:
     // Method to add two non-negative lists, using constant auxiliary space - O(N+M) & O(1) : Where N is the size of "list1" and M of "list2"
-    ListNode* addTwoNumbers(ListNode* headNode1, ListNode* headNode2) {
-        int length1 = getListLength(headNode1); // Calculate and store the length of "list1"
-        int length2 = getListLength(headNode2); // Calculate and store the length of "list2"
+    ListNode* addTwoNumbers(ListNode* headNodeA, ListNode* headNodeB) {
+        int lengthA = getListLength(headNodeA); // Calculate and store the length of "list1"
+        int lengthB = getListLength(headNodeB); // Calculate and store the length of "list2"
         int carry = 0;                          // Stores the "carry value" get by adding the value of two nodes
 
         ListNode* sumListHead    = nullptr; // Tracks the head node of the resultant list (sum list)
         ListNode* sumListCurrent = nullptr; // Tracks the nodes of the resultant list (sum list)
 
-        while(headNode1 || headNode2) {
-            ListNode* currNode1 = headNode1; // Tracks the current node of the "list1"
-            ListNode* currNode2 = headNode2; // Tracks the current node of the "list2"
+        while(headNodeA || headNodeB) {
+            ListNode* currNodeA = headNodeA; // Tracks the current node of the "list1"
+            ListNode* currNodeB = headNodeB; // Tracks the current node of the "list2"
             int sum = carry;                 // At the beginning, update the "sum value" by "carry value"
             
             // If the node of "list1" exists then add its value to the sum
-            if(headNode1)
-                sum += headNode1->val, headNode1 = headNode1->next;
+            if(headNodeA)
+                sum += headNodeA->val, headNodeA = headNodeA->next;
             // If the node of "list2" exists then add its value to the sum
-            if(headNode2)
-                sum += headNode2->val, headNode2 = headNode2->next;
+            if(headNodeB)
+                sum += headNodeB->val, headNodeB = headNodeB->next;
             
             // Calculate and store the "remainder" and "carry" value
             int rem = sum % 10; carry = sum / 10;
 
             // If the length of "list1" is greater then set the node of "list1" in the resultant list 
-            if(length1 > length2)
-                currNode1->val = rem, updateLinks(currNode1, sumListHead, sumListCurrent);
+            if(lengthA > lengthB)
+                currNodeA->val = rem, updateLinks(currNodeA, sumListHead, sumListCurrent);
             // Else when the length of "list2" is greater then set the node of "list2" in the resultant list 
             else
-                currNode2->val = rem, updateLinks(currNode2, sumListHead, sumListCurrent);
+                currNodeB->val = rem, updateLinks(currNodeB, sumListHead, sumListCurrent);
         }
 
         // If the "carry value" is remaining then create a new node of it and set it as the last node of the resultant list
@@ -137,23 +137,23 @@ void printList(ListNode* headNode) {
 // Driver code
 int main() {
     // Creating list1, connecting nodes and initializing their data
-    ListNode* headNode1 = new ListNode(3, new ListNode(4, new ListNode(2)));
+    ListNode* headNodeA = new ListNode(3, new ListNode(4, new ListNode(2)));
 
     // Creating list2, connecting nodes and initializing their data
-    ListNode* headNode2 = new ListNode(4, new ListNode(8, new ListNode(7)));
+    ListNode* headNodeB = new ListNode(4, new ListNode(8, new ListNode(7)));
 
     // Print call
-    printList(headNode1); printList(headNode2);
+    printList(headNodeA); printList(headNodeB);
 
     // Addition call
     Solution_V2 obj;
-    ListNode* sumListHead = obj.addTwoNumbers(headNode1, headNode2);
+    ListNode* sumListHead = obj.addTwoNumbers(headNodeA, headNodeB);
 
     // Print call
     printList(sumListHead);
 
     // Deletion call
-    delete headNode1; delete headNode2;
+    delete headNodeA; delete headNodeB;
 
     return 0;
 }
