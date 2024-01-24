db98033c204c7c7bce18bfd8dba59a36fa4c210c
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-25 01:38:27 +0530

    Maximum Product Difference Between Two Pairs.cpp

--- Array/Maximum Product Difference Between Two Pairs.cpp
+++ Array/Maximum Product Difference Between Two Pairs.cpp
@@ -6,51 +6,40 @@
 #include <thread>
 #include <chrono>
 
-// #1 Method to find the maximized product difference using sorting - O(NLogN) & O(1) : Where N is the size of the array "nums"
+// #1 Method to find the maximized product difference using sorting - O(NLogN) & O(1)
 int maxProductDifference_V1(std::vector<int>& nums) {
     int n = nums.size();
     std::sort(begin(nums), end(nums));
     return (nums[n-1] * nums[n-2]) - (nums[0] * nums[1]);
 }
 
-// #2 Method to find the maximized product difference using constant auxiliary space - O(N) & O(1) : Where N is the size of the array "nums"
+// #2 Method to find the maximized product difference using constant auxiliary space - O(N) & O(1)
 int maxProductDifference_V2(std::vector<int>& nums) {
     int n = nums.size();
 
-    int maxElement1 = INT_MIN; // Stores the first largest element
-    int maxElement2 = INT_MIN; // Stores the second largest element
-    int max1Idx = -1;          // Stores the index of the first largest element
+    int maxElement1 = INT_MIN, max1Idx = -1, maxElement2 = INT_MIN;
+    int minElement1 = INT_MAX, min1Idx = -1, minElement2 = INT_MAX;
 
-    int minElement1 = INT_MAX; // Stores the first smallest element
-    int minElement2 = INT_MAX; // Stores the second smallest element
-    int min1Idx = -1;          // Stores the index of the first smallest element
-
-    // Iterate and find the first largest and first smallest element
     for(int i=0; i<n; i++) {
         (nums[i] > maxElement1) ? maxElement1 = nums[i], max1Idx = i : 0;
         (nums[i] < minElement1) ? minElement1 = nums[i], min1Idx = i : 0;
     }
 
-    int diff1 = INT_MAX; // Tracks the smallest difference needed to find the second largest element
-    int diff2 = INT_MAX; // Tracks the smallest difference needed to find the second smallest element
+    int diff1 = INT_MAX, diff2 = INT_MAX;
 
-    // Iterate and find the second largest and second smallest element
     for(int i=0; i<n; i++) {
-        int currDiff1 = abs(nums[i] - maxElement1); // Stores the difference between ith and the first largest element
-        int currDiff2 = abs(nums[i] - minElement1); // Stores the difference between ith and the first smallest element
-        // If the current difference is lesser than the smallest difference seen till now
+        int currDiff1 = abs(nums[i] - maxElement1);
+        int currDiff2 = abs(nums[i] - minElement1);
         if(i != max1Idx && currDiff1 < diff1) {
-            maxElement2 = nums[i]; // Update the second largest element as the ith element
-            diff1 = currDiff1;     // Update the smallest difference needed to find the second largest element
+            maxElement2 = nums[i];
+            diff1 = currDiff1;
         }
-        // If the current difference is lesser than the smallest difference seen till now
         if(i != min1Idx && currDiff2 < diff2) {
-            minElement2 = nums[i]; // Update the second smallest element as the ith element
-            diff2 = currDiff2;     // Update the smallest difference needed to find the second smallest element
+            minElement2 = nums[i];
+            diff2 = currDiff2;
         }
     }
 
-    // Return the maximized product difference
     return (maxElement1 * maxElement2) - (minElement1 * minElement2);
 }
 
@@ -59,6 +48,11 @@ int main() {
     int testCases; 
     std::cout<<"Enter the number of testcases you want: ";
     std::cin>>testCases;
+
+    if(testCases <= 0) {
+        std::cout<<"Enter a valid number for the testcases, application expects a positive integer!";
+        return 0;
+    }
 
     while(testCases--) {
         // Handles console clearence for both "windows" and "linux" user
