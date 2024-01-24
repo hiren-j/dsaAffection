52e4f0c181e2038c86d5b8d2ef5792af23cf9ba0
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-24 19:03:37 +0530

    Average Of Levels In Binary Tree.cpp

--- Tree/Average Of Levels In Binary Tree.cpp
+++ Tree/Average Of Levels In Binary Tree.cpp
@@ -147,6 +147,11 @@ int main() {
     std::cout<<"Enter the number of testcases you want: ";
     std::cin>>testCases;
 
+    if(testCases <= 0) {
+        std::cout<<"Enter a valid number for the testcases, application expects a positive integer!";
+        return 0;
+    }
+
     while(testCases--) {
         system("clear || cls");
         int n;
