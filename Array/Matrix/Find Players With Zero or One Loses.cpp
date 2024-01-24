ca3f7ef2a617307a46ace8ce89cd146d6372f1af
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-25 01:40:58 +0530

    Find Players With Zero or One Loses.cpp

--- Array/Matrix/Find Players With Zero or One Loses.cpp
+++ Array/Matrix/Find Players With Zero or One Loses.cpp
@@ -80,6 +80,11 @@ int main() {
     std::cout<<"Enter the number of testcases you want: ";
     std::cin>>testCases;
 
+    if(testCases <= 0) {
+        std::cout<<"Enter an valid number for the testcases, application expects a positive integer!";
+        return 0;
+    }
+
     while(testCases--) {
         system("clear || cls"); 
         int n;
