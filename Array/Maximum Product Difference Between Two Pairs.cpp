fb54d8443f17ec27453ec56b2f45ba2daff10b0d
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-27 19:54:46 +0530

    Maximum Product Difference Between Two Pairs.cpp

--- Array/Maximum Product Difference Between Two Pairs.cpp
+++ Array/Maximum Product Difference Between Two Pairs.cpp
@@ -14,32 +14,43 @@ int maxProductDifference_V1(std::vector<int>& nums) {
 }
 
 // #2 Method to find the maximized product difference using constant auxiliary space - O(N) & O(1)
-int maxProductDifference_V2(std::vector<int>& nums) {
+int maxProductDifference_(std::vector<int>& nums) {
     int n = nums.size();
 
-    int maxElement1 = INT_MIN, max1Idx = -1, maxElement2 = INT_MIN;
-    int minElement1 = INT_MAX, min1Idx = -1, minElement2 = INT_MAX;
+    int maxElement1 = INT_MIN; // Stores the first largest element
+    int maxElement2 = INT_MIN; // Stores the second largest element
+    int max1Idx = -1;          // Stores the index of the first largest element
 
+    int minElement1 = INT_MAX; // Stores the first smallest element
+    int minElement2 = INT_MAX; // Stores the second smallest element
+    int min1Idx = -1;          // Stores the index of the first smallest element
+
+    // Iterate and find the first largest and first smallest element
     for(int i=0; i<n; i++) {
         (nums[i] > maxElement1) ? maxElement1 = nums[i], max1Idx = i : 0;
         (nums[i] < minElement1) ? minElement1 = nums[i], min1Idx = i : 0;
     }
 
-    int diff1 = INT_MAX, diff2 = INT_MAX;
+    int diff1 = INT_MAX; // Tracks the smallest difference needed to find the second largest element
+    int diff2 = INT_MAX; // Tracks the smallest difference needed to find the second smallest element
 
+    // Iterate and find the second largest and second smallest element
     for(int i=0; i<n; i++) {
-        int currDiff1 = abs(nums[i] - maxElement1);
-        int currDiff2 = abs(nums[i] - minElement1);
+        int currDiff1 = abs(nums[i] - maxElement1); // Stores the difference between ith and the first largest element
+        int currDiff2 = abs(nums[i] - minElement1); // Stores the difference between ith and the first smallest element
+        // If the current difference is lesser than the smallest difference seen till now
         if(i != max1Idx && currDiff1 < diff1) {
-            maxElement2 = nums[i];
-            diff1 = currDiff1;
+            maxElement2 = nums[i]; // Update the second largest element as the ith element
+            diff1 = currDiff1;     // Update the smallest difference needed to find the second largest element
         }
+        // If the current difference is lesser than the smallest difference seen till now
         if(i != min1Idx && currDiff2 < diff2) {
-            minElement2 = nums[i];
-            diff2 = currDiff2;
+            minElement2 = nums[i]; // Update the second smallest element as the ith element
+            diff2 = currDiff2;     // Update the smallest difference needed to find the second smallest element
         }
     }
 
+    // Return the maximized product difference
     return (maxElement1 * maxElement2) - (minElement1 * minElement2);
 }
 
