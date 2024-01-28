144091d46bdd81f63f2d522cab47f5b2dcc06c8e
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-28 13:19:01 +0530

    Pascal's Triangle.cpp

--- Dynamic Programming/Grid + Distinct Ways/Pascal's Triangle.cpp
+++ Dynamic Programming/Grid + Distinct Ways/Pascal's Triangle.cpp
@@ -1,17 +1,18 @@
-// Program to find the first "rowIndex" of "Pascal's triangle" ~ coded by Hiren
+// Program to find the first "numRows" of "Pascal's triangle" ~ coded by Hiren
 #include <iostream>
+#include <cstdlib>
 #include <vector>
 using namespace std;
 
 // Class to implement the "Bottom Up Approach":
-class BottomUpDP {
+class BottomUp {
 public:
-    // #1 Method to find the first "rowIndex" of Pascal's triangle, using tabulation - O(rowIndex^2) & O(rowIndex^2) 
-    vector<int> getRow_V1(int rowIndex) {
+    // Method to find the first "numRows" of Pascal's triangle, using tabulation - O(numRows^2) & O(numRows^2) 
+    vector<vector<int>> generateTable(int numRows) {
         // Stores the values of the "Pascal's Triangle".
         vector<vector<int>> pascalTable;
 
-        for(int row = 0; row < rowIndex; row++) {
+        for(int row = 0; row < numRows; row++) {
             // Create a new buffer to store the values of the current row of the "Pascal's Triangle".
             pascalTable.emplace_back(vector<int>(row+1, 1));
 
@@ -22,46 +23,27 @@ public:
 
         }   
 
-        // Return the "rowIndex" row of the "Pascal's Triangle". 
-        return pascalTable[rowIndex];
-    }
-
-    // #2 Method to find the first "rowIndex" of Pascal's triangle, using tabulation - O(rowIndex^2) & O(rowIndex) 
-    vector<int> getRow_V2(int rowIndex) {
-        vector<int> prevRow(rowIndex+1, 1); // Tracks the tabulation values for the previous row.
-        vector<int> currRow(rowIndex+1, 1); // Tracks the tabulation values for the current row.
-
-        // Fill the values for the table
-        for(int r = 0; r < rowIndex+1; ++r) {
-            // Iterate and fill the values for the columns of the current row.
-            for(int c = 1; c < r; ++c) {
-                currRow[c] = prevRow[c] + prevRow[c-1];
-            }
-            // Update the previous row for the upcoming row.
-            prevRow = currRow;
-        }
-
-        // Return the "rowIndex" row of the "Pascal's Triangle".
-        return currRow;
-    }
-
-    // #3 Method to find the first "rowIndex" of Pascal's triangle, using tabulation - O(rowIndex^2) & O(rowIndex) 
-    vector<int> getRow_V3(int rowIndex) {
-        // Tracks the tabulation values for the current row.
-        vector<int> currRow(rowIndex+1, 1);
-
-        // Fill the values for the table
-        for(int r = 2; r <= rowIndex; ++r) {
-            // Iterate and fill the values for the columns of the current row.
-            for(int c = r-1; c > 0; --c) {
-                currRow[c] += currRow[c - 1];
-            }
-        }
-
-        // Return the "rowIndex" row of the "Pascal's Triangle".
-        return currRow;
+        // Return the table of the "Pascal's Triangle". 
+        return pascalTable;
     }
 };
+// Note: The auxiliary space is considered because even if its not the user's demand then still we have to create the table of "pascalTable".
+
+// Method to print the table of the "Pascal's Triangle" - O(numRows^2) & O(1)
+void printTable(vector<vector<int>>& pascalTable, int numRows) {
+    cout<<'\n';
+
+    for(int row = 0; row < numRows; ++row) {
+        int rowSize = pascalTable[row].size();
+        cout<<"[";
+        for(int col = 0; col < rowSize; ++col) {
+            (col < rowSize-1) ? cout<<pascalTable[row][col]<<", " : cout<<pascalTable[row][col] ;
+        }
+        cout<<"]\n";
+    }
+
+    cout<<'\n';
+}
 
 // Driver code
 int main() {
@@ -73,35 +55,30 @@ int main() {
         system("cls || clear");
 
         // Input section for the number of rows.
-        int rowIndex;
-        cout<<"Enter the index of the row which you want from the pascal's triangle: ";
-        cin>>rowIndex;
+        int numRows;
+        cout<<"Enter the number of rows for the pascal's triangle: ";
+        cin>>numRows;
 
         // Check for the given size for the row is valid or not.
-        if(rowIndex < 0) {
-            cout<<"Enter a valid size for the row, application expects a non-negative integer!";
+        if(numRows <= 0) {
+            cout<<"Enter a valid size for the row, application expects a positive integer!";
             return 0;
         }
 
-        // Call to get the "rowIndex" row of the "Pascal's Triangle".
-        BottomUpDP bottomUp;
-        vector<int> row = bottomUp.getRow_V3(rowIndex);
+        // Call to get the "Pascal's Triangle".
+        BottomUp bottomUp;
+        vector<vector<int>> pascalTable = bottomUp.generateTable(numRows);
 
-        // Print values.
-        cout<<"\nThe values of the "<<rowIndex<<"th row is: [";
-        int size = row.size();
-        for(int i=0; i<size; i++) {
-            (i < size-1) ? cout<<row[i]<<", " : cout<<row[i] ;
-        }
-        cout<<"]";
+        // Print table.
+        printTable(pascalTable, numRows);
 
         // Input section to handle the flow of iterations of the application. 
         char userChoise;
-        cout<<"\n\nPress \'R\' to restart the application, else application will exit automatically: ";
+        cout<<"Press \'R\' to restart the application, else application will exit automatically: ";
         cin>>userChoise;
         userWantsOperation = (userChoise == 'R') ? true : false ;
     }
 
     return 0;
 }
-// Link: https://leetcode.com/problems/pascals-triangle-ii/
+// Link: https://leetcode.com/problems/pascals-triangle/description/
