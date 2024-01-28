3afb961c66dab70803e9e789ee0fc26636aa1fab
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-01-28 11:33:23 +0530

    Check If Bitwise OR Has Trailing Zeros.cpp

--- Bit Manipulation/Check If Bitwise OR Has Trailing Zeros.cpp
+++ Bit Manipulation/Check If Bitwise OR Has Trailing Zeros.cpp
@@ -1,88 +1,95 @@
 // Program to check if it is possible to select two or more elements whose bitwise OR has trailing zeros or not ~ coded by Hiren
 #include <bits/stdc++.h>
-using namespace std;
 #define loop(i,n,k) for(int i=(k?k:0); i<n; i++)
 #define even(n) (!(n % 2))
+using namespace std;
 
-// #1 Method to check if bitwise OR has trailing zeros or not, using brute force - O(N*N) & O(1)
-bool hasTrailingZeros_V1(vector<int>& nums) {
-    int n = nums.size();
-    
-    loop(i, n, 0) 
-        loop(j, n, i+1) 
-            // Its observed that the "even elements" always contain trailing zero, hence check for the trailing zeros by OR operation
-            if((nums[i] | nums[j]) % 2 == 0)
+// Solution class:
+class Solution {
+public:
+    // #1 Method to check if bitwise OR has trailing zeros or not, using brute force - O(N*N) & O(1)
+    bool hasTrailingZeros_V1(vector<int>& nums) {
+        int n = nums.size();
+
+        loop(i, n, 0) 
+            loop(j, n, i+1) 
+                // Its observed that the "even elements" always contain trailing zero, hence check for the trailing zeros by OR operation
+                if((nums[i] | nums[j]) % 2 == 0)
+                    return true;
+
+        // Return false, if reached here 
+        return false;
+    }
+
+    // #2 Method to check if bitwise OR has trailing zeros or not, using constant auxiliary space - O(N) & O(1)
+    bool hasTrailingZeros_V2(vector<int>& nums) {
+        int n = nums.size();
+
+        // Stores any "even element" with its index
+        int evenNum = -1, evenNumIdx = -1;
+
+        // Iterate the array
+        loop(i, n, 0) {
+            // If the ith element is "even" then mark the element with its index
+            if(even(nums[i])) {
+                evenNum = nums[i], evenNumIdx = i; 
+                break;
+            }
+        }
+
+        // Iterate the array and look for an another "even element"
+        loop(i, n, 0) {
+            // If the ith element is "even" and its at unique index then check for the trailing zeros by OR operation
+            if((i != evenNumIdx) && (even(nums[i])) && (evenNum | nums[i]) % 2 == 0) {
                 return true;
-    
-    // Return false, if reached here 
-    return false;
-}
-
-// #2 Method to check if bitwise OR has trailing zeros or not, using constant auxiliary space - O(N) & O(1)
-bool hasTrailingZeros_V2(vector<int>& nums) {
-    int n = nums.size();
-
-    // Stores any "even element" with its index
-    int evenNum = -1, evenNumIdx = -1;
-    
-    // Iterate the array
-    loop(i, n, 0) {
-        // If the ith element is "even" then mark the element with its index
-        if(even(nums[i])) {
-            evenNum = nums[i], evenNumIdx = i; 
-            break;
+            }
         }
-    }
-    
-    // Iterate the array and look for an another "even element"
-    loop(i, n, 0) {
-        // If the ith element is "even" and its at unique index then check for the trailing zeros by OR operation
-        if((i != evenNumIdx) && (even(nums[i])) && (evenNum | nums[i]) % 2 == 0) {
-            return true;
-        }
-    }
 
-    // Return false, if reached here 
-    return false;
-}
+        // Return false, if reached here 
+        return false;
+    }
+};
 
 // Driver code
 int main() {
-    cout<<"Enter the number of testcases you want: ";
-    int testCases; cin>>testCases;
+    // Tracks for the user wants to perform the operation or not
+    bool canPerformOperation = true;
 
-    if(testCases <= 0) {
-        std::cout<<"Enter a valid number for the testcases, application expects a positive integer!";
-        return 0;
-    }
-
-    while(testCases--) {
-        system("clear || cls");
+    while(canPerformOperation) {
+        // Handles console clearance for both "windows" and "linux" user
+        system("cls || clear");
         
+        // Input section for the size of the array
         cout<<"Enter the size of the array: ";
-        int n; cin>>n;
+        int n; cin>>n;  
+
+        // Check for the given size is valid or not
+        if(n <= 0) {
+            cout<<"Enter a valid size, application expects a positive integer!";
+            return 0;
+        }
 
         vector<int> nums(n);
 
-        cout<<"\nEnter the array elements: ";
+        cout<<"Enter the array elements: ";
 
+        // Input section for the array elements
         for(int i=0; i<n; i++) {
             cin>>nums[i];
         }
 
         // Verification call
-        if(hasTrailingZeros_V2(nums))
-            cout<<"\nIt is possible to select two or more elements whose bitwise OR has trailing zeros!";
+        Solution solution;
+        if(solution.hasTrailingZeros_V2(nums))
+            cout<<"Its possible to select two or more elements whose bitwise OR has trailing zeros!";
         else
-            cout<<"\nIt is not possible to select two or more elements whose bitwise OR has trailing zeros!";
-        
-        if(testCases != 0)
-            cout<<"\n\nApplication will restart in 10 seconds!";
-        else
-            cout<<"\n\nApplication will close in 10 seconds!";
-            
-        // Add a 10-second delay before the next iteration
-        this_thread::sleep_for(chrono::seconds(10));
+            cout<<"Its not possible to select two or more elements whose bitwise OR has trailing zeros!";
+
+        // Input section to handle the flow of iterations of the application
+        char userChoise;
+        cout<<"\n\nPress \'R\' to restart the application, else application will exit automatically: ";
+        cin>>userChoise;
+        canPerformOperation = (userChoise == 'R' ? true : false);        
     }
 
     return 0;
