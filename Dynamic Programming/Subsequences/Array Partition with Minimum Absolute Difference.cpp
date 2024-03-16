28cbe82e528f79202cda2c6cdbc16ee3c07d307c
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-03-17 01:00:27 +0530

    Array Partition with Minimum Absolute Difference.cpp

--- Dynamic Programming/Subsequences/Array Partition with Minimum Absolute Difference.cpp
+++ Dynamic Programming/Subsequences/Array Partition with Minimum Absolute Difference.cpp
@@ -10,32 +10,32 @@ using namespace std;
 class TopDown {
 public:
     // Method to partition the array into two sets and to find the minimum absolute difference between their sums, using recursion with memoization - O(N*S) & O(N*S) : Where N let be the array size and S let be the total sum
-	int minSubsetSumDifference(vector<int>& nums, int N) {
-	    int totalSum = accumulate(begin(nums), end(nums), 0);
+    int minSubsetSumDifference(vector<int>& nums, int N) {
+	int totalSum = accumulate(begin(nums), end(nums), 0);
 	    
         // Tabulation buffer: dp[index][amount] represents, is there exists any subset within the "index" elements of the start of the array, such that their sum equals to the "amount / currSum"
-	    vector<vector<int>> dp(N, vector<int>(totalSum + 1, -1));
+	vector<vector<int>> dp(N, vector<int>(totalSum + 1, -1));
 	    
-		// If a single subset exists, then the another subset can easily be identified, hence look for the existence of all the possible subsets through their sum value
-	    for(int currSum = 0; currSum <= totalSum; ++currSum) {
-	        solveWithMemo(dp, nums, N, N - 1, currSum);
-	    }
-	    
-		// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
-	    int minAbsDiff = INT_MAX;
-	    
-		// Iterate and check the existence of the subsets in the array 
-	    for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
-			// If a subset exists, then it's a valid partition, hence update the result value by the minimum value
-	        if(dp[N - 1][subset1Sum] == 1) {
-	            int subset2Sum = totalSum - subset1Sum;
-	            minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
-	        }
-	    }
-	    
-		// Return the result value
-	    return minAbsDiff;
+	// If a single subset exists, then the another subset can easily be identified, hence look for the existence of all the possible subsets through their sum value
+	for(int currSum = 0; currSum <= totalSum; ++currSum) {
+	    solveWithMemo(dp, nums, N, N - 1, currSum);
 	}
+	    
+	// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
+	int minAbsDiff = INT_MAX;
+	    
+	// Iterate and check the existence of the subsets in the array 
+	for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
+	    // If a subset exists, then it's a valid partition, hence update the result value by the minimum value
+	    if(dp[N - 1][subset1Sum] == 1) {
+	        int subset2Sum = totalSum - subset1Sum;
+	        minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
+	    }
+	}
+	    
+	// Return the result value
+	return minAbsDiff;
+    }
 	
 private:
     bool solveWithMemo(vector<vector<int>>& dp, vector<int>& nums, int N, int index, int amount) {
@@ -43,23 +43,23 @@ private:
         if(amount == 0)
             return dp[index][amount] = true;        
     
-	    // Edge case: If all the elements are exhausted and if the amount becomes zero, then there exists a subset with given amount
+	// Edge case: If all the elements are exhausted and if the amount becomes zero, then there exists a subset with given amount
         if(index == 0)
             return dp[index][amount] = (nums[index] == amount);
             
-		// Memoization table: If the current state is already computed, then return the computed value
+	// Memoization table: If the current state is already computed, then return the computed value
         if(dp[index][amount] != -1)
             return dp[index][amount];
             
-		// There are always two possibilties to perform at each index
+	// There are always two possibilties to perform at each index
         bool currSkip = solveWithMemo(dp, nums, N, index - 1, amount); // Is to skip the index value and move on
-        bool currTake = false;										   // Is to take the index value and move on	
+        bool currTake = false;				               // Is to take the index value and move on	
         
-		// If the subset equal to given amount doesn't exist, then if possible, then take the index value
+	// If the subset equal to given amount doesn't exist, then if possible, then take the index value
         if(!currSkip && nums[index] <= amount) 
             currTake = solveWithMemo(dp, nums, N, index - 1, amount - nums[index]);
             
-		// Store the presence information of the subset to the memoization table and then return it
+	// Store the presence information of the subset to the memoization table and then return it
         return dp[index][amount] = (currTake || currSkip);
     }
 };
@@ -68,14 +68,14 @@ private:
 class BottomUp_V1 {
 public:
     // Method to partition the array into two sets and to find the minimum absolute difference between their sums, using 2D buffer for tabulation - O(N*S) & O(N*S) : Where N let be the array size and S let be the total amount
-	int minSubsetSumDifference(vector<int>& nums, int N) {
-	    int totalSum = accumulate(begin(nums), end(nums), 0);
+    int minSubsetSumDifference(vector<int>& nums, int N) {
+	int totalSum = accumulate(begin(nums), end(nums), 0);
 	    
         // Tabulation buffer: dp[index][currSum] represents, is there exists any subset within the "index" elements from the start of the array such that their sum equals to the "currSum"
-	    vector<vector<bool>> dp(N + 1, vector<bool>(totalSum + 1, false));
+	vector<vector<bool>> dp(N + 1, vector<bool>(totalSum + 1, false));
 		
         // Edge case: If the array is empty and the sum is zero, then there always exists a subset with sum 0
-	    dp[0][0] = true;
+	dp[0][0] = true;
 	    
         // Treat each index as the "number of elements can take from the start of the array" and check the existence of the subset whose sum equals to the "currSum"
         for(int index = 1; index <= N; ++index) {
@@ -89,37 +89,37 @@ public:
             }
         }
 	    
-		// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
-	    int minAbsDiff = INT_MAX;
+	// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
+	int minAbsDiff = INT_MAX;
 	    
-		// Iterate and check the existence of the subsets in the array 
-	    for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
-			// If a subset exists, then it's a valid partition, hence update the result value by the minimum value
-	        if(dp[N][subset1Sum] == 1) {
-	            int subset2Sum = totalSum - subset1Sum;
-	            minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
-	        }
+	// Iterate and check the existence of the subsets in the array 
+	for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
+	    // If a subset exists, then it's a valid partition, hence update the result value by the minimum value
+	    if(dp[N][subset1Sum] == 1) {
+	        int subset2Sum = totalSum - subset1Sum;
+	        minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
 	    }
-	    
-		// Return the result value
-	    return minAbsDiff;
 	}
+	    
+	// Return the result value
+	return minAbsDiff;
+    }
 };
 
 // #2 Class to implement the Bottom-up approach:
 class BottomUp_V2 {
 public:
     // Method to partition the array into two sets and to find the minimum absolute difference between their sums, using 1D buffer for tabulation - O(N*S) & O(S) : Where N let be the array size and S let be the total amount
-	int minSubsetSumDifference(vector<int>& nums, int N) {
-	    int totalSum = accumulate(begin(nums), end(nums), 0);
+    int minSubsetSumDifference(vector<int>& nums, int N) {
+	int totalSum = accumulate(begin(nums), end(nums), 0);
 	    
         // Tabulation buffer: "prevRow[currSum] / currRow[currSum]" represents, is there exists any subset within the "index" elements from the start of the array such that their sum equals to the "currSum"
-	    vector<bool> prevRow(totalSum + 1, false), currRow(totalSum + 1, false);
+	vector<bool> prevRow(totalSum + 1, false), currRow(totalSum + 1, false);
 
-		// Edge case: If the array is empty and the sum is zero, then there always exists a subset with sum 0
-	    prevRow[0] = true;
+	// Edge case: If the array is empty and the sum is zero, then there always exists a subset with sum 0
+	prevRow[0] = true;
 	    
-		// Treat each index as the "number of elements can take from the start of the array" and check the existence of the subset whose sum equals to the "currSum"
+	// Treat each index as the "number of elements can take from the start of the array" and check the existence of the subset whose sum equals to the "currSum"
         for(int index = 1; index <= N; ++index) {
             for(int currSum = 0; currSum <= totalSum; ++currSum) {
                 bool currSkip = prevRow[currSum];
@@ -132,21 +132,21 @@ public:
             prevRow = currRow;
         }
 	    	    
-		// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
-	    int minAbsDiff = INT_MAX;
+	// Stores the "minimum absolute difference of two subsets sum after array partition / result value"
+	int minAbsDiff = INT_MAX;
 	    
-		// Iterate and check the existence of the subsets in the array 
-	    for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
-			// If a subset exists, then it's a valid partition, hence update the result value by the minimum value
-	        if(prevRow[subset1Sum] == 1) {
-	            int subset2Sum = totalSum - subset1Sum;
-	            minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
-	        }
+	// Iterate and check the existence of the subsets in the array 
+	for(int subset1Sum = 0; subset1Sum <= totalSum/2; ++subset1Sum) {
+	    // If a subset exists, then it's a valid partition, hence update the result value by the minimum value
+	    if(prevRow[subset1Sum] == 1) {
+	        int subset2Sum = totalSum - subset1Sum;
+	        minAbsDiff = min(minAbsDiff, abs(subset1Sum - subset2Sum));
 	    }
-	    
-		// Return the result value
-	    return minAbsDiff;
 	}
+	    
+	// Return the result value
+	return minAbsDiff;
+    }
 };
 
 // Driver code
