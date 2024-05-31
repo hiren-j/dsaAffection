013196fd9d6c679cabe1a8aaaa275e229f5238f0
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-05-31 15:52:48 +0530

    Update String Subsequence.cpp

--- Dynamic Programming/Subsequences/String Subsequence.cpp
+++ Dynamic Programming/Subsequences/String Subsequence.cpp
@@ -1,5 +1,4 @@
 // Program to count the total number of subsequences of string "s1" equal to string "s2" ~ coded by Hiren
-#define mod 1000000007
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,7 +7,7 @@ using namespace std;
 class TopDown_A {
     int n, m;
 
-    // O(2^N) & O(N) 
+    // O(2^N) & O(max(N,M)) 
     int solveWithoutMemo(string& s1, string& s2, int i, int j) {
         // Edge case: If all the letters of "s2" are exhausted then you've one valid way 
         if(j == m)
@@ -19,15 +18,15 @@ class TopDown_A {
             return 0;
             
         // There are always two possibilites to perform at each index
-        int currSkip = solveWithoutMemo(s1, s2, i + 1, j) % mod; // Is to skip the ith letter of "s1"
-        int currTake = 0;                                        // Is to take the ith letter of "s1"
+        int currSkip = solveWithoutMemo(s1, s2, i + 1, j); // Is to skip the ith letter of "s1"
+        int currTake = 0;                                  // Is to take the ith letter of "s1"
         
         // If the ith and jth letter of both the strings are equal then take the ith letter of "s1"
         if(s1[i] == s2[j])
-            currTake = solveWithoutMemo(s1, s2, i + 1, j + 1) % mod;
+            currTake = solveWithoutMemo(s1, s2, i + 1, j + 1);
             
         // Return ther result value
-        return (currSkip + currTake) % mod;
+        return (currSkip + currTake);
     }
     
     // O(N*M) & O(N*M)
@@ -45,20 +44,20 @@ class TopDown_A {
             return memory[i][j];
         
         // There are always two possibilites to perform at each index
-        int currSkip = solveWithMemo(memory, s1, s2, i + 1, j) % mod; // Is to skip the ith letter of "s1"
-        int currTake = 0;                                             // Is to take the ith letter of "s1"      
+        int currSkip = solveWithMemo(memory, s1, s2, i + 1, j); // Is to skip the ith letter of "s1"
+        int currTake = 0;                                       // Is to take the ith letter of "s1"      
         
         // If the ith and jth letter of both the strings are equal then take the ith letter of "s1"
         if(s1[i] == s2[j])
-            currTake = solveWithMemo(memory, s1, s2, i + 1, j + 1) % mod;
+            currTake = solveWithMemo(memory, s1, s2, i + 1, j + 1);
         
         // Store the result value to the memoization table and then return it
-        return memory[i][j] = (currSkip + currTake) % mod;
+        return memory[i][j] = (currSkip + currTake);
     }
     
 public:
     // Method to count the total number of subsequences of string "s1" equal to string "s2", using recursion with memoization - O(N*M) & O(N*M)
-    int countWays(string& s1, string& s2) {
+    int numDistinct(string& s1, string& s2) {
         n = s1.size(), m = s2.size();
         vector<vector<int>> memory(n, vector<int>(m, -1));
         return solveWithMemo(memory, s1, s2, 0, 0);
@@ -69,7 +68,7 @@ public:
 class TopDown_B {
     int n, m;
 
-    // O(2^N) & O(N)
+    // O(2^N) & O(max(N,M))
     int solveWithoutMemo(string& s1, string& s2, int startIndex, int j) {
         // Edge case: If all the letters of "s2" are exhausted then you've one valid way 
         if(j == m)
@@ -85,12 +84,12 @@ class TopDown_B {
         // Iterate "s1" and if the ith and jth letter of both the strings are equal then take the ith letter of "s1"
         for(int i = startIndex; i < n; ++i) {
             if(s1[i] == s2[j]) {
-                resCount = ((resCount % mod) + solveWithoutMemo(s1, s2, i + 1, j + 1)) % mod;
+                resCount += solveWithoutMemo(s1, s2, i + 1, j + 1);
             }
         }
         
         // Return the result value
-        return (resCount) % mod;
+        return resCount;
     }
 
     // O(N*M) & O(N*M)
@@ -113,17 +112,17 @@ class TopDown_B {
         // Iterate "s1" and if the ith and jth letter of both the strings are equal then take the ith letter of "s1"
         for(int i = startIndex; i < n; ++i) {
             if(s1[i] == s2[j]) {
-                resCount = ((resCount % mod) + solveWithMemo(memory, s1, s2, i + 1, j + 1)) % mod;
+                resCount += solveWithMemo(memory, s1, s2, i + 1, j + 1);
             }
         }
         
         // Store the result value to the memoization table and then return it
-        return memory[startIndex][j] = (resCount) % mod;
+        return memory[startIndex][j] = resCount;
     }
     
 public:
     // Method to count the total number of subsequences of string "s1" equal to string "s2", using recursion with memoization - O(N*M) & O(N*M)
-    int countWays(string& s1, string& s2) {
+    int numDistinct(string& s1, string& s2) {
         n = s1.size(), m = s2.size();
         vector<vector<int>> memory(n, vector<int>(m, -1));
         return solveWithMemo(memory, s1, s2, 0, 0);
@@ -133,8 +132,8 @@ public:
 // Class to implement the Bottom-up approach:
 class BottomUp {
 public:
-    // Method to count the total number of subsequences of string "s1" equal to string "s2", using 2D tabulation - O(N*M) & O(N*M)
-    int countWays_A(string& s1, string& s2) {
+    // #1 Method to count the total number of subsequences of string "s1" equal to string "s2", using 2D tabulation - O(N*M) & O(N*M)
+    int numDistinct_A(string& s1, string& s2) {
         int n = s1.size(), m = s2.size();
         
         // 2D table for tabulation: dp[i][j] stores the count of total number of subsequences of "s1" equal to "s2" such that by considering the first i letters of "s1" and j letters of "s2"
@@ -148,12 +147,12 @@ public:
         // Fill the rest of the table
         for(int i = 1; i <= n; ++i) {
             for(int j = 1; j <= m; ++j) {
-                int currSkip = dp[i - 1][j] % mod;
-                int currTake = 0; 
+                unsigned int currSkip = dp[i - 1][j];
+                unsigned int currTake = 0; 
                 if(s1[i - 1] == s2[j - 1]) {
-                    currTake = dp[i - 1][j - 1] % mod;
+                    currTake = dp[i - 1][j - 1];
                 }
-                dp[i][j] = (currSkip + currTake) % mod;
+                dp[i][j] = (currSkip + currTake);
             }
         }
         
@@ -161,8 +160,8 @@ public:
         return dp[n][m];
     }
 
-    // O(N*M) & O(M)
-    int countWays_B(string& s1, string& s2) {
+    // #2 Method to count the total number of subsequences of string "s1" equal to string "s2", using 1D tabulation - O(N*M) & O(M)
+    int numDistinct_B(string& s1, string& s2) {
         int n = s1.size(), m = s2.size();
         
         // 1D table for tabulation: "prevRow[j] or currRow[j]" stores the count of total number of subsequences of "s1" equal to "s2" such that by considering the first i letters of "s1" and j letters of "s2"
@@ -173,12 +172,12 @@ public:
             // If "s2" is empty then for any size of "s1" you've one valid way
             prevRow[0] = 1;
             for(int j = 1; j <= m; ++j) {
-                int currSkip = prevRow[j] % mod;
-                int currTake = 0; 
+                unsigned int currSkip = prevRow[j];
+                unsigned int currTake = 0; 
                 if(s1[i - 1] == s2[j - 1]) {
-                    currTake = prevRow[j - 1] % mod;
+                    currTake = prevRow[j - 1];
                 }
-                currRow[j] = (currSkip + currTake) % mod;
+                currRow[j] = (currSkip + currTake);
             }
             // Update the previous row of the upcoming row
             prevRow = currRow;
@@ -207,7 +206,7 @@ int main() {
         }
         else {
             BottomUp bottomUp;
-            cout<<"The total number of subsequences is: "<<bottomUp.countWays_B(s1, s2)<<'\n';    
+            cout<<"The total number of subsequences is: "<<bottomUp.numDistinct_B(s1, s2)<<'\n';    
         }
     }
 
@@ -215,5 +214,7 @@ int main() {
 }
 /*
     Topics: String | Dynamic Programming
-    Link: https://www.geeksforgeeks.org/problems/find-number-of-times-a-string-occurs-as-a-subsequence3020/1
+    Links: https://leetcode.com/discuss/interview-question/433901/Google-or-Phone-or-Distinct-subsequences
+           https://leetcode.com/problems/distinct-subsequences/description/
+           https://www.geeksforgeeks.org/problems/find-number-of-times-a-string-occurs-as-a-subsequence3020/1
 */
