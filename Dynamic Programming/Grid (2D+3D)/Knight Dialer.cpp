66a289f5cb130a4ef4c7362cfa001551e27f3b13
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-06-21 14:54:35 +0530

    Update Knight Dialer.cpp

--- Dynamic Programming/Grid (2D+3D)/Knight Dialer.cpp
+++ Dynamic Programming/Grid (2D+3D)/Knight Dialer.cpp
@@ -14,11 +14,11 @@ class TopDown {
 
     // O(8^10) & O(N)
     int solveWithoutMemo(int N, int R, int C) {
-        // Edge case: No number exists outside of the dialpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
+        // Base case: No number exists outside of the dialpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
         if(R < 0 || C < 0 || R >= 4 || C >= 3 || dialpad[R][C] == -1)
             return 0;
 
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
+        // Base case: If you've correctly dialed a phone number of length N then you've one valid way
         if(N == 0)
             return 1;
 
@@ -36,13 +36,13 @@ class TopDown {
         return count % MOD;
     }
 
-    // O(8*N*4*3) & O(N+N*4*3)
+    // O(10*8*N*4*3) & O(N*4*3 + N)
     int solveWithMemo(vector<vector<vector<int>>>& memory, int N, int R, int C) {
-        // Edge case: No number exists outside of the dialpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
+        // Base case: No number exists outside of the dialpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
         if(R < 0 || C < 0 || R >= 4 || C >= 3 || dialpad[R][C] == -1)
             return 0;
 
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
+        // Base case: If you've correctly dialed a phone number of length N then you've one valid way
         if(N == 0)
             return 1;
 
@@ -73,13 +73,61 @@ public:
         vector<vector<vector<int>>> memory(N, vector<vector<int>>(4, vector<int>(3, -1)));
 
         // Start dialing from the cells which doesn't starts from '*' or '#'
-        for(int R = 0; R < 4; ++R) {
-            for(int C = 0; C < 3; ++C) {
-                if(dialpad[R][C] != -1) {
+        for(int R = 0; R < 4; ++R) 
+            for(int C = 0; C < 3; ++C) 
+                if(dialpad[R][C] != -1) 
                     resCount = ((resCount) % MOD + solveWithMemo(memory, N - 1, R, C)) % MOD;
+
+        // Return the count of distinct phone numbers of length N you can dial
+        return resCount % MOD;
+    }
+};
+
+// Class to implement the Bottom-up approach:
+class BottomUp {
+    vector<vector<int>> directions = {{-2, -1}, {-1, -2}, {-2, 1}, {-1, 2}, {2, -1}, {1, -2}, {2, 1}, {1, 2}};
+    vector<vector<int>> dialpad    = {{1, 2, 3},
+                                      {4, 5, 6},
+                                      {7, 8, 9},
+                                      {-1, 0, -1}};
+public:
+    // Method to find how many distinct phone numbers of length N you can dial, using 3D tabulation - O(N*4*3*4) & O(N*4*3)
+    int knightDialer(int N) {
+        // 3D Tabulation table
+        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(4, vector<int>(3, 0)));
+
+        // Base case: If the length is 0 then there exists one valid way
+        for(int R = 0; R < 4; ++R) 
+            for(int C = 0; C < 3; ++C)
+                if(dialpad[R][C] != -1)
+                    dp[0][R][C] = 1;
+
+        // Fill the table for lengths from 1 to N
+        for(int step = 1; step <= N; ++step) {
+            for(int R = 3; R >= 0; --R) {
+                for(int C = 2; C >= 0; --C) {
+                    if(dialpad[R][C] != -1) {
+                        int count = 0;
+                        for(auto& dir : directions) {
+                            int reachRow = R + dir[0];
+                            int reachCol = C + dir[1];
+                            if(reachRow >= 0 && reachCol >= 0 && reachRow < 4 && reachCol < 3 && dialpad[reachRow][reachCol] != -1) {
+                                count = ((count % MOD) + dp[step - 1][reachRow][reachCol]) % MOD;
+                            }
+                        }
+                        dp[step][R][C] = count % MOD;
+                    }
                 }
             }
         }
+        
+        int resCount = 0;
+
+        // Sum up all the numbers of length N
+        for(int R = 0; R < 4; ++R) 
+            for(int C = 0; C < 3; ++C) 
+                if(dialpad[R][C] != -1) 
+                    resCount = ((resCount) % MOD + dp[N - 1][R][C]) % MOD;
 
         // Return the count of distinct phone numbers of length N you can dial
         return resCount % MOD;
@@ -92,7 +140,7 @@ int main() {
 
     // Input section to control the flow of iterations of the application
     int testCases;
-    cout<<"Enter the total number of cases to test: ";
+    cout<<"Enter total number of cases to test: ";
     cin>>testCases;
 
     while(testCases--) {
@@ -106,14 +154,14 @@ int main() {
             cout<<"You must enter a value which lies between 1 and 5000!\n";
         }
         else {
-            TopDown topDown;
-            cout<<"The total distinct phone numbers of length "<<N<<" you can dial is: "<<topDown.knightDialer(N)<<'\n';
+            BottomUp bottomUp;
+            cout<<"The total distinct phone numbers of length "<<N<<" you can dial is: "<<bottomUp.knightDialer(N)<<'\n';
         }
     }
 
     return 0;
 }
 /*
-    Topics: Dynamic Programming 
+    Topics: Dynamic Programming
     Link: https://leetcode.com/problems/knight-dialer/description/
 */
