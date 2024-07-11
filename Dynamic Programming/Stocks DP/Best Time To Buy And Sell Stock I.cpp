819a1e48aaa70c8ea52c144b6cf1e98f3e0d9ec4
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-07-12 01:53:43 +0530

    Create Best Time To Buy And Sell Stock I.cpp

---
+++ Dynamic Programming/Stocks DP/Best Time To Buy And Sell Stock I.cpp
@@ -0,0 +1,23 @@
+// Code to find the maximum profit you can achieve from the specified transaction ~ coded by Hiren
+
+-------------------------------------------------------------------------------------------
+
+class BottomUp {
+public:
+    // Method to find the maximum profit you can achieve - O(N) & O(1)
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, minPrice = INT_MAX;
+
+        for(int price : prices) {
+            profit   = max(profit, price - minPrice);
+            minPrice = min(minPrice, price);
+        }
+
+        return profit;
+    }
+};
+
+-------------------------------------------------------------------------------------------
+
+Topics: Array | Dynamic Programming
+Link  : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
