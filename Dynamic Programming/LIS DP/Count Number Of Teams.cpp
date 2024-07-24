47272757e08b03eab8baf852eb4f064eb5b59688
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-07-24 22:36:36 +0530

    Update Count Number Of Teams.cpp

--- Dynamic Programming/LIS DP/Count Number Of Teams.cpp
+++ Dynamic Programming/LIS DP/Count Number Of Teams.cpp
@@ -18,7 +18,7 @@ private:
     int n;
 
     // O(2*N*N*4) & O(N*N*4 + N)
-    int solveWithMemo(vector<vector<vector<int>>>& memory, vector<int>& rating, int k, int index, int   prevIndex, bool toFindIncreasing) {
+    int solveWithMemo(vector<vector<vector<int>>>& memory, vector<int>& rating, int k, int index, int prevIndex, bool toFindIncreasing) {
         // Edge case: If you've found a strictly increasing subsequence of length 3 then return 1 
         if(k == 0)
             return 1;
