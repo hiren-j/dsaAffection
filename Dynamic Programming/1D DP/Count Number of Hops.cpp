8b618f4896c7b5178911cd377fb61fc01cd13f68
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-07-30 22:01:58 +0530

    Update Count Number of Hops.cpp

--- Dynamic Programming/1D DP/Count Number of Hops.cpp
+++ Dynamic Programming/1D DP/Count Number of Hops.cpp
@@ -1,5 +1,5 @@
 // Program to find the total number of ways the frog can take to reach the top of the Nth step, a frog can jump either 1, 2, or 3 steps to go to the top ~ coded by Hiren
-#define mod 1000000007
+#define MOD 1000000007
 #define ll long long
 #include <iostream>
 #include <vector>
@@ -30,12 +30,12 @@ private:
             return memory[N];
         
         // There are always three posibilities to reach the Nth step
-        ll jumpOneStep   = solveWithMemo(N-1, memory) % mod; // Is to jump from the immediate 1st step below the Nth step  
-        ll jumpTwoStep   = solveWithMemo(N-2, memory) % mod; // Is to jump from the immediate 2nd step below the Nth step     
-        ll jumpThreeStep = solveWithMemo(N-3, memory) % mod; // Is to jump from the immediate 3rd step below the Nth step   
+        ll jumpOneStep   = solveWithMemo(N-1, memory) % MOD; // Is to jump from the immediate 1st step below the Nth step  
+        ll jumpTwoStep   = solveWithMemo(N-2, memory) % MOD; // Is to jump from the immediate 2nd step below the Nth step     
+        ll jumpThreeStep = solveWithMemo(N-3, memory) % MOD; // Is to jump from the immediate 3rd step below the Nth step   
 
         // Store the result value to the memoization table and then return it 
-        return memory[N] = (jumpOneStep + jumpTwoStep + jumpThreeStep) % mod;   
+        return memory[N] = (jumpOneStep + jumpTwoStep + jumpThreeStep) % MOD;   
     }
 
     // O(3^N) & O(N)
@@ -49,12 +49,12 @@ private:
             return 0;
             
         // There are always three posibilities to reach the Nth step
-        ll jumpOneStep   = solveWithoutMemo(N-1) % mod; // Is to jump from the immediate 1st step below the Nth step  
-        ll jumpTwoStep   = solveWithoutMemo(N-2) % mod; // Is to jump from the immediate 2nd step below the Nth step     
-        ll jumpThreeStep = solveWithoutMemo(N-3) % mod; // Is to jump from the immediate 3rd step below the Nth step   
+        ll jumpOneStep   = solveWithoutMemo(N-1) % MOD; // Is to jump from the immediate 1st step below the Nth step  
+        ll jumpTwoStep   = solveWithoutMemo(N-2) % MOD; // Is to jump from the immediate 2nd step below the Nth step     
+        ll jumpThreeStep = solveWithoutMemo(N-3) % MOD; // Is to jump from the immediate 3rd step below the Nth step   
             
         // Return the number of ways the frog can take to reach the Nth step
-        return (jumpOneStep + jumpTwoStep + jumpThreeStep) % mod;   
+        return (jumpOneStep + jumpTwoStep + jumpThreeStep) % MOD;   
     }
 };
 
@@ -75,10 +75,10 @@ public:
         dp[2] = (N > 1) ? 2 : 0; // Stores the number of ways the frog can take to reach the top (considering the first two steps only)
         
         for(int J=3; J<=N; ++J) {
-            ll jumpOneStep   = dp[J-1] % mod; 
-            ll jumpTwoStep   = dp[J-2] % mod; 
-            ll jumpThreeStep = dp[J-3] % mod; 
-            dp[J] = (jumpOneStep + jumpTwoStep + jumpThreeStep) % mod;
+            ll jumpOneStep   = dp[J-1] % MOD; 
+            ll jumpTwoStep   = dp[J-2] % MOD; 
+            ll jumpThreeStep = dp[J-3] % MOD; 
+            dp[J] = (jumpOneStep + jumpTwoStep + jumpThreeStep) % MOD;
         }
         
         // Return the number of ways the frog can take to reach the Nth step
@@ -97,10 +97,10 @@ public:
         ll numWays       = 0; // Stores the result value
         
         for(int J=1; J<=N; ++J) {
-            numWays       = (jumpThreeStep + jumpTwoStep + jumpOneStep) % mod;
-            jumpThreeStep = jumpTwoStep % mod; 
-            jumpTwoStep   = jumpOneStep % mod; 
-            jumpOneStep   = numWays % mod;     
+            numWays       = (jumpThreeStep + jumpTwoStep + jumpOneStep) % MOD;
+            jumpThreeStep = jumpTwoStep % MOD; 
+            jumpTwoStep   = jumpOneStep % MOD; 
+            jumpOneStep   = numWays % MOD;     
         }
             
         // Return the total number of ways the frog can take to reach the Nth step
