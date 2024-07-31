9623f50d9378d115bb3bc9bfd2cb826df4ff4af3
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-07-31 16:30:04 +0530

    Update and rename Geek Jump With K Distances.cpp to Geek Jump With K Distances.cpp

---
+++ Dynamic Programming/1D DP/Geek Jump With K Distances.cpp
@@ -0,0 +1,90 @@
+// Code to find the minimum possible total cost incurred before the Geek reaches the Nth stone, the Geek can jump to any one of the following, stone startStone + 1, startStone + 2, ... startStone + K stone and cost will be [hi - hj] is incurred, where jump is the stone to land on ~ coded by Hiren
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+// Class to implement the Top-down approach:
+class TopDown {
+public:
+    // Method to find the minimum possible total cost, using recursion with memoization - O(N*K) & O(N)
+    int minimizeCost(vector<int>& height, int N, int K) {
+        vector<int> memory(N-1, -1);
+        return solveWithMemo(memory, height, N, K, 0);
+    }
+    
+private:
+    // O(K*N) & O(N+N)
+    int solveWithMemo(vector<int>& memory, vector<int>& height, int N, int K, int startStone) {
+        // Edge case: If Geek reached the last stone then there's no need to compute more cost
+        if(startStone == N-1)
+            return 0;
+
+        // Memoization table: If the current state is already computed, then return the computed value
+        if(memory[startStone] != -1)
+            return memory[startStone];
+        
+        // Stores the result value 
+        int minCost = INT_MAX;
+        
+        // Try each possible jump from the current stone and look for the minimum cost
+        for(int jump = 1; (jump <= K && startStone + jump < N); jump++) {
+            int nextCost = solveWithMemo(memory, height, N, K, startStone + jump);
+            minCost      = min(minCost, nextCost + abs(height[startStone] - height[startStone + jump]));
+        }
+        
+        // Store the result value to the memoization table and then return it
+        return memory[startStone] = minCost;
+    }
+
+    // O(K^N) & O(N)
+    int solveWithoutMemo(vector<int>& height, int N, int K, int startStone) {
+        // Edge case: If Geek reached the last stone then there's no need to compute more cost
+        if(startStone == N-1)
+            return 0;
+
+        // Stores the result value 
+        int minCost = INT_MAX;
+        
+        // Try each possible jump from the current stone and look for the minimum cost
+        for(int jump = 1; (jump <= K && startStone + jump < N); jump++) {
+            int nextCost = solveWithoutMemo(height, N, K, startStone + jump);
+            minCost      = min(minCost, nextCost + abs(height[startStone] - height[startStone + jump]));
+        }
+        
+        // Return the result value            
+        return minCost;
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+// Class to implement the Bottom-up Approach:
+class BottomUp {
+public:
+    // Method to find the minimum possible total cost, using 1D tabulation - O(N*K) & O(N)
+    int minimizeCost(vector<int>& height, int N, int K) {
+        // 1D table: dp[stone] represents the minimum cost required to reach the individual stone
+        vector<int> dp(N, INT_MAX); 
+
+        // Set the edge case: If Geek reached the last stone then there's no need to compute more cost
+        dp[N - 1] = 0;                 
+        
+        for(int startStone = N-2; startStone >= 0; --startStone) {  
+            int minCost = INT_MAX;
+
+            for(int jump = 1; (jump <= K && startStone + jump < N); jump++) {
+                int nextCost = dp[startStone + jump];
+                minCost      = min(minCost, nextCost + abs(height[startStone] - height[startStone + jump]));
+            }
+
+            dp[startStone] = minCost; 
+        }
+
+        return dp[0];
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+Topics: Array | Dynamic Programming
+Links : https://www.geeksforgeeks.org/problems/minimal-cost/1
+        https://www.codingninjas.com/studio/problems/minimal-cost_8180930?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf 
