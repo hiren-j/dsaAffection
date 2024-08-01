95051171689b364e200d751a37cdcfb95d3f3d23
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-01 22:15:15 +0530

    Update and rename Perfect Squares.cpp to Perfect Squares.cpp

---
+++ Dynamic Programming/1D DP/Perfect Squares.cpp
@@ -0,0 +1,82 @@
+// Code to find the least number of perfect square numbers that sums to N ~ coded by Hiren
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+// Class to implement the Top-down approach:
+class TopDown {
+public:
+    // Method to find the least number of perfect square numbers that sums to N, using recursion with memoization - O(N*sqrt(N)) & O(N)
+    int numSquares(int N) {
+        vector<int> memory(N+1, -1);
+        return solveWithMemo(N, memory);
+    }
+
+private:
+    // O(N*sqrt(N)) & O(N+N)
+    int solveWithMemo(int N, vector<int>& memory) {
+        // Edge case: If N is equal to 0 then its not possible to get any perfect square number
+        if(N == 0)
+            return 0;
+
+        // Memoization table: If the current state is already computed then return the computed value
+        if(memory[N] != -1)
+            return memory[N];
+
+        // Stores the result value
+        int minCount = INT_MAX;
+
+        // Explore all the possibilities which can sum to N and update the result by the minimum value
+        for(int J=1; J*J<=N; J++) 
+            minCount = min(minCount, 1 + solveWithMemo(N - J*J, memory));
+
+        // Store the result value to the memoization table and then return it
+        return memory[N] = minCount;
+    }
+
+    // O(sqrt(N)^N) & O(N)
+    int solveWithoutMemo(int N) {
+        // Edge case: If N is equal to 0 then its not possible to get any perfect square number
+        if(N == 0)
+            return 0;
+
+        // Stores the result value
+        int minCount = INT_MAX;
+
+        // Explore all the possibilities which can sum to N and update the result by the minimum value
+        for(int J=1; J*J<=N; J++) 
+            minCount = min(minCount, 1 + solveWithoutMemo(N - J*J));
+
+        // Return the result value
+        return minCount;
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+// Class to implement the Bottom-up approach:
+class BottomUp {
+public:
+    // Method to find the least number of perfect square numbers that sums to N, using 1D tabulation - O(N*sqrt(N)) & O(N)
+    int numSquares(int N) {
+        // 1D table: dp[num] stores the least number of perfect square numbers which can sum to num
+        vector<int> dp(N+1, INT_MAX);
+
+        // Set the edge case: If N is equal to 0 then its not possible to get any perfect square number
+        dp[0] = 0;
+        
+        // Consider each num as an individual N and calculate the least number of perfect square numbers that sums to it
+        for(int num=1; num<=N; num++) {
+            for(int J=1; J*J<=num; J++) {
+                dp[num] = min(dp[num], 1 + dp[num - J*J]);
+            }
+        }
+
+        // Return the result value
+        return dp[N];
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+Topics: Math | Dynamic Programming
+Link  : https://leetcode.com/problems/perfect-squares/description/
