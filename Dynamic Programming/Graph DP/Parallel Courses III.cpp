c35f6ce1c53543ed03831b8c275e52c5af72972e
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-08 23:52:56 +0530

    Create Parallel Courses III.cpp

---
+++ Dynamic Programming/Graph DP/Parallel Courses III.cpp
@@ -0,0 +1,83 @@
+// Code to find the minimum number of months needed to complete all the courses such that by following the mentioned rules ~ coded by Hiren
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Class to implement the Top-down approach:
+class DynamicProgramming {
+    vector<vector<int>> adjList;
+    vector<int> visited;  
+
+    // O(V * (V+E)) & O(V)
+    int solveWithoutMemo(int course, vector<int>& time) {
+        // Stores the total time required to complete the course
+        int maximumTime = 0;
+
+        // Explore each neighbor courses and find the maximum remaining time to complete the course
+        for(int neighbor : adjList[course]) 
+            maximumTime = max(maximumTime, solveWithoutMemo(neighbor, time));
+
+        // Return the result value
+        return (maximumTime == 0) ? time[course - 1] : time[course - 1] + maximumTime;
+    }
+    
+    // O(V + V+E) & O(V+V)
+    int solveWithMemo(int course, vector<int>& time) {
+        // Stores the total time required to complete the course
+        int maximumTime = 0;
+
+        for(int neighbor : adjList[course]) {
+            // If the current state is not computed then find the maximum remaining time to complete the course
+            if(!visited[neighbor]) {
+                maximumTime = max(maximumTime, solveWithMemo(neighbor, time));
+            }
+            // If the current state is already computed then update the result by the maximum value
+            else {
+                maximumTime = max(maximumTime, visited[neighbor]);
+            }
+        }
+
+        // Store the result value to the memoization table and then return it
+        return visited[course] = (maximumTime == 0) ? time[course - 1] : time[course - 1] + maximumTime;
+    }
+
+public:
+    // Method to find minimum number of months needed to complete all the courses, using recursion with memoization - O(V+E) & O(V+E)
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        // Edge case: If the graph is empty then return the maximum time  
+        if(relations.size() == 0)
+            return *max_element(begin(time), end(time));
+
+        adjList.resize(n + 1); // Stores the list of neighbours of each course
+        visited.resize(n + 1); // Memoization table
+
+        int source = -1, maxNeighbors = -1;
+
+        // Create the adjacency list and get the maximum number of neighbours
+        for(auto& r : relations) {
+            int prevCourse = r[1];
+            int nextCourse = r[0];
+            adjList[prevCourse].push_back(nextCourse);
+            int totalNeighbors = adjList[prevCourse].size();
+            if(totalNeighbors > maxNeighbors) {
+                source = prevCourse;
+                maxNeighbors = totalNeighbors;
+            }
+        }
+
+        // Stores the result value
+        int minMonths = 0;
+        
+        // Explore each course 
+        for(int course = 1; course <= n; ++course) 
+            if(!visited[course]) 
+                minMonths = max(minMonths, solveWithMemo(course, time));
+
+        // Return the result value
+        return minMonths;
+    }
+};
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Topics: Array | Dynamic Programming | Graph
+Link  : https://leetcode.com/problems/parallel-courses-iii/description/
