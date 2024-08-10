a4a3d75fa95215ff0a338dfa91676be268a3b5e7
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-10 21:17:53 +0530

    Update and rename Pascal's Triangle.cpp to 1 - Pascal's Triangle.cpp

--- Dynamic Programming/Multi Dimensional DP/1 - Pascal's Triangle.cpp
+++ Dynamic Programming/Multi Dimensional DP/1 - Pascal's Triangle.cpp
@@ -1,16 +1,16 @@
 // Code to find the first "numRows" of Pascal's triangle ~ coded by Hiren
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Class to implement the Bottom-up approach:
 class BottomUp {
 public:
-    // Method to find the first "numRows" of Pascal's triangle, using 2D tabulation - O(numRows^2) & O(numRows^2) 
+    // Method to find the first "numRows" of pascal's triangle, using 2D tabulation - O(numRows^2) & O(numRows^2) 
     vector<vector<int>> generateTable(int numRows) {
-        // Stores the values of the Pascal's triangle
+        // Stores the values of the pascal's triangle
         vector<vector<int>> pascalTable;
 
-        // In Pascal's triangle suppose you're on the 0th row then it will contain 1 column, if on the 1th row then it will contain 2 columns and so on for the rest of the rows
+        // In pascal's triangle suppose you're on the 0th row then it will contain 1 column, if on the 1th row then it will contain 2 columns and so on for the rest of the rows
         for(int R = 0; R < numRows; R++) {
             pascalTable.emplace_back(vector<int>(R + 1, 1));
             for(int C = 1; C < R; C++) {
@@ -23,7 +23,7 @@ public:
 };
 // Note: The auxiliary space is considered because even if its not the user's demand but still we have to create the pascalTable to get the output
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: Array | Dynamic Programming
 Link  : https://leetcode.com/problems/pascals-triangle/description/
