29f1b8160ef845e72a2007dc5c8c62545dd7ca3b
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-11 16:24:45 +0530

    Update 19 - Minimize The Difference Between Target And Chosen Elements.cpp

--- Dynamic Programming/Multi Dimensional DP/19 - Minimize The Difference Between Target And Chosen Elements.cpp
+++ Dynamic Programming/Multi Dimensional DP/19 - Minimize The Difference Between Target And Chosen Elements.cpp
@@ -11,13 +11,15 @@ NOTE: So, In the worst case the highest value of pathSum could be 70*70 = 4900.
       So, I measured it through the problem constraints. Hope you've got it!
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+/*
+      
 NOTE: In the bottom-up approach, the sum iterations start from 2500 and go down to 0. Initially, I started with a larger value, around 4089, and experimented with different values to avoid extra iterations. 
       After several adjustments, 2500 emerged as the most fitting value based on my intuition. This adjustment is necessary to ensure we cover all relevant sum values. 
       Starting from 4089 works, but starting from 4900 or higher can lead to segmentation faults due to out-of-bounds accesses. 
       Starting from 2500 ensures we cover all possible sums more efficiently and avoids such issues, requiring only 2500 iterations. 
-      This approach helps to effectively explore all possible sums and capture the minimum absolute difference within the problem's context
+      This approach helps to effectively explore all possible sums and capture the minimum absolute difference within the problem's context.
 
+*/
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
       
 // Class to implement the Top-down approach:
