ea856c0b473ccdd0a00c078eeeb8ff6dfc7f3dc9
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-12 10:30:54 +0530

    Update 10 - Ones And Zeroes.cpp

--- Dynamic Programming/Subsequences DP/10 - Ones And Zeroes.cpp
+++ Dynamic Programming/Subsequences DP/10 - Ones And Zeroes.cpp
@@ -85,26 +85,26 @@ public:
             vector<vector<vector<int>>> dp(size + 1, vector<vector<int>>(m + 2, vector<int>(n + 2, INT_MIN)));
 
             // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most m 0's and n 1's in it then return 0 as a valid indication of it
-            for(int M = 0; M <= m+1; ++M) 
-                for(int N = 0; N <= n+1; ++N) 
-                    dp[size][M][N] = 0;
+            for(int zeros = 0; zeros <= m+1; ++zeros) 
+                for(int ones = 0; ones <= n+1; ++ones) 
+                    dp[size][zeros][ones] = 0;
 
             // Fill the rest of the table
             for(int index = size-1; index >= 0; --index) {
-                for(int M = 0; M <= m; ++M) {
-                    for(int N = 0; N <= n; ++N) {
-                        int currSkip = dp[index + 1][M][N];
+                for(int zeros = 0; zeros <= m; ++zeros) {
+                    for(int ones = 0; ones <= n; ++ones) {
+                        int currSkip = dp[index + 1][zeros][ones];
                         int currTake = INT_MIN, countOnes = 0, countZeros = 0;
     
                         for(char bit : strs[index]) 
                             (bit == '1') ? countOnes++ : countZeros++; 
                         
-                        if(M - countZeros >= 0 && N - countOnes >= 0) {
-                            int nextLength = dp[index + 1][M - countZeros][N - countOnes];
+                        if(zeros - countZeros >= 0 && ones - countOnes >= 0) {
+                            int nextLength = dp[index + 1][zeros - countZeros][ones - countOnes];
                             if(nextLength != INT_MIN) currTake = 1 + nextLength;
                         }  
     
-                        dp[index][M][N] = max(currSkip, currTake);
+                        dp[index][zeros][ones] = max(currSkip, currTake);
                     }
                 }
             }
@@ -121,26 +121,26 @@ public:
             vector<vector<int>> nextRow(m + 2, vector<int>(n + 2, INT_MIN)), idealRow(m + 2, vector<int>(n + 2, INT_MIN));
 
             // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most m 0's and n 1's in it then return 0 as a valid indication of it
-            for(int M = 0; M <= m+1; ++M) 
-                for(int N = 0; N <= n+1; ++N) 
-                    nextRow[M][N] = 0;
+            for(int zeros = 0; zeros <= m+1; ++zeros) 
+                for(int ones = 0; ones <= n+1; ++ones) 
+                    nextRow[zeros][ones] = 0;
 
             // Fill the rest of the table
             for(int index = size-1; index >= 0; --index) {
-                for(int M = 0; M <= m; ++M) {
-                    for(int N = 0; N <= n; ++N) {
-                        int currSkip = nextRow[M][N];
+                for(int zeros = 0; zeros <= m; ++zeros) {
+                    for(int ones = 0; ones <= n; ++ones) {
+                        int currSkip = nextRow[zeros][ones];
                         int currTake = INT_MIN, countOnes = 0, countZeros = 0;
     
                         for(char bit : strs[index]) 
                             (bit == '1') ? countOnes++ : countZeros++; 
     
-                        if(M - countZeros >= 0 && N - countOnes >= 0) {
-                            int nextLength = nextRow[M - countZeros][N - countOnes];
+                        if(zeros - countZeros >= 0 && ones - countOnes >= 0) {
+                            int nextLength = nextRow[zeros - countZeros][ones - countOnes];
                             currTake = (nextLength != INT_MIN ? 1 + nextLength : INT_MIN);
                         }
     
-                        idealRow[M][N] = max(currSkip, currTake);
+                        idealRow[zeros][ones] = max(currSkip, currTake);
                     }
                 }
                 nextRow = idealRow;
@@ -236,8 +236,8 @@ public:
     // Subclass to implement the Bottom-up approach:
     class BottomUp {
     public:
-        // #1 Method to find the size of the largest subset, using 3D tabulation - O(size*m*n) & O(size*m*n)
-        int findMaxForm_V1(vector<string>& strs, int m, int n) {
+        // #1 Method to find the size of the largest subset, using 3D tabulation - O(size*zeros*ones) & O(size*zeros*ones)
+        int findMaxcForm_V1(vector<string>& strs, int m, int n) {
             int size = strs.size();
 
             // Stores the count of 0's and 1's of each string of "strs"
@@ -255,26 +255,26 @@ public:
             // 3D DP table
             vector<vector<vector<int>>> dp(size + 1, vector<vector<int>>(m + 2, vector<int>(n + 2, INT_MIN)));
             
-            // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most m 0's and n 1's in it then return 0 as a valid indication of it
-            for(int m = 0; m <= m+1; ++m)
-                for(int n = 0; n <= n+1; ++n)
-                    dp[size][m][n] = 0;
+            // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most zeros 0's and ones 1's in it then return 0 as a valid indication of it
+            for(int zeros = 0; zeros <= m+1; ++zeros)
+                for(int ones = 0; ones <= n+1; ++ones)
+                    dp[size][zeros][ones] = 0;
 
             // Fill the rest of the table
             for(int index = size-1; index >= 0; --index) {
-                for(int m = 0; m <= m; ++m) {
-                    for(int n = 0; n <= n; ++n) {
-                        int currSkip = dp[index + 1][m][n];
+                for(int zeros = 0; zeros <= m; ++zeros) {
+                    for(int ones = 0; ones <= n; ++ones) {
+                        int currSkip = dp[index + 1][zeros][ones];
                         int currTake = INT_MIN;
-                        int newRow   = m - countZerosOnes[index].first; 
-                        int newCol   = n - countZerosOnes[index].second; 
+                        int newRow   = zeros - countZerosOnes[index].first; 
+                        int newCol   = ones - countZerosOnes[index].second; 
 
                         if(newRow >= 0 && newCol >= 0) {
                             int nextLength = dp[index + 1][newRow][newCol];
                             currTake = (nextLength != INT_MIN ? 1 + nextLength : INT_MIN);
                         }
 
-                        dp[index][m][n] = max(currSkip, currTake); 
+                        dp[index][zeros][ones] = max(currSkip, currTake); 
                     }
                 }
             }
@@ -283,7 +283,7 @@ public:
             return dp[0][m][n];
         }
 
-        // #2 Method to find the size of the largest subset, using 2D tabulation - O(size*m*n) & O(m*n)
+        // #2 Method to find the size of the largest subset, using 2D tabulation - O(size*zeros*ones) & O(zeros*ones)
         int findMaxForm_V2(vector<string>& strs, int m, int n) {
             int size = strs.size();
 
@@ -302,26 +302,26 @@ public:
             // 2D DP tables
             vector<vector<int>> nextRow(m + 2, vector<int>(n + 2, INT_MIN)), idealRow(m + 2, vector<int>(n + 2, INT_MIN));
 
-            // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most m 0's and n 1's in it then return 0 as a valid indication of it
-            for(int m = 0; m <= m+1; ++m)
-                for(int n = 0; n <= n+1; ++n)
-                    nextRow[m][n] = 0;
+            // Initialize the first base case: If all the strings are exhausted and you've found a subset such that there are at most zeros 0's and ones 1's in it then return 0 as a valid indication of it
+            for(int zeros = 0; zeros <= m+1; ++zeros)
+                for(int ones = 0; ones <= n+1; ++ones)
+                    nextRow[zeros][ones] = 0;
 
             // Fill the rest of the table
             for(int index = size-1; index >= 0; --index) {
-                for(int m = 0; m <= m; ++m) {
-                    for(int n = 0; n <= n; ++n) {
-                        int currSkip = nextRow[m][n];
+                for(int zeros = 0; zeros <= m; ++zeros) {
+                    for(int ones = 0; ones <= n; ++ones) {
+                        int currSkip = nextRow[zeros][ones];
                         int currTake = INT_MIN;
-                        int newRow   = m - countZerosOnes[index].first; 
-                        int newCol   = n - countZerosOnes[index].second; 
+                        int newRow   = zeros - countZerosOnes[index].first; 
+                        int newCol   = ones - countZerosOnes[index].second; 
 
                         if(newRow >= 0 && newCol >= 0) {
                             int nextLength = nextRow[newRow][newCol];
                             currTake = (nextLength != INT_MIN ? 1 + nextLength : INT_MIN);
                         }
 
-                        idealRow[m][n] = max(currSkip, currTake); 
+                        idealRow[zeros][ones] = max(currSkip, currTake); 
                     }
                 }
                 nextRow = idealRow;
