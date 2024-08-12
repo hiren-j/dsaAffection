7e8cfdfe1098f54cafb019d3fd423ef6e61416e1
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-12 19:01:23 +0530

    Update 4 - Print Longest Increasing Subsequence.cpp

--- Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence.cpp
+++ Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence.cpp
@@ -23,7 +23,7 @@ public:
             }
         }
 
-        int resultIndex = -1; // Stores the index of the result value which has to be taken
+        int resultIndex = -1; // Stores the index of the value which has to be taken (result value)
         int maxLength   = -1; // Stores the length of the LIS
 
         // Find the length of the LIS 
