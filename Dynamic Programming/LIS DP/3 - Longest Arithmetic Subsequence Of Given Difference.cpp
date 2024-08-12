8da1f56cb890211154285b4f7bfb8d03d5a706fc
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-12 16:22:01 +0530

    Update and rename Longest Arithmetic Subsequence Of Given Difference.cpp to 3 - Longest Arithmetic Subsequence Of Given Difference.cpp

--- Dynamic Programming/LIS DP/3 - Longest Arithmetic Subsequence Of Given Difference.cpp
+++ Dynamic Programming/LIS DP/3 - Longest Arithmetic Subsequence Of Given Difference.cpp
@@ -1,6 +1,6 @@
 // Code to find the length of the longest subsequence in array which is an arithmetic sequence such that the difference between adjacent elements in the subsequence is equal to the given difference ~ coded by Hiren
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // #1 Class to implement the Top-down approach:
 class TopDown_V1 {
@@ -55,8 +55,8 @@ private:
 };
 // Note: This solution can lead to the memory-limit-exceed
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
 // #2 Class to implement the Top-down approach:
 class TopDown_V2 {
 public:
@@ -90,9 +90,47 @@ private:
         return memory[prevIndex + 1] = max(currSkip, currTake);
     }
 };
-// Note: This solution can lead to the time-limit-exceed
+// Note: This solution (TopDown_V2) is the space optimized version of the (TopDown_V1) solution but it will lead to the time-limit-exceed
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// #3 Class to implement the Top-down approach:
+class TopDown_V3 {
+public:
+    // Method to find the length of the longest arithmetic subsequence of given difference, using recursion with memoization - O(N^3) & O(N^2)
+    int longestSubsequence(vector<int>& nums, int difference) {
+        int n = nums.size();
+        vector<vector<int>> memory(n, vector<int>(n + 1, -1));
+        return solveWithMemo(memory, nums, difference, n, 0, -1);
+    }
+
+private:
+    // O(N*N*N) & O(N*N + N)
+    int solveWithMemo(vector<vector<int>>& memory, vector<int>& nums, int difference, int n, int startIndex, int prevIndex) {
+        // Edge case: If all the values are exhausted then you can't pick any more
+        if(startIndex == n)
+            return 0;
+
+        // Memoization table: If the current state is already computed then return the computed value
+        if(memory[startIndex][prevIndex + 1] != -1)
+            return memory[startIndex][prevIndex + 1];
+
+        // Stores the result value
+        int maxLength = 0;
+
+        // Iterate and if the difference between previous and the index value is equal to the given difference then pick the index value
+        for(int index = startIndex; index < n; ++index) 
+            if(prevIndex == -1 || nums[index] - nums[prevIndex] == difference)
+                maxLength = max(maxLength, 1 + solveWithMemo(memory, nums, difference, n, index + 1, index));
+
+        // Store the result value to the memoization table and then return it
+        return memory[startIndex][prevIndex + 1] = maxLength;
+    }
+    // Note: `solveWithoutMemo` function will have O(N^N) time complexity and O(N) auxiliary space. You can easily create it by removing the memoization from this `solveWithMemo`, which is straightforward to implement. The full function isn't provided here to avoid larger code
+};
+// Note: This solution (TopDown_V3) is the loop conversion of the first solution (TopDown_V1) and you could see that the time complexity increases in this (TopDown_V3) that's why it will give memory-limit-exceed
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // #1 Class to implement the Bottom-up approach:
 class BottomUp_V1 {
@@ -107,17 +145,16 @@ public:
             for(int prevIndex = index-1; prevIndex >= -1; --prevIndex) {
                 int currSkip = dp[index + 1][prevIndex + 1];
                 int currTake = 0;
-        
-                if(prevIndex == -1 || nums[index] - nums[prevIndex] == difference)
+                if(prevIndex == -1 || nums[index] - nums[prevIndex] == difference) {
                     currTake = 1 + dp[index + 1][index + 1];
-        
+                }
                 dp[index][prevIndex + 1] = max(currSkip, currTake);
             }
         }
 
         return dp[0][0];
     }
-    // Note: This solution can lead to the memory-limit-exceed
+    // Note: This bottom-up solution is created from the memoized solution of (TopDown_V1). It will lead to the memory-limit-exceed
 
     // #2 Method to find the length of the longest arithmetic subsequence of given difference, using 1D tabulation - O(N*N) & O(N)
     int longestSubsequence_V2(vector<int>& nums, int difference) {
@@ -129,10 +166,9 @@ public:
             for(int prevIndex = index-1; prevIndex >= -1; --prevIndex) {
                 int currSkip = nextRow[prevIndex + 1];
                 int currTake = 0;
-        
-                if(prevIndex == -1 || nums[index] - nums[prevIndex] == difference)
+                if(prevIndex == -1 || nums[index] - nums[prevIndex] == difference) {
                     currTake = 1 + nextRow[index + 1];
-        
+                }
                 idealRow[prevIndex + 1] = max(currSkip, currTake);
             }
             nextRow = idealRow;
@@ -143,7 +179,7 @@ public:
     // Note: This solution can lead to the time-limit-exceed
 };
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // #2 Class to implement the Bottom-up approach:
 class BottomUp_V2 {
@@ -152,19 +188,22 @@ public:
     int longestSubsequence(vector<int>& nums, int difference) {
         int n = nums.size();
 
+        // 1D table: dp[index] represents the length of the longest subsequence ending at the individual index
         vector<int> dp(n, 1);
 
+        // Fill the table
         for(int index = 0; index < n; ++index) 
             for(int prevIndex = 0; prevIndex < index; ++prevIndex) 
                 if(nums[index] - nums[prevIndex] == difference)
                     dp[index] = max(dp[index], 1 + dp[prevIndex]);
-                
+
+        // Return the result value
         return *max_element(begin(dp), end(dp));
     }
 };
 // Note: This solution can lead to the time-limit-exceed
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // #3 Class to implement the Bottom-up approach:
 class BottomUp_V3 {
@@ -187,7 +226,7 @@ public:
     }
 };
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: Array | Hash Table | Dynamic Programming 
 Link  : https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
