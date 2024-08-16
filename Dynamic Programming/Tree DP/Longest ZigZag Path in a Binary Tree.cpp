92a0a9bc0c5e173378971c47c1a2d675c65760f5
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-16 10:20:02 +0530

    Update Longest ZigZag Path in a Binary Tree.cpp

--- Dynamic Programming/Tree DP/Longest ZigZag Path in a Binary Tree.cpp
+++ Dynamic Programming/Tree DP/Longest ZigZag Path in a Binary Tree.cpp
@@ -1,13 +1,13 @@
 // Code to find the longest ZigZag path contained in that tree. Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0) ~ coded by Hiren
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
     DON'T IGNORE MUST READ (REGARDING TIME AND AUXILIARY SPACE OF THE FUNCTION):
 
         `getLongestPath() without memoization` 
             - Time complexity: O(N*N) 
-              Explanation: The total number of nodes is N. The auxiliary time of the `getLongestPath()` is O(N) here and as we're calling the function from each node of the tree hence the overall time complexity becomes O(N*N).
+              Explanation: Let the total number of nodes is N. The auxiliary time of the `getLongestPath()` is O(N) here and as we're calling the function from each node of the tree hence the overall time complexity becomes O(N*N).
                
             - Auxiliary space : O(H)
               Explanation: Where H let be the height of the tree. It represents the maximum depth of the recursion stack.
@@ -15,15 +15,15 @@
         `getLongestPath() with memoization` 
             - Time complexity: O(NLogN) 
               Explanation: The memoization table will end up storing results for N*2 states in the worst case. As the table is an ordered map and we've performed operations such as insertion and searching which could take `Log(sizeofmap)` time. 
-              More clearly Log(N) time, the results will be calculated once after that the memoization table will be used to return the result in Log(N) time, 
-              as we're calling the `getLongestPath()` for each node then the overall time complexity becomes O(NLogN). 
+              More clearly Log(N) time. The results will be calculated once after that the memoization table will be used to return the result in Log(N) time, and
+              as we're calling the `getLongestPath()` from each node then the overall time complexity becomes O(NLogN). 
                           
             - Auxiliary space : O(N*2 + H)
-              Explanation: The first term N*2 represents the space taken by the memoization table. It will end up storing results for N*2 states in the worst case.
+              Explanation: The first term N*2 represents the space taken by the memoization table. Why 2? It's because the second parameter could be either true or false. So this how it will end up storing results for N*2 states in the worst case.
               Where H let be the height of the tree. It represents the maximum depth of the recursion stack.
 */
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Class to implement the Top-down approach:
 class DynamicProgramming {
@@ -71,7 +71,7 @@ public:
     }
 };
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: Dynamic Programming | Tree | Depth-First Search | Binary Tree
 Link  : https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/
