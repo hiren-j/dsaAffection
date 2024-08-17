e580a90921ba5b02e705089338eb62f61ac84b2a
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-17 12:33:54 +0530

    Update and rename Palindrome Partitioning II.cpp to 4 - Palindrome Partitioning II.cpp

--- Dynamic Programming/Partition DP/4 - Palindrome Partitioning II.cpp
+++ Dynamic Programming/Partition DP/4 - Palindrome Partitioning II.cpp
@@ -1,9 +1,9 @@
 // Code to partition the string "s" such that every substring of the partition is a palindrome and to find the minimum cuts needed for a palindrome partitioning of "s" ~ coded by Hiren
 
-------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the first solution :-
-class Solution_V1 {
+// Class to implement the brute force approach:
+class DP_V1 {
     // Method to check whether a string is a palindrome or not - O(N) & O(1)
     static bool isPalindrome(string& s, int start, int end) {
         while(start < end) {
@@ -17,7 +17,7 @@ class Solution_V1 {
     }
 
 public:
-    // Class to implement the Top-down approach:
+    // Subclass to implement the Top-down approach:
     class TopDown {
         // O(N^3) & O(N+N)
         int solveWithMemo(vector<int>& memory, string& s, int n, int startIndex) {
@@ -32,11 +32,11 @@ public:
             // Stores the result value
             int minCuts = INT_MAX;
             
-            // Perform all the palindrome partitions on the index and update the result by the minimum value
+            // Perform all the palindrome partitions and update the result by the minimum value
             for(int index = startIndex; index < n; ++index) {
                 if(isPalindrome(s, startIndex, index)) {
-                    int currCuts = solveWithMemo(memory, s, n, index + 1);
-                    minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + currCuts) : currCuts);
+                    int nextCuts = solveWithMemo(memory, s, n, index + 1);
+                    minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + nextCuts) : nextCuts);
                 }
             }
 
@@ -53,37 +53,40 @@ public:
         }
     };
 
-    // Class to implement the Bottom-up approach:
+    // Subclass to implement the Bottom-up approach:
     class BottomUp {
     public:
         // Method to find the minimum cuts needed for such palindrome partitioning, using 1D tabulation - O(N^3) & O(N)
         int minCut(string& s) {
             int n = s.size();
 
+            // 1D DP table
             vector<int> dp(n + 1, 0);
 
+            // Fill the table
             for(int startIndex = n-1; startIndex >= 0; --startIndex) {
                 int minCuts = INT_MAX;
 
                 for(int index = startIndex; index < n; ++index) {
                     if(isPalindrome(s, startIndex, index)) {
-                        int currCuts = dp[index + 1];
-                        minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + currCuts) : currCuts);
+                        int nextCuts = dp[index + 1];
+                        minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + nextCuts) : nextCuts);
                     }
                 }
                 
                 dp[startIndex] = minCuts;
             }
 
+            // Return the result value
             return dp[0];
         }
     };
 };
 
-------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the second solution :-
-class Solution_V2 {
+// Class to implement the optimized approach:
+class DP_V2 {
     // Method to check whether a string is a palindrome or not, using recursion with memoization :-
     static bool isPalindrome(vector<vector<int>>& memory, const string& s, int start, int end) {
         // Edge case: If all the letters are equal then the string is a palindrome
@@ -105,7 +108,7 @@ class Solution_V2 {
 public:
     // Class to implement the Top-down approach:
     class TopDown {
-        // O(N^2 + N^2) & O(N^2 + 2*N)
+        // O(N^2 + N^2) & O(N^2 + N+N)
         int solveWithMemo(vector<vector<int>>& memory1, vector<int>& memory2, const string& s, int n, int startIndex) {
             // Edge case: If all the elements are exhausted then you can't do more partitions
             if(startIndex == n) 
@@ -118,11 +121,11 @@ public:
             // Stores the result value
             int minCuts = INT_MAX;
 
-            // Perform all the palindrome partitions on the index and update the result by the minimum value
+            // Perform all the palindrome partitions and update the result by the minimum value
             for(int index = startIndex; index < n; ++index) {
                 if(isPalindrome(memory1, s, startIndex, index)) {
-                    int currCuts = solveWithMemo(memory1, memory2, s, n, index + 1);
-                    minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + currCuts) : currCuts);
+                    int nextCuts = solveWithMemo(memory1, memory2, s, n, index + 1);
+                    minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + nextCuts) : nextCuts);
                 }
             }
 
@@ -146,29 +149,32 @@ public:
         // Method to find the minimum cuts needed for such palindrome partitioning, using 2D tabulation - O(N^2) & O(N^2)
         int minCut(string& s) {
             int n = s.size();
-
+            
+            // DP tables
             vector<vector<int>> dp1(n, vector<int>(n, -1));
             vector<int> dp2(n + 1, 0);
 
+            // Fill the table
             for(int startIndex = n-1; startIndex >= 0; --startIndex) {
                 int minCuts = INT_MAX;
 
                 for(int index = startIndex; index < n; ++index) {
                     if(isPalindrome(dp1, s, startIndex, index)) {
-                        int currCuts = dp2[index + 1];
-                        minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + currCuts) : currCuts);
+                        int nextCuts = dp2[index + 1];
+                        minCuts = (index != n-1 ? minCuts = min(minCuts, 1 + nextCuts) : nextCuts);
                     }
                 }
 
                 dp2[startIndex] = minCuts;
             }
 
+            // Return the result value
             return dp2[0];
         }
     };
 };
 
-------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: String | Dynamic Programming
 Link  : https://leetcode.com/problems/palindrome-partitioning-ii/description/
