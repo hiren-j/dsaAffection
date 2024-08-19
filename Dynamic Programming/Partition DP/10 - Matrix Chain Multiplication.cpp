2cc7bafb389abbbd6a8e5fbcb209735071026eb3
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-08-19 10:20:03 +0530

    Update 10 - Matrix Chain Multiplication.cpp

--- Dynamic Programming/Partition DP/10 - Matrix Chain Multiplication.cpp
+++ Dynamic Programming/Partition DP/10 - Matrix Chain Multiplication.cpp
@@ -3,8 +3,11 @@
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-    NOTE (REGARDING TIME COMPLEXITY OF `solveWithoutMemo` FUNCTION) => O(N * 2^N) : At each matrix you're exploring 2 options or doing 2 partitions. As we've N-1 matrices, so for all of them you're exploring 2 options, which makes it 2^(N-1) = O(2^N). 
-                                                                                    In each recursive call you will again explore 2 options on that same matrix, which you may have already explored for any other matrix of a different recurive call, So in the worst case it will be O(N * 2^N).
+    NOTE (REGARDING TIME COMPLEXITY OF `solveWithoutMemo` FUNCTION) => O(N * 2^N) : At each matrix, you're exploring 2 options or doing 2 partitions. 
+                                                                                    As we have N-1 matrices, so for all of them, you're exploring 2 options, which makes it 2^(N-1) = O(2^N). 
+                                                                                    Now, apart from this, in each recursive call, you will again explore 2 options on that same matrix, which you may 
+                                                                                    have already explored for any other matrix in a different recursive call. So, in the worst case, we have N-1 matrices, 
+                                                                                    so for each, you may do the same thing in the recursion, making it O(N * 2^N).
 */
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
