61e919c94e65e98df0319852b3de25278d1dbb95
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-09-15 14:55:14 +0530

    Update 3 - Linked List in Binary Tree.cpp

--- Dynamic Programming/Tree DP/3 - Linked List in Binary Tree.cpp
+++ Dynamic Programming/Tree DP/3 - Linked List in Binary Tree.cpp
@@ -7,39 +7,6 @@ class DynamicProgramming {
     map<pair<ListNode*, TreeNode*>, bool> dp;
     ListNode* origHead = nullptr;
 
-    // O(N^4) & O(H) : Where N and H is the number of nodes and height of the tree
-    bool solveWithoutMemo(ListNode* headNode, TreeNode* rootNode) {
-        // Edge case: If the list is empty then return true
-        if(!headNode)
-            return true;
-
-        // Edge case: At this point if the tree is empty then return false
-        if(!rootNode)
-            return false;
-
-        // If the value of tree node and list node is equal
-        if(rootNode->val == headNode->val) {
-            // If its the last node of the list then return true
-            if(!headNode->next)
-                return true;
-
-            // If the value of left child is equal to the next node of list then check the existence of the remaining list at the left subtree
-            if(rootNode->left && headNode->next->val == rootNode->left->val && solveWithoutMemo(headNode->next, rootNode->left))
-                return true;
-
-            // If the value of right child is equal to the next node of list then check the existence of the remaining list at the right subtree
-            if(rootNode->right && headNode->next->val == rootNode->right->val && solveWithoutMemo(headNode->next, rootNode->right)) 
-                return true;
-        }
-        
-        // Check the existence of the list from its first node at the left and right side of the tree node
-        bool isPathFromLeft  = solveWithoutMemo(origHead, rootNode->left);
-        bool isPathFromRight = solveWithoutMemo(origHead, rootNode->right);
-
-        // If the list is present as a path in any of the subtrees then return true
-        return isPathFromLeft || isPathFromRight;
-    }
-
     // O((M*N)Log(M*N)) & O(M*N + H) : Where M is the length of the list. N and H is the number of nodes and height of the tree
     bool solveWithMemo(ListNode* headNode, TreeNode* rootNode) {
         // Edge case: If the list is empty then return true
