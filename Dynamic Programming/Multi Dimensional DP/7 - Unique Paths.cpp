70e643a4b572baeb417104d5c59dd1d8c9f058bd
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2024-10-10 09:08:47 +0530

    Update 7 - Unique Paths.cpp

--- Dynamic Programming/Multi Dimensional DP/7 - Unique Paths.cpp
+++ Dynamic Programming/Multi Dimensional DP/7 - Unique Paths.cpp
@@ -2,7 +2,6 @@
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Class to implement the Top-down approach:
 class TopDown {
 public:
     // Method to find the number of possible unique paths, using recursion with memoization - O(M*N) & O(M*N)
@@ -54,8 +53,7 @@ private:
 };
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    
-// Class to implement the Bottom-up approach:
+
 class BottomUp {
 public:
     // #1 Method to find the number of possible unique paths, using 2D tabulation - O(M*N) & O(M*N)
