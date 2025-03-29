2e0dbea016eae951d26288a8e98939b4cd3cd6fe
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-03-29 23:09:04 +0530

    Update 1 - Word Break.cpp

--- Dynamic Programming/Partition DP/1 - Word Break.cpp
+++ Dynamic Programming/Partition DP/1 - Word Break.cpp
@@ -5,13 +5,13 @@
 /*
     DON'T IGNORE MUST READ: This Partition DP pattern is designed to help you understand different ways to partition an array or string to solve complex problems. The pattern contains a total of 14 problems:
     
-        -> Problems 1 to 12: These problems involve making a single partition on the right side after reaching any point in the array or string. For example, consider an array [1, 2, 3]. If you choose index 1 as the partition point, you split the array into [1] and [2, 3]. 
+        -> Problems 1 to 13: These problems involve making a single partition on the right side after reaching any point in the array or string. For example, consider an array [1, 2, 3]. If you choose index 1 as the partition point, you split the array into [1] and [2, 3]. 
                             You then solve the problem for the right side starting from index 2. In these problems, you are considering only one cut or one partition at a time.
     
-        -> Problems 13 to 15: These problems involve making two cuts or two partitions simultaneously to cover multiple segments of the array or string. For example, with the array [1, 2, 3, 4, 5], if you make two cuts at indices 1 and 3, you divide the array into [1], [2, 3, 4], and [5]. 
+        -> Problems 14 to 16: These problems involve making two cuts or two partitions simultaneously to cover multiple segments of the array or string. For example, with the array [1, 2, 3, 4, 5], if you make two cuts at indices 1 and 3, you divide the array into [1], [2, 3, 4], and [5]. 
                              Here, you are considering two cuts or two partitions at the same time to cover multiple segments.
     
-        -> Difference: In problems 1 to 11, you only need to make one cut at a time to split the array or string into segments. In problems 13 to 15, you need to consider making two cuts simultaneously to define multiple segments. 
+        -> Difference: In problems 1 to 11, you only need to make one cut at a time to split the array or string into segments. In problems 14 to 16, you need to consider making two cuts simultaneously to define multiple segments. 
                        Understanding this difference is crucial for applying the correct strategy.
     
         -> If this distinction isn’t clear yet, don’t worry—solving the problems will help you grasp the concept better. This note is just to highlight the key differences.
