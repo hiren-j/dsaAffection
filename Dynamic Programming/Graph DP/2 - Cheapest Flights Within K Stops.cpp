83fb4d8482cbd96f4cb27bb5588f0895d50df09b
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-04-22 20:52:19 +0530

    Update 2 - Cheapest Flights Within K Stops.cpp

--- Dynamic Programming/Graph DP/2 - Cheapest Flights Within K Stops.cpp
+++ Dynamic Programming/Graph DP/2 - Cheapest Flights Within K Stops.cpp
@@ -25,7 +25,7 @@ class DynamicProgramming {
         return minPrice;
     }
 
-    // O(N*N*K) & O(N*K) : Where N = total flights or total nodes.
+    // O(N*K) & O(N*K) : Where N = total flights or total nodes.
     int solveWithMemo(vector<vector<int>>& dp, int src, int dst, int k) {
         if(src == dst)
             return (k >= 0) ? 0 : INT_MAX; // If reached destination within k stops then return 0 as an indication
