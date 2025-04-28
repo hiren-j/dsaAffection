6d96a7626b8ac57d25bc5e7d69829c786518cd25
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-04-28 16:44:17 +0530

    Update 3 - Consecutive Ones Not Allowed.cpp

--- Dynamic Programming/1D DP/3 - Consecutive Ones Not Allowed.cpp
+++ Dynamic Programming/1D DP/3 - Consecutive Ones Not Allowed.cpp
@@ -33,7 +33,7 @@ class TopDown {
     }
 
 public:
-    int countStrings(int n) {
+    int numDistinctBinaryStrs(int n) {
         vector<int> dp(n + 2, -1);
         return solveWithMemo(dp, n + 1); // The main task is to find the n + 1th number of the fibonacci sequence
     }
@@ -74,7 +74,7 @@ class BottomUp {
     }
     
 public:
-    int countStrings(int n) {
+    int numDistinctBinaryStrs(int n) {
         return solveWithoutTable(n);
     }
 };
