630793e24b89e984c1a34fc2346848c4c82849f1
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-04-29 16:27:29 +0530

    Update 11 - Count Ways To Build Good Strings.cpp

--- Dynamic Programming/1D DP/11 - Count Ways To Build Good Strings.cpp
+++ Dynamic Programming/1D DP/11 - Count Ways To Build Good Strings.cpp
@@ -4,39 +4,38 @@
     
 class TopDown {
     const int MOD = 1e9+7;
+    int low, high, zero, one;
 
-    // O(2^strLen) & O(H)
-    int solveWithoutMemo(int low, int high, int zero, int one, int strLen) {
+    // O(2^strLen) & O(high)
+    int solveWithoutMemo(int strLen) {
         if(strLen > high)
             return 0;
 
-        int appendZero  = solveWithoutMemo(low, high, zero, one, strLen + zero);
-        int appendOne   = solveWithoutMemo(low, high, zero, one, strLen + one);
-        int numGoodStrs = ((appendZero + appendOne) % MOD + (strLen >= low && strLen <= high)) % MOD;
+        int appendZero = solveWithoutMemo(strLen + zero);
+        int appendOne  = solveWithoutMemo(strLen + one);
 
-        return numGoodStrs;
+        return (strLen >= low) + (appendZero + appendOne) % MOD;
     }
-    
-    // O(2*H) & O(2*H)
-    int solveWithMemo(vector<int>& dp, int low, int high, int zero, int one, int strLen) {
+
+    // O(2*high) & O(2*high)
+    int solveWithMemo(vector<int>& dp, int strLen) {
         if(strLen > high)
             return 0;
-        
+
         if(dp[strLen] != -1)
             return dp[strLen];
 
-        int appendZero  = solveWithMemo(dp, low, high, zero, one, strLen + zero);
-        int appendOne   = solveWithMemo(dp, low, high, zero, one, strLen + one);
-        int numGoodStrs = ((appendZero + appendOne) % MOD + (strLen >= low && strLen <= high)) % MOD;
+        int appendZero = solveWithMemo(dp, strLen + zero);
+        int appendOne  = solveWithMemo(dp, strLen + one);
 
-        return dp[strLen] = numGoodStrs;
+        return dp[strLen] = (strLen >= low) + (appendZero + appendOne) % MOD;
     }
 
 public:
-    // Method to find the number of good strings, using recursion with memoization - O(H) & O(H) : Where H = high
-    int countGoodStrings(int low, int high, int zero, int one) {
+    int countGoodStrings(int l, int h, int z, int o) {
+        low = l, high = h, zero = z, one = o;
         vector<int> dp(high + 1, -1);
-        return solveWithMemo(dp, low, high, zero, one, 0);
+        return solveWithMemo(dp, 0);
     }
 };
 
@@ -46,21 +45,21 @@ class BottomUp {
     const int MOD = 1e9+7;
 
 public:
-    // Method to find the number of good strings, using 1D tabulation - O(H) & O(H)
+    // O(1*high) & O(1*high)
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1, 0);
-
+        vector<int> dp(high + 1, -1);   
+        
         for(int strLen = high; strLen >= 0; --strLen) {
-            int newIndex1 = strLen + zero;
-            int newIndex2 = strLen + one;
-            int appendZero  = (newIndex1 <= high) ? dp[newIndex1] : 0;
-            int appendOne   = (newIndex2 <= high) ? dp[newIndex2] : 0;
-            int numGoodStrs = ((appendZero + appendOne) % MOD + (strLen >= low && strLen <= high)) % MOD;
-            dp[strLen] = numGoodStrs;
+            int nextIndex1 = strLen + zero; 
+            int nextIndex2 = strLen + one;
+            int appendZero = (nextIndex1 <= high) ? dp[nextIndex1] : 0;
+            int appendOne  = (nextIndex2 <= high) ? dp[nextIndex2] : 0;
+            dp[strLen] = (strLen >= low) + (appendZero + appendOne) % MOD;
         }
 
         return dp[0];
     }
+    // Note: We can't do space optimization in this, as you could see dp[nextIndex1], dp[nextIndex2], the value of `nextIndex1`, `nextIndex2` could be anything, overall its not constant
 };
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
