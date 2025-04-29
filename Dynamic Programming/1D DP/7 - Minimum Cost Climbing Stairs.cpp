b74030a456278bef749125dc42154e385d8ac15d
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-04-29 11:45:02 +0530

    NEW Update 7 - Minimum Cost Climbing Stairs.cpp

--- Dynamic Programming/1D DP/7 - Minimum Cost Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/7 - Minimum Cost Climbing Stairs.cpp
@@ -2,97 +2,88 @@
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class TopDown {
-public:
-    // Method to find the minimum cost, using recursion with memoization - O(N) & O(N)
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
+class Solution {
+    int n;
 
-        // 1D memoization tables
-        vector<int> memory1(n + 1, -1), memory2(n + 1, -1);
+    int solveWithoutMemo(vector<int>& cost, int step) {
+        if(step >= n)
+            return 0;
+        
+        int climb1Step = solveWithoutMemo(cost, step + 1);
+        int climb2Step = solveWithoutMemo(cost, step + 2);
 
-        int startFromZero = solveWithMemo(cost, memory1, n, 0); // Start movements from the 0th step
-        int startFromOne  = solveWithMemo(cost, memory2, n, 1); // Start movements from the 1th step
-
-        // Return the result value
-        return min(startFromZero, startFromOne);
+        return min(climb1Step, climb2Step) + cost[step];
     }
 
-private:
-    // O(2*N) & O(N+N)
-    int solveWithMemo(vector<int>& cost, vector<int>& memory, int n, int step) {
-        // Edge case: If we reached the top of the floor then we don't need any cost
+    int solveWithMemo(vector<int>& memory, vector<int>& cost, int step) {
         if(step >= n)
             return 0;
 
-        // Memoization table: If the current state is already computed then return the computed value
         if(memory[step] != -1)
             return memory[step];
         
-        // There are always two possibilities to perform at each step
-        int climbOneStep = solveWithMemo(cost, memory, n, step + 1); // Is to jump and advance one step ahead from it
-        int climbTwoStep = solveWithMemo(cost, memory, n, step + 2); // Is to jump and advance two steps ahead from it
+        int climb1Step = solveWithMemo(memory, cost, step + 1);
+        int climb2Step = solveWithMemo(memory, cost, step + 2);
 
-        // Store the result value to the memoization table and then return it
-        return memory[step] = cost[step] + min(climbOneStep, climbTwoStep);
+        return memory[step] = min(climb1Step, climb2Step) + cost[step];
     }
 
-    // O(2^N) & O(N)
-    int solveWithoutMemo(vector<int>& cost, int n, int step) {
-        // Edge case: If we reached the top of the floor then we don't need any cost
-        if(step >= n)
-            return 0;
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        n = cost.size();
 
-        // There are always two possibilities to perform at each step
-        int climbOneStep = solveWithoutMemo(cost, n, step + 1); // Is to jump and advance one step ahead from it
-        int climbTwoStep = solveWithoutMemo(cost, n, step + 2); // Is to jump and advance two steps ahead from it
+        vector<int> memory1(n, -1), memory2(n, -1);
+        
+        int startFrom0 = solveWithMemo(memory1, cost, 0);
+        int startFrom1 = solveWithMemo(memory2, cost, 1);
 
-        // As we're striving for the minimum cost hence return the minimum value
-        return cost[step] + min(climbOneStep, climbTwoStep);
+        return min(startFrom0, startFrom1);
     }
 };
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class BottomUp {
-public:
-    // #1 Method to find the minimum cost, using 1D tabulation - O(N) & O(N)
-    int minCostClimbingStairs_V1(vector<int>& cost) {
-        int n = cost.size();
+class Solution {
+    int n;
 
-        // 1D table: dp[step] represents the minimum cost needed to reach the individual step by considering the movements starting from 0th and 1th step
-        vector<int> dp(n, 0);
-        
-        dp[0] = cost[0]; // For the 0th step the minimum cost needed to reach it is the cost that the step have
-        dp[1] = cost[1]; // For the 1th step the minimum cost needed to reach it is the cost that the step have
+    int solveWith1DTable(vector<int>& cost, int startPoint) {
+        vector<int> dp(n + 2, -1);
+        dp[n] = 0;
+        dp[n + 1] = 0;
 
-        // Iterate and compute the minimum cost needed to reach the individual step
-        for(int step = 2; step < n; ++step)
-            dp[step] = cost[step] + min(dp[step - 1], dp[step - 2]);
+        for(int step = n-1; step >= startPoint; --step) {
+            int climb1Step = dp[step + 1];
+            int climb2Step = dp[step + 2];
+            dp[step] = min(climb1Step, climb2Step) + cost[step];
+        }
 
-        // Return the result value
-        return min(dp[n - 1], dp[n - 2]);
+        return dp[startPoint];
     }
 
-    // #2 Method to find the minimum cost, using constant auxiliary space - O(N) & O(1)
-    int minCostClimbingStairs_V2(vector<int>& cost) {
-        int n = cost.size();
+    int solveWithoutTable(vector<int>& cost, int startPoint) {
+        int climb1Step = 0;
+        int climb2Step = 0;
+        int minCost;
 
-        int prevPrevCost = cost[0];               // For the 0th step the minimum cost needed to reach it is the cost that the step have
-        int prevCost     = cost[1];               // For the 1th step the minimum cost needed to reach it is the cost that the step have            
-        int currCost     = min(cost[0], cost[1]); // Compute and store the minimum cost needed to reach the top (considering the first two steps only)
+        for(int step = n-1; step >= startPoint; --step) {
+            minCost    = min(climb1Step, climb2Step) + cost[step];
+            climb2Step = climb1Step;
+            climb1Step = minCost;
+        }
 
-        // Iterate and compute the minimum cost needed to reach the individual step
-        for(int step = 2; step < n; ++step)
-            currCost     = cost[step] + min(prevPrevCost, prevCost),
-            prevPrevCost = prevCost,
-            prevCost     = currCost;
+        return minCost;
+    }
 
-        // Return the result value
-        return min(currCost, prevPrevCost);
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        n = cost.size();
+
+        int startFrom0 = solveWithoutTable(cost, 0);
+        int startFrom1 = solveWithoutTable(cost, 1);
+
+        return min(startFrom0, startFrom1);
     }
 };
-
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: Array | Dynamic Programming
