6986e1a2a0d9a099bd37968d78bd09ae4cd73623
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-04-29 11:27:34 +0530

    Update 6 - Climbing Stairs.cpp

--- Dynamic Programming/1D DP/6 - Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/6 - Climbing Stairs.cpp
@@ -3,89 +3,83 @@
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
+    int solveWithoutMemo(int n) {
+        if(n <= 0)
+            return (n == 0);
+
+        int climb1Step = solveWithMemo(n - 1);
+        int climb2Step = solveWithMemo(n - 2);
+
+        return climb1Step + climb2Step;
+    }
+
+    int solveWithMemo(vector<int>& memory, int n) {
+        if(n <= 0)
+            return (n == 0);
+
+        if(memory[n] != -1)
+            return memory[n];
+
+        int climb1Step = solveWithMemo(memory, n - 1);
+        int climb2Step = solveWithMemo(memory, n - 2);
+
+        return memory[n] = (climb1Step + climb2Step);
+    }
+
 public:
-    // Method to find the total number of distinct ways, using recursion with memoization - O(N) & O(N)
-    int climbStairs(int N) {
-        vector<int> cache(N+1, -1);
-        return solveWithMemo(N, cache);
-    }
-
-private:
-    // O(2*N) & O(N+N)
-    int solveWithMemo(int N, vector<int>& cache) {
-        // Edge case: When N is less than 2 then there's just a 1 way to reach the stair
-        if(N < 2) 
-            return 1;
-
-        // Memoization table: If the current state is already computed then return the computed value
-        if(cache[N] != -1)
-            return cache[N];
-
-        // There are always two possibilities to reach the Nth stair
-        int climbOneStep = solveWithMemo(N-1, cache); // Is to jump from the immediate 1st stair which is below the Nth stair
-        int climbTwoStep = solveWithMemo(N-2, cache); // Is to jump from the immediate 2nd stair which is below the Nth stair
-
-        // Store the result value to the memoization table and then return it
-        return cache[N] = climbOneStep + climbTwoStep;
-    }
-
-    // O(2^N) & O(N)
-    int solveWithoutMemo(int N) {
-        // Edge case: When N is less than 2 then there's just 1 way to reach the stair
-        if(N < 2) 
-            return 1;
-
-        // There are always two possibilities to reach the Nth stair
-        int climbOneStep = solveWithoutMemo(N-1); // Is to jump from the immediate 1st stair which is below the Nth stair
-        int climbTwoStep = solveWithoutMemo(N-2); // Is to jump from the immediate 2nd stair which is below the Nth stair
-
-        // Return the result value
-        return climbOneStep + climbTwoStep;
+    int numWaysToReachTop(int n) {
+        vector<int> memory(n + 1, -1);
+        return solveWithoutTable(n);
     }
 };
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the total number of distinct ways, using 1D tabulation - O(N) & O(N)
-    int climbStairs_V1(int N) {
-        // Edge case: If N is negative, then no valid stairs exist, as because there can't be negative stairs
-        if(N < 0)
-            return 0;
+    int solveWithMemo(vector<int>& memory, int n) {
+        if(n <= 0)
+            return (n == 0);
 
-        // 1D table: dp[J] represents the total number of ways to reach the Jth stair 
-        vector<int> dp(N+1);
+        if(memory[n] != -1)
+            return memory[n];
 
-        dp[0] = 1; // Initially, if there is no stair that means we're already on it
-        dp[1] = 1; // If there is 1 stair that means we can reach it from the 0th stair
+        int climb1Step = solveWithMemo(memory, n - 1);
+        int climb2Step = solveWithMemo(memory, n - 2);
 
-        // Iterate and compute the number of ways to reach the Jth stair
-        for(int J=2; J<=N; J++)
-            dp[J] = dp[J-1] + dp[J-2];
-
-        // Return the number of ways to reach the Nth stair
-        return dp[N];
+        return memory[n] = (climb1Step + climb2Step);
     }
 
-    // #2 Method to find the total number of distinct ways, using constant auxiliary space - O(N) & O(1)
-    int climbStairs_V2(int N) {
-        // Edge case: If N is negative, then no valid stairs exist, as because there can't be negative stairs
-        if(N < 0)
-            return 0;
+    int solveWith1DTable(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = 1;
 
-        int prevPrevWays = 1; // Initially, if there is no stair that means we're already on it
-        int prevWays     = 1; // If there is 1 stair that means we can reach it from the 0th stair
-        int currWays     = 1; // The total number of ways to reach the 1th stair is 1
-   
-        // Iterate and compute the number of ways to reach the Jth stair
-        for(int J=2; J<=N; J++)
-            currWays = prevPrevWays + prevWays,
-            prevPrevWays = prevWays,
-            prevWays = currWays;
+        for(int i = 1; i <= n; ++i) {
+            int climb1Step = dp[i - 1];
+            int climb2Step = (i - 2 >= 0) ? dp[i - 2] : 0;
+            dp[i] = climb1Step + climb2Step;
+        }
+        
+        return dp[n];
+    }
 
-        // Return the number of ways to reach the Nth stair
-        return currWays;
+    int solveWithoutTable(int n) {
+        int climb1Step = 1;
+        int climb2Step = 0;
+        int numWays    = 0;
+
+        for(int i = 1; i <= n; ++i) {
+            numWays    = climb1Step + climb2Step;
+            climb2Step = climb1Step;
+            climb1Step = numWays;
+        }
+        
+        return numWays;
+    }
+
+public:
+    int numWaysToReachTop(int n) {
+        vector<int> memory(n + 1, -1);
+        return solveWithoutTable(n);
     }
 };
 
