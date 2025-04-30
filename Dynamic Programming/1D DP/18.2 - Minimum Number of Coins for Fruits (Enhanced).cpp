3c89c7e52c7c3ef4c91252e1754254af82b7ea64
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-01 01:06:30 +0530

    Update 18.2 - Minimum Number of Coins for Fruits (Enhanced).cpp

--- Dynamic Programming/1D DP/18.2 - Minimum Number of Coins for Fruits (Enhanced).cpp
+++ Dynamic Programming/1D DP/18.2 - Minimum Number of Coins for Fruits (Enhanced).cpp
@@ -57,7 +57,7 @@ public:
         int n = prices.size();
 
         vector<int> dp(n + 1, -1);
-        dp[n] = 0;
+        dp[n] = 0; // Init the edge case
 
         for(int index = n-1; index >= 0; --index) {
             int jumpLimit = index + 1;
