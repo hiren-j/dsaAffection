5b6cb0389732c9b9fefe005da25ba6ffda32f954
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-01 12:01:41 +0530

    Update 21 - Maximum Number Of Jumps To Reach The Last Index.cpp

--- Dynamic Programming/1D DP/21 - Maximum Number Of Jumps To Reach The Last Index.cpp
+++ Dynamic Programming/1D DP/21 - Maximum Number Of Jumps To Reach The Last Index.cpp
@@ -3,64 +3,57 @@
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-public:
-    // Method to find the maximum number of jumps, using recursion with memoization - O(N*N) & O(N)
-    int maxJumpsToReachLastIndex(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> memory(n, -1);
-        int maxJumps = solveWithMemo(memory, nums, n, target, 0);
-        return (maxJumps == INT_MIN) ? -1 : maxJumps;
-    }
+    int n;
 
-private:
-    // O(N*N) & O(N+N)
-    int solveWithMemo(vector<int>& memory, vector<int>& nums, int n, int target, int startIndex) {
-        // Edge case: If you reached the last index then return 0 as a valid indication of it
-        if(startIndex == n-1)
+    int solveWithoutMemo(vector<int>& nums, int target, int start) {
+        if(start == n-1)
             return 0;
-
-        // Memoization table: If the current state is already computed, then return the computed value
-        if(memory[startIndex] != -1)
-            return memory[startIndex];
         
-        // Stores the result value
         int maxJumps = INT_MIN;
 
-        // Explore all the possibilities of jumping from the current index and update the result by the maximum value
-        for(int jump = startIndex+1; jump < n; ++jump) {
-            if((-target <= nums[jump] - nums[startIndex]) && (nums[jump] - nums[startIndex] <= target)) {
-                int nextJumps = solveWithMemo(memory, nums, n, target, jump);
+        for(int jumpIdx = start+1; jumpIdx < n; ++jumpIdx) {
+            int delta = nums[jumpIdx] - nums[start];
+
+            if(-target <= delta && delta <= target) {
+                int nextJumps = solveWithoutMemo(nums, target, jumpIdx);
                 if(nextJumps != INT_MIN) {
-                    maxJumps = max(maxJumps, 1 + nextJumps);
-                } 
+                    maxJumps = max(maxJumps, nextJumps + 1);
+                }
             }
         }
 
-        // Store the result value to the memoization table and then return it
-        return memory[startIndex] = maxJumps;
+        return maxJumps;
     }
 
-    // O(N^N) & O(N)
-    int solveWithoutMemo(vector<int>& nums, int n, int target, int startIndex) {
-        // Edge case: If you reached the last index then return 0 as a valid indication of it
-        if(startIndex == n-1)
+    int solveWithMemo(vector<int>& dp, vector<int>& nums, int target, int start) {
+        if(start == n-1)
             return 0;
 
-        // Stores the result value
+        if(dp[start] != -1)
+            return dp[start];
+        
         int maxJumps = INT_MIN;
 
-        // Explore all the possibilities of jumping from the current index and update the result by the maximum value
-        for(int jump = startIndex+1; jump < n; ++jump) {
-            if((-target <= nums[jump] - nums[startIndex]) && (nums[jump] - nums[startIndex] <= target)) {
-                int nextJumps = solveWithoutMemo(nums, n, target, jump);
+        for(int jumpIdx = start+1; jumpIdx < n; ++jumpIdx) {
+            int delta = nums[jumpIdx] - nums[start];
+            
+            if(-target <= delta && delta <= target) {
+                int nextJumps = solveWithMemo(dp, nums, target, jumpIdx);
                 if(nextJumps != INT_MIN) {
-                    maxJumps = max(maxJumps, 1 + nextJumps);
-                } 
+                    maxJumps = max(maxJumps, nextJumps + 1);
+                }
             }
         }
 
-        // Return the result value
-        return maxJumps;
+        return dp[start] = maxJumps;
+    }
+
+public:
+    int maxJumpsToLastIndex(vector<int>& nums, int target) {
+        n = nums.size();
+        vector<int> dp(n-1, -1);
+        int maxJumps = solveWithMemo(dp, nums, target, 0);
+        return (maxJumps == INT_MIN) ? -1 : maxJumps;
     }
 };
 
@@ -68,35 +61,30 @@ private:
     
 class BottomUp {
 public:
-    // Method to find the maximum number of jumps, using 1D tabulation - O(N*N) & O(N)
-    int maxJumpsToReachLastIndex(vector<int>& nums, int target) {
+    int maxJumpsToLastIndex(vector<int>& nums, int target) {
         int n = nums.size();
+        
+        vector<int> dp(n, -1);
+        dp[n - 1] = 0;
 
-        // 1D table: dp[index] represents the maximum number of jumps required to reach the individual index
-        vector<int> dp(n, INT_MIN);
-
-        // Initialize the edge case: If you reached the last index then return 0 as a valid indication of it
-        dp[n-1] = 0;
-
-        // Fill the rest of the table
-        for(int startIndex = n-2; startIndex >= 0; --startIndex) {
+        for(int start = n-2; start >= 0; --start) {
             int maxJumps = INT_MIN;
-            
-            for(int jump = startIndex+1; jump < n; ++jump) {
-                if((-target <= nums[jump] - nums[startIndex]) && (nums[jump] - nums[startIndex] <= target)) {
-                    int nextJumps = dp[jump];
+
+            for(int jumpIdx = start+1; jumpIdx < n; ++jumpIdx) {
+                int delta = nums[jumpIdx] - nums[start];
+
+                if(-target <= delta && delta <= target) {
+                    int nextJumps = dp[jumpIdx];
                     if(nextJumps != INT_MIN) {
-                        maxJumps = max(maxJumps, 1 + nextJumps);
-                    } 
+                        maxJumps = max(maxJumps, nextJumps + 1);
+                    }
                 }
             }
-            
-            dp[startIndex] = maxJumps;
+
+            dp[start] = maxJumps;
         }
 
         int maxJumps = dp[0];
-
-        // Return the result value
         return (maxJumps == INT_MIN) ? -1 : maxJumps;
     }
 };
