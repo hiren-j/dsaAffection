1e2c53db777289e764f87d926feedca8247c0d4a
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-02 20:05:56 +0530

    Update 27 - Removing Digits.cpp

--- Dynamic Programming/1D DP/27 - Removing Digits.cpp
+++ Dynamic Programming/1D DP/27 - Removing Digits.cpp
@@ -3,65 +3,49 @@
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-public:
-    // Method to count the total steps required to reduce n to 0, using recursion with memoization - O(NLogN) & O(N)
-    int minStepsToMakeZero(int n) {
-        vector<int> dp(n + 1, -1);
-        return solveWithMemo(dp, n);
-    }
-
-private:
-    // O(LogN*N) & O(N+N)
-    int solveWithMemo(vector<int>& dp, int n) {
-        // Edge case: If n is 0, then no steps are required to make n equal to 0
+    int solveWithoutMemo(int n) {
         if(n == 0)
             return 0;
+        
+        int minSteps = INT_MAX;
+        int num = n;
 
-        // Edge case: If n is negative, then it is not possible to make n equal to 0
-        if(n < 0)
-            return INT_MAX;
+        while(num) {
+            int digit = num % 10;
+            num /= 10;
+            if(digit == 0) continue;
+            int nextSteps = solveWithoutMemo(n - digit);
+            minSteps = min(minSteps, nextSteps + 1);
+        }
 
-        // Memoization table: If the current state is already computed then return the computed value
+        return minSteps;
+    }
+
+    int solveWithMemo(vector<int>& dp, int n) {
+        if(n == 0)
+            return 0;
+        
         if(dp[n] != -1)
             return dp[n];
 
-        int curr = n, minSteps = INT_MAX;
+        int minSteps = INT_MAX;
+        int num = n;
 
-        // Subtract the digits one by one and find the minimum steps to make number equal to 0 and then update the result by the minimum value
-        while(curr) {
-            int digit = curr % 10;
-            curr /= 10;
+        while(num) {
+            int digit = num % 10;
+            num /= 10;
             if(digit == 0) continue;
             int nextSteps = solveWithMemo(dp, n - digit);
-            if(nextSteps != INT_MAX) minSteps = min(minSteps, 1 + nextSteps);
+            minSteps = min(minSteps, nextSteps + 1);
         }
 
-        // Store the result value to the memoization table and then return it
-        return dp[n] = minSteps; 
+        return dp[n] = minSteps;
     }
 
-    // O(LogN^N) & O(N)
-    int solveWithoutMemo(int n) {
-        // Edge case: If n is 0, then no steps are required to make n equal to 0
-        if(n == 0)
-            return 0;
-
-        // Edge case: If n is negative, then it is not possible to make n equal to 0
-        if(n < 0)
-            return INT_MAX;
-
-        int curr = n, minSteps = INT_MAX;
-
-        // Subtract the digits one by one and find the minimum steps to make number equal to 0 and then update the result by the minimum value
-        while(curr) {
-            int digit = curr % 10;
-            curr /= 10;
-            if(digit == 0) continue;
-            int nextSteps = solveWithoutMemo(n - digit);
-            if(nextSteps != INT_MAX) minSteps = min(minSteps, 1 + nextSteps);
-        }
-
-        return minSteps; 
+public:
+    int minStepsToMakeZero(int n) {
+        vector<int> dp(n + 1, -1);
+        return solveWithMemo(dp, n);
     }
 };
 
@@ -69,31 +53,26 @@ private:
 
 class BottomUp {
 public:
-    // Method to count the total steps required to reduce n to 0, using 1D tabulation - O(NLogN) & O(N)
-    int minStepsToMakeZero(int n) {
-        // 1D DP table: dp[i] represents the total steps required to reduce i to 0
-        vector<int> dp(n + 1, INT_MAX);
-
-        // Initialize the edge case
+    int minStepsToMakeZero(int given_n) {
+        vector<int> dp(given_n + 1, -1);
         dp[0] = 0;
 
-        // Fill the rest of the table
-        for(int number = 1; number <= n; ++number) {
-            int curr = number, minSteps = INT_MAX;
+        for(int n = 1; n <= given_n; ++n) {
+            int minSteps = INT_MAX;
+            int num = n;
 
-            while(curr) {
-                int digit = curr % 10;
-                curr /= 10;
+            while(num) {
+                int digit = num % 10;
+                num /= 10;
                 if(digit == 0) continue;
-                int nextSteps = (number - digit >= 0) ? dp[number - digit] : INT_MAX;
-                if(nextSteps != INT_MAX) minSteps = min(minSteps, 1 + nextSteps);
+                int nextSteps = dp[n - digit];
+                minSteps = min(minSteps, nextSteps + 1);
             }
 
-            dp[number] = minSteps; 
+            dp[n] = minSteps;
         }
 
-        // Return the result value
-        return dp[n];
+        return dp[given_n];
     }
 };
 
