bb23256b92951caff758e0735c468b8346a548b8
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-08 11:49:29 +0530

    Update 5 - Unique Paths II.cpp

--- Dynamic Programming/Multi Dimensional DP/5 - Unique Paths II.cpp
+++ Dynamic Programming/Multi Dimensional DP/5 - Unique Paths II.cpp
@@ -3,124 +3,120 @@
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-public:
-    // Method to find the number of possible unique paths, using recursion with memoization - O(N*M) & O(N*M)
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int N = grid.size(), M = grid[0].size();
-        if(grid[N-1][M-1] == 1) return 0;
-        vector<vector<int>> memory(N, vector<int>(M, -1));
-        return solveWithMemo(memory, grid, N, M, 0, 0);
-    }
+    int M, N;
 
-private:
-    // O(2*N*M) & O(N*M + N+M)
-    int solveWithMemo(vector<vector<int>>& memory, vector<vector<int>>& grid, int N, int M, int R, int C) {
-        // Edge case: If all the cells are exhausted or the cell contains an obstacle then you've no valid way
-        if(R == N || C == M || grid[R][C] == 1)
+    int solveWithoutMemo(vector<vector<int>>& grid, int R, int C) {
+        if(R == M || C == N || grid[R][C] == 1)
             return 0;
 
-        // Edge case: If reached the bottom up corner then you've one valid way
-        if(R == N-1 && C == M-1)
-            return 1;   
+        if(R == M-1 && C == N-1)
+            return 1;
         
-        // Memoization table: If the current state is already computed then return the computed value 
-        if(memory[R][C] != -1)
-            return memory[R][C];
+        int moveRight = solveWithoutMemo(grid, R, C+1);
+        int moveDown  = solveWithoutMemo(grid, R+1, C);
 
-        // There are always two possibilities to perform at each cell
-        int moveRight = solveWithMemo(memory, grid, N, M, R, C+1); // Is to move right
-        int moveDown  = solveWithMemo(memory, grid, N, M, R+1, C); // Is to move down
-
-        // Store the result value to the memoization table and then return it
-        return memory[R][C] = moveRight + moveDown;
-    }
-
-    // O(2^(N*M)) & O(N+M)
-    int solveWithoutMemo(vector<vector<int>>& grid, int N, int M, int R, int C) {
-        // Edge case: If all the cells are exhausted or the cell contains an obstacle then you've no valid way
-        if(R == N || C == M || grid[R][C] == 1)
-            return 0;
-
-        // Edge case: If reached the bottom up corner then you've one valid way
-        if(R == N-1 && C == M-1)
-            return 1;   
-        
-        // There are always two possibilities to perform at each cell
-        int moveRight = solveWithoutMemo(grid, N, M, R, C+1); // Is to move right
-        int moveDown  = solveWithoutMemo(grid, N, M, R+1, C); // Is to move down
-
-        // Return the result value
         return moveRight + moveDown;
+    }
+
+    int solveWithMemo(vector<vector<int>>& dp, vector<vector<int>>& grid, int R, int C) {
+        if(R == M || C == N || grid[R][C] == 1)
+            return 0;
+
+        if(R == M-1 && C == N-1)
+            return 1;
+
+        if(dp[R][C] != -1)
+            return dp[R][C];
+        
+        int moveRight = solveWithMemo(dp, grid, R, C+1);
+        int moveDown  = solveWithMemo(dp, grid, R+1, C);
+
+        return dp[R][C] = moveRight + moveDown;
+    }
+
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0] == 1 || grid[M-1][N-1])
+            return 0;
+        M = grid.size(), N = grid[0].size();
+        vector<vector<int>> dp(M, vector<int>(N, -1));
+        return solveWithMemo(dp, grid, 0, 0);
     }
 };
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the number of possible unique paths, using 2D tabulation - O(N*M) & O(N*M)
-    int uniquePathsWithObstacles_V1(vector<vector<int>>& grid) {
-        int N = grid.size(), M = grid[0].size();
+    int M, N;
 
-        // Early exit: If the destination cell contains an obstacle then you can't reach it
-        if(grid[N-1][M-1] == 1)
-            return 0;
+    int solveWith2DTable(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(M, vector<int>(N, -1));
+        dp[M-1][N-1] = 1;
 
-        // 2D DP table
-        vector<vector<long>> dp(N+1, vector<long>(M+1, 0));
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if(R == M-1 && C == N-1) {
+                    continue;
+                } else if(grid[R][C] == 1) {
+                    dp[R][C] = 0;
+                } else {
+                    long moveRight = (C+1 < N) ? dp[R][C+1] : 0;
+                    long moveDown  = (R+1 < M) ? dp[R+1][C] : 0;
+                    dp[R][C] = moveRight + moveDown;
+                }
+            }
+        }
 
-        // Initialize the edge case: If all the cells are exhausted or the cell contains an obstacle then you've no valid way
-        dp[N-1][M-1] = 1;
+        return dp[0][0];
+    }
 
-        // Fill the rest of the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = M-1; C >= 0; --C) {
-                if((R == N-1 && C == M-1) || grid[R][C] == 1)
-                    continue;   
-                long moveRight = dp[R][C+1]; 
-                long moveDown  = dp[R+1][C]; 
+    int solveWith2DEnhanced(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(M+1, vector<int>(N+1, 0));
+        dp[M-1][N-1] = 1;
+
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if((R == M-1 && C == N-1) || grid[R][C] == 1) 
+                    continue;
+                long moveRight = dp[R][C+1];
+                long moveDown  = dp[R+1][C];
                 dp[R][C] = moveRight + moveDown;
             }
         }
 
-        // Return the result value
         return dp[0][0];
     }
+    
+    int solveWith1DTable(vector<vector<int>>& grid) {
+        vector<int> nextRow(N+1, 0), idealRow(N+1, 0);
+        idealRow[N-1] = 1;
 
-    // #2 Method to find the number of possible unique paths, using 1D tabulation - O(N*M) & O(M)
-    int uniquePathsWithObstacles_V2(vector<vector<int>>& grid) {
-        int N = grid.size(), M = grid[0].size();
-
-        // Early exit: If the destination cell contains an obstacle then you can't reach it
-        if(grid[N-1][M-1] == 1)
-            return 0;
-
-        // 1D DP tables
-        vector<long> nextRow(M+1, 0), currRow(M+1, 0);
-
-        // Initialize the edge case: If all the cells are exhausted or the cell contains an obstacle then you've no valid way
-        currRow[M-1] = 1;
-
-        // Fill the rest of the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = M-1; C >= 0; --C) {
-                if(R == N-1 && C == M-1) {   
-                    continue;   
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if(R == M-1 && C == N-1) {
+                    continue;
                 }
                 else if(grid[R][C] == 1) {
-                    currRow[C] = 0;
+                    idealRow[C] = 0;   
                 }
                 else {
-                    long moveRight = currRow[C+1]; 
-                    long moveDown  = nextRow[C]; 
-                    currRow[C] = moveRight + moveDown;
+                    long moveRight = idealRow[C+1];
+                    long moveDown  = nextRow[C];
+                    idealRow[C] = moveRight + moveDown;
                 }
             }
-            nextRow = currRow;
+            nextRow = idealRow;
         }
 
-        // Return the result value
-        return currRow[0];
+        return nextRow[0];
+    }
+
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0] == 1 || grid[M-1][N-1]) 
+            return 0;
+        M = grid.size(), N = grid[0].size();
+        return solveWith2DTable(grid);
     }
 };
 
