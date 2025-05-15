d1bdf9b7df57922d4bf3c5e4818c2ee05004b9be
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-15 17:38:48 +0530

    Update 10 - Check if There Is a Valid Parentheses String Path.cpp

--- Dynamic Programming/Multi Dimensional DP/10 - Check if There Is a Valid Parentheses String Path.cpp
+++ Dynamic Programming/Multi Dimensional DP/10 - Check if There Is a Valid Parentheses String Path.cpp
@@ -5,6 +5,29 @@
 class TopDown {
     int M, N, maxStackLen;
 
+    // O(2^(M*N)) & O(M+N)
+    bool solveWithoutMemo(vector<vector<char>>& grid, int R, int C, int stackLen) {
+        if(R == M || C == N)
+            return false;
+
+        // If it's a opening parentheses then assume we're pushing it to the stack hence increase the stack length by 1, else assuming poping from stack so reduce length by 1 
+        (grid[R][C] == '(') ? stackLen++ : stackLen--;
+
+        // Edge case: If the stack length becomes negative then there doesn't exist a valid path
+        if(stackLen < 0)
+            return false;
+
+        // Edge case: If you reached the bottom right corner and the stack becomes empty then we've found a valid path
+        if(R == M-1 && C == N-1)
+            return stackLen == 0;
+
+        bool moveRight = solveWithoutMemo(grid, R, C+1, stackLen);
+        bool moveDown  = solveWithoutMemo(grid, R+1, C, stackLen);
+
+        return moveRight || moveDown;
+    }
+
+    // O(2*M*N*MSL) & O(M*N*MSL + M+N) : Where MSL = maxStackLen
     bool solveWithMemo(vector<vector<vector<int>>>& dp, vector<vector<char>>& grid, int R, int C, int stackLen) {
         if(R == M || C == N)
             return false;
@@ -30,7 +53,7 @@ class TopDown {
     }
 
 public:
-    // Method to check whether there exists a valid parentheses string path, using recursion with memoization - O() & O()
+    // Method to check whether there exists a valid parentheses string path, using recursion with memoization - O(M*N*MSL) & O(M*N*MSL) : Where MSL = maxStackLen
     bool hasValidPath(vector<vector<char>>& grid) {
         M = grid.size(), N = grid[0].size(), maxStackLen = M+N;     
         if(grid[M-1][N-1] == '(') 
@@ -43,6 +66,7 @@ public:
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
+    // O(M*N*MSL) & O(M*N*MSL)
     int solveWith3DTable(vector<vector<char>>& grid) {
         vector<vector<vector<bool>>> dp(M+1, vector<vector<bool>>(N+1, vector<bool>(maxStackLen, false)));
 
@@ -67,6 +91,7 @@ class BottomUp {
         return dp[0][0][0];
     }
 
+    // O(M*N*MSL) & O(2*N*MSL)
     int solveWith2DTable(vector<vector<char>>& grid) {
         vector<vector<bool>> nextRow(N+1, vector<bool>(maxStackLen, false));
         vector<vector<bool>> idealRow(N+1, vector<bool>(maxStackLen, false));
@@ -100,7 +125,7 @@ public:
         M = grid.size(), N = grid[0].size(), maxStackLen = M+N;     
         if(grid[M-1][N-1] == '(') 
             return false;
-        return solveWith3DTable(grid);
+        return solveWith2DTable(grid);
     }
 };  
 
