78d44af49269aead58af245e577c8e8c32c0e539
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-25 13:54:56 +0530

    Update 20 - Student Attendance Record II.cpp

--- Dynamic Programming/Multi Dimensional DP/20 - Student Attendance Record II.cpp
+++ Dynamic Programming/Multi Dimensional DP/20 - Student Attendance Record II.cpp
@@ -4,54 +4,43 @@
 
 class TopDown {
     const int MOD = 1e9+7;
+    const int maxLate = 3, maxAbsences = 2;
+
+    int solveWithoutMemo(int N, int consecutiveLate, int absences) {
+        if(consecutiveLate == maxLate || absences == maxAbsences)
+            return 0;
+
+        if(N == 0)
+            return 1;
+
+        int P = solveWithoutMemo(N - 1, 0, absences);
+        int A = solveWithoutMemo(N - 1, 0, absences + 1);
+        int L = solveWithoutMemo(N - 1, consecutiveLate + 1, absences);
+
+        return ((P + A) % MOD + L) % MOD;
+    }
+
+    int solveWithMemo(vector<vector<vector<int>>>& memory, int N, int consecutiveLate, int absences) {
+        if(consecutiveLate == maxLate || absences == maxAbsences)
+            return 0;
+
+        if(N == 0)
+            return 1;
+
+        if(memory[N][consecutiveLate][absences] != -1)
+            return memory[N][consecutiveLate][absences];
+
+        int P = solveWithMemo(memory, N - 1, 0, absences);
+        int A = solveWithMemo(memory, N - 1, 0, absences + 1);
+        int L = solveWithMemo(memory, N - 1, consecutiveLate + 1, absences);
+
+        return memory[N][consecutiveLate][absences] = ((P + A) % MOD + L) % MOD;
+    }
 
 public:
-    // Method to find the total number of specified attendance records of length N, using recursion with memoization - O(N) & O(N)
     int checkRecord(int N) {
-        vector<vector<vector<int>>> memory(N + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> memory(N + 1, vector<vector<int>>(maxLate, vector<int>(maxAbsences, -1)));
         return solveWithMemo(memory, N, 0, 0);
-    }
-
-private:
-    // O(3*N*2*3) & O(N*2*3 + N)
-    int solveWithMemo(vector<vector<vector<int>>>& memory, int N, int absences, int consecutiveLate) {
-        // Edge case: If the student goes absent for 2 days or goes late for consecutively 3 days then there's no valid way
-        if(absences == 2 || consecutiveLate == 3)
-            return 0;
-
-        // Edge case: If it's possible to create a valid attendence record of length N then there's 1 valid way
-        if(N == 0)
-            return 1;
-        
-        // Memoization table: If the current state is already computed then return the computed value
-        if(memory[N][absences][consecutiveLate] != -1)
-            return memory[N][absences][consecutiveLate];
-
-        // There are three possibilities which the student can perform at each day
-        int A = solveWithMemo(memory, N - 1, absences + 1, 0) % MOD;               // Is to go absent 
-        int L = solveWithMemo(memory, N - 1, absences, consecutiveLate + 1) % MOD; // Is to go late
-        int P = solveWithMemo(memory, N - 1, absences, 0) % MOD;                   // Is to present 
-
-        // Store the result value to the memoization table and then return it
-        return memory[N][absences][consecutiveLate] = ((A + L) % MOD + P) % MOD;
-    }
-
-    // O(3^N) & O(N)
-    int solveWithoutMemo(int N, int absences, int consecutiveLate) {
-        // Edge case: If the student goes absent for 2 days or goes late for consecutively 3 days then there's no valid way
-        if(absences == 2 || consecutiveLate == 3)
-            return 0;
-
-        // Edge case: If it's possible to create a valid attendence record of length N then there's 1 valid way
-        if(N == 0)
-            return 1;
-        
-        // There are three possibilities which the student can perform at each day
-        int A = solveWithoutMemo(N - 1, absences + 1, 0) % MOD;               // Is to go absent 
-        int L = solveWithoutMemo(N - 1, absences, consecutiveLate + 1) % MOD; // Is to go late
-        int P = solveWithoutMemo(N - 1, absences, 0) % MOD;                   // Is to present 
-
-        return ((A + L) % MOD + P) % MOD;
     }
 };
 
@@ -59,63 +48,110 @@ private:
 
 class BottomUp {
     const int MOD = 1e9+7;
+    const int maxLate = 3, maxAbsences = 2;
 
-public:
-    // #1 Method to find the total number of specified attendance records of length N, using 3D tabulation - O(N*2*3) & O(N*3*4)
-    int checkRecord_V1(int N) {
-        // 3D DP table
-        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(3, vector<int>(4, 0)));
+    int solveWithoutMemo(int N, int consecutiveLate, int absences) {
+        if(consecutiveLate == maxLate || absences == maxAbsences)
+            return 0;
 
-        // Initialize the first edge case: If it's possible to create a valid attendence record of length N then there's 1 valid way
-        for(int absences = 0; absences < 2; ++absences)
-            for(int consecutiveLate = 0; consecutiveLate < 3; ++consecutiveLate)
-                dp[0][absences][consecutiveLate] = 1;
+        if(N == 0)
+            return 1;
 
-        // Fill the rest of the table
-        for(int length = 1; length <= N; ++length) {
-            for(int absences = 1; absences >= 0; --absences) {
-                for(int consecutiveLate = 2; consecutiveLate >= 0; --consecutiveLate) {
-                    int A = dp[length - 1][absences + 1][0] % MOD; 
-                    int L = dp[length - 1][absences][consecutiveLate + 1] % MOD; 
-                    int P = dp[length - 1][absences][0] % MOD;                   
-                    dp[length][absences][consecutiveLate] = ((A + L) % MOD + P) % MOD;   
+        int P = solveWithoutMemo(N - 1, 0, absences);
+        int A = solveWithoutMemo(N - 1, 0, absences + 1);
+        int L = solveWithoutMemo(N - 1, consecutiveLate + 1, absences);
+
+        return ((P + A) % MOD + L) % MOD;
+    }
+
+    int solveWithMemo(vector<vector<vector<int>>>& memory, int N, int consecutiveLate, int absences) {
+        if(consecutiveLate == maxLate || absences == maxAbsences)
+            return 0;
+
+        if(N == 0)
+            return 1;
+
+        if(memory[N][consecutiveLate][absences] != -1)
+            return memory[N][consecutiveLate][absences];
+
+        int P = solveWithMemo(memory, N - 1, 0, absences);
+        int A = solveWithMemo(memory, N - 1, 0, absences + 1);
+        int L = solveWithMemo(memory, N - 1, consecutiveLate + 1, absences);
+
+        return memory[N][consecutiveLate][absences] = ((P + A) % MOD + L) % MOD;
+    }
+
+    int solveWith3DTable(int N) {
+        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(maxLate, vector<int>(maxAbsences, -1)));
+        
+        for(int consecutiveLate = 0; consecutiveLate < maxLate; ++consecutiveLate)
+            for(int absences = 0; absences < maxAbsences; ++absences)
+                dp[0][consecutiveLate][absences] = 1;
+        
+        for(int day = 1; day <= N; ++day) {
+            for(int consecutiveLate = maxLate-1; consecutiveLate >= 0; --consecutiveLate) {
+                for(int absences = maxAbsences-1; absences >= 0; --absences) {
+                    int P = dp[day - 1][0][absences];
+                    int A = (absences + 1 < maxAbsences) ? dp[day - 1][0][absences + 1] : 0;
+                    int L = (consecutiveLate + 1 < maxLate) ? dp[day - 1][consecutiveLate + 1][absences] : 0;
+                    dp[day][consecutiveLate][absences] = ((P + A) % MOD + L) % MOD;
                 }
             }
         }
-
-        // Return the result value
+        
         return dp[N][0][0];
     }
 
-    // #2 Method to find the total number of specified attendance records of length N, using 2D tabulation - O(N*2*3) & O(2*3*4)
-    int checkRecord_V2(int N) {
-        // 2D DP tables
-        vector<vector<int>> prevRow(3, vector<int>(4, 0)), currRow(3, vector<int>(4, 0));
-
-        // Initialize the first edge case: If it's possible to create a valid attendence record of length N then there's 1 valid way
-        for(int absences = 0; absences < 2; ++absences)
-            for(int consecutiveLate = 0; consecutiveLate < 3; ++consecutiveLate)
-                prevRow[absences][consecutiveLate] = 1;
-
-        // Fill the rest of the table
-        for(int length = 1; length <= N; ++length) {
-            for(int absences = 1; absences >= 0; --absences) {
-                for(int consecutiveLate = 2; consecutiveLate >= 0; --consecutiveLate) {
-                    int A = prevRow[absences + 1][0] % MOD; 
-                    int L = prevRow[absences][consecutiveLate + 1] % MOD; 
-                    int P = prevRow[absences][0] % MOD;                   
-                    currRow[absences][consecutiveLate] = ((A + L) % MOD + P) % MOD;   
+    int solveWith3DEnhanced(int N) {
+        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(maxLate + 1, vector<int>(maxAbsences + 1, 0)));
+        
+        for(int consecutiveLate = 0; consecutiveLate < maxLate; ++consecutiveLate)
+            for(int absences = 0; absences < maxAbsences; ++absences)
+                dp[0][consecutiveLate][absences] = 1;
+        
+        for(int day = 1; day <= N; ++day) {
+            for(int consecutiveLate = maxLate-1; consecutiveLate >= 0; --consecutiveLate) {
+                for(int absences = maxAbsences-1; absences >= 0; --absences) {
+                    int P = dp[day - 1][0][absences];
+                    int A = dp[day - 1][0][absences + 1];
+                    int L = dp[day - 1][consecutiveLate + 1][absences];
+                    dp[day][consecutiveLate][absences] = ((P + A) % MOD + L) % MOD;
                 }
             }
-            prevRow = currRow;
         }
+        
+        return dp[N][0][0];
+    }
 
-        // Return the result value
+    int solveWith2DTable(int N) {
+        vector<vector<int>> prevRow(maxLate + 1, vector<int>(maxAbsences + 1, 0)), idealRow(maxLate + 1, vector<int>(maxAbsences + 1, 0));
+
+        for(int consecutiveLate = 0; consecutiveLate < maxLate; ++consecutiveLate)
+            for(int absences = 0; absences < maxAbsences; ++absences)
+                prevRow[consecutiveLate][absences] = 1;
+        
+        for(int day = 1; day <= N; ++day) {
+            for(int consecutiveLate = maxLate-1; consecutiveLate >= 0; --consecutiveLate) {
+                for(int absences = maxAbsences-1; absences >= 0; --absences) {
+                    int P = prevRow[0][absences];
+                    int A = prevRow[0][absences + 1];
+                    int L = prevRow[consecutiveLate + 1][absences];
+                    idealRow[consecutiveLate][absences] = ((P + A) % MOD + L) % MOD;
+                }
+            }
+            prevRow = idealRow;
+        }
+        
         return prevRow[0][0];
+    }
+
+public:
+    int checkRecord(int N) {
+        return solveWith2DTable(N);
     }
 };
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
-Topics: Dynamic Programming
+Topics: Combinatorics | Dynamic Programming
 Link  : https://leetcode.com/problems/student-attendance-record-ii/description/?envType=daily-question&envId=2024-05-26
