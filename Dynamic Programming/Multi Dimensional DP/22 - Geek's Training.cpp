7db1e93b1b83186db5c970840cd81975089c8c51
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-05-29 02:28:27 +0530

    Update 22 - Geek's Training.cpp

--- Dynamic Programming/Multi Dimensional DP/22 - Geek's Training.cpp
+++ Dynamic Programming/Multi Dimensional DP/22 - Geek's Training.cpp
@@ -41,7 +41,7 @@ public:
     int gainMaximumPoints(vector<vector<int>>& points) {
         N = points.size();
         vector<vector<int>> dp(N, vector<int>(4, -1));
-        return solveWithMemo(dp, points, 0, 3); // I passed 3 as skip-column but you can pass any value just avoid the columns indeces 0, 1, 2
+        return solveWithMemo(dp, points, 0, 3); // I passed 3 as previously-chosen-column but you can pass any value just avoid the columns indeces 0, 1, 2
     }
 };
 
