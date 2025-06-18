6f75790a1ea132b016a9c776b162a0d929977f30
Hiren Joshi <155013795+hiren-j@users.noreply.github.com>
2025-06-18 21:51:34 +0530

    Update 26 - Minimum Path Cost In A Grid.cpp

--- Dynamic Programming/Multi Dimensional DP/26 - Minimum Path Cost In A Grid.cpp
+++ Dynamic Programming/Multi Dimensional DP/26 - Minimum Path Cost In A Grid.cpp
@@ -3,120 +3,135 @@
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-    int N, M;
+    int M, N;
 
-    // O(M^(N*M)) & O(N)
+    // O(N^(M*N)) & O(M)
     int solveWithoutMemo(vector<vector<int>>& grid, vector<vector<int>>& moveCost, int R, int prevCol) {
-        // Edge case: If all the rows are exhausted then there's no more cost to pick 
-        if(R == N)
+        if(R == M)
             return 0;
-
-        // Stores the result value
-        int minPathCost = INT_MAX;
-
-        // Explore all the paths from the cell and update the result value by the minimum cost
-        for(int C = 0; C < M; ++C) {
-            int currPathCost = grid[R][C] + solveWithoutMemo(grid, moveCost, R+1, C);
-            if(prevCol != -1) {
-                currPathCost += moveCost[grid[R-1][prevCol]][C];
-            }
-            minPathCost = min(minPathCost, currPathCost);
-        }   
-
-        // Return the result value
-        return minPathCost;
-    }
-
-    // O(M*N*M) & O(N*M + N)
-    int solveWithMemo(vector<vector<int>>& memory, vector<vector<int>>& grid, vector<vector<int>>& moveCost, int R, int prevCol) {
-        // Edge case: If all the rows are exhausted then there's no more cost to pick
-        if(R == N)
-            return 0;
-
-        // Memoization table: If the current state is already computed then return the computed value
-        if(memory[R][prevCol + 1] != -1)
-            return memory[R][prevCol + 1];
         
-        // Stores the result value
         int minPathCost = INT_MAX;
 
-        // Explore all the paths from the cell and update the result value by the minimum cost
-        for(int C = 0; C < M; ++C) {
-            int currPathCost = grid[R][C] + solveWithMemo(memory, grid, moveCost, R+1, C);
-            if(prevCol != -1) {
+        for(int C = 0; C < N; ++C) {
+            int currPathCost = grid[R][C] + solveWithoutMemo(grid, moveCost, R+1, C);
+            if(prevCol != N) {
                 currPathCost += moveCost[grid[R-1][prevCol]][C];
             }
             minPathCost = min(minPathCost, currPathCost);
         }
 
-        // Store the result value to the memoization table and then return it
-        return memory[R][prevCol + 1] = minPathCost;
+        return minPathCost;
+    }
+
+    // O(N*M*N) & O(M*N + M)
+    int solveWithMemo(vector<vector<int>>& dp, vector<vector<int>>& grid, vector<vector<int>>& moveCost, int R, int prevCol) {
+        if(R == M)
+            return 0;
+
+        if(dp[R][prevCol] != -1)
+            return dp[R][prevCol];
+        
+        int minPathCost = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathCost = grid[R][C] + solveWithMemo(dp, grid, moveCost, R+1, C);
+            if(prevCol != N) {
+                currPathCost += moveCost[grid[R-1][prevCol]][C];
+            }
+            minPathCost = min(minPathCost, currPathCost);
+        }
+
+        return dp[R][prevCol] = minPathCost;
     }
 
 public:
-    // Method to find the minimum cost, using recursion with memoization - O(N*M*M) & O(N*M)
+    // Method to find the minimum cost, using recursion with memoization - O(M*N*N) & O(M*N)
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-        N = grid.size(), M = grid[0].size();
-        vector<vector<int>> memory(N, vector<int>(M+1, -1));
-        return solveWithMemo(memory, grid, moveCost, 0, -1);
+        M = grid.size(), N = grid[0].size();
+        vector<vector<int>> dp(M, vector<int>(N+1, -1));
+        return solveWithMemo(dp, grid, moveCost, 0, N);
     }
 };
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the minimum cost, using 2D tabulation - O(N*M*M) & O(N*M)
-    int minPathCost_V1(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-        int N = grid.size(), M = grid[0].size();
+    int M, N;
 
-        // 2D DP table
-        vector<vector<int>> dp(N+1, vector<int>(M+1, 0));
+    // O(M*N*N) & O(M*N)
+    int solveWith2DTable(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
+        vector<vector<int>> dp(M, vector<int>(N+1, -1));
 
-        // Fill the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int prevCol = M-1; prevCol >= -1; --prevCol) {
+        for(int R = M-1; R >= 0; --R) {
+            for(int prevCol = 0; prevCol <= N; ++prevCol) {
                 int minPathCost = INT_MAX;
-                for(int C = 0; C < M; ++C) {
-                    int currPathCost = grid[R][C] + dp[R+1][C+1];
-                    if(prevCol != -1 && R-1 >= 0) {
+
+                for(int C = 0; C < N; ++C) {
+                    int currPathCost = grid[R][C] + (R+1 < M ? dp[R+1][C] : 0);
+                    if(prevCol != N && R-1 >= 0) {
                         currPathCost += moveCost[grid[R-1][prevCol]][C];
                     }
                     minPathCost = min(minPathCost, currPathCost);
                 }
-                dp[R][prevCol + 1] = minPathCost;
+
+                dp[R][prevCol] = minPathCost;
             }
         }
 
-        // Return the result value
-        return dp[0][0];
+        return dp[0][N];
     }
 
-    // #2 Method to find the minimum cost, using 1D tabulation - O(M*N*M) & O(M)
-    int minPathCost_V2(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-        int N = grid.size(), M = grid[0].size();
+    // O(M*N*N) & O(M*N)
+    int solveWith2DEnhanced(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
+        vector<vector<int>> dp(M+1, vector<int>(N+1, 0));
 
-        // 1D DP tables
-        vector<int> nextRow(M+1, 0), currRow(M+1, 0);
-
-        // Fill the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int prevCol = M-1; prevCol >= -1; --prevCol) {
+        for(int R = M-1; R >= 0; --R) {
+            for(int prevCol = 0; prevCol <= N; ++prevCol) {
                 int minPathCost = INT_MAX;
-                for(int C = 0; C < M; ++C) {
-                    int currPathCost = grid[R][C] + nextRow[C+1];
-                    if(prevCol != -1 && R-1 >= 0) {
+
+                for(int C = 0; C < N; ++C) {
+                    int currPathCost = grid[R][C] + dp[R+1][C];
+                    if(prevCol != N && R-1 >= 0) {
                         currPathCost += moveCost[grid[R-1][prevCol]][C];
                     }
                     minPathCost = min(minPathCost, currPathCost);
                 }
-                currRow[prevCol + 1] = minPathCost;
+
+                dp[R][prevCol] = minPathCost;
             }
-            nextRow = currRow;
         }
 
-        // Return the result value
-        return currRow[0];
+        return dp[0][N];
+    }
+
+    // O(M*N*N) & O(2*N)
+    int solveWith1DTable(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
+        vector<int> nextRow(N+1, 0), idealRow(N+1, 0);
+
+        for(int R = M-1; R >= 0; --R) {
+            for(int prevCol = 0; prevCol <= N; ++prevCol) {
+                int minPathCost = INT_MAX;
+
+                for(int C = 0; C < N; ++C) {
+                    int currPathCost = grid[R][C] + nextRow[C];
+                    if(prevCol != N && R-1 >= 0) {
+                        currPathCost += moveCost[grid[R-1][prevCol]][C];
+                    }
+                    minPathCost = min(minPathCost, currPathCost);
+                }
+
+                idealRow[prevCol] = minPathCost;
+            }
+            nextRow = idealRow;
+        }
+
+        return nextRow[N];
+    }
+
+public:
+    int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
+        M = grid.size(), N = grid[0].size();
+        return solveWith1DTable(grid, moveCost);
     }
 };
 
