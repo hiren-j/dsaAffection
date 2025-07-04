b17123a0cd555a76cc8593a6159018847e478161
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-07-04 16:06:49 +0530

    Update 29 - Mobile Numeric Keypad.cpp

--- Dynamic Programming/Multi Dimensional DP/29 - Mobile Numeric Keypad.cpp
+++ Dynamic Programming/Multi Dimensional DP/29 - Mobile Numeric Keypad.cpp
@@ -1,83 +1,73 @@
-// Code to find the number of possible unique sequences of length n that you can create by pressing buttons (You can start from any digit) ~ coded by Hiren
+// Code to find the number of possible unique sequences of len n that you can create by pressing buttons (You can start from any digit) ~ coded by Hiren
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {    
-    #define LL long long
+    #define rowLimit 4
+    #define colLimit 3
 
     vector<vector<int>> directions = {{0,0}, {-1,0}, {1,0}, {0,-1}, {0,1}};
-    vector<vector<int>> numpad     = {{1, 2, 3},
+    vector<vector<int>> keypad     = {{1, 2, 3},
                                       {4, 5, 6},
                                       {7, 8, 9},
                                       {-1, 0, -1}};
-
     // O(10 * 5^N) & O(N) 
-    LL solveWithoutMemo(int N, int R, int C) {
-        // Edge case: No number exists outside of the numpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
-        if(R < 0 || C < 0 || R == 4 || C == 3 || numpad[R][C] == -1)
+    int solveWithoutMemo(int N, int R, int C) {
+        // Edge case: No number exists outside of the keypad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
+        if(R < 0 || C < 0 || R == rowLimit || C == colLimit || keypad[R][C] == -1)
             return 0;
             
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
+        // Edge case: If you've correctly dialed a phone number of len N then you've one valid way
         if(N == 0)
             return 1;
                     
-        // Stores the result value
-        LL count = 0;
+        int count = 0;
         
-        // Explore all the 5 directions one by one from the cell (R, C) and dial the phone numbers 
         for(auto& dir : directions) {
-            int reachRow = R + dir[0];
-            int reachCol = C + dir[1];
-            count += solveWithoutMemo(N - 1, reachRow, reachCol);
+            int newR = R + dir[0];
+            int newC = C + dir[1];
+            count += solveWithoutMemo(N - 1, newR, newC);
         }
 
-        // Return the result value
         return count;
     }
 
     // O(10 + 5*N*4*3) & O(N*4*3 + N)
-    LL solveWithMemo(vector<vector<vector<LL>>>& memory, int N, int R, int C) {
-        // Edge case: No number exists outside of the numpad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
-        if(R < 0 || C < 0 || R == 4 || C == 3 || numpad[R][C] == -1)
+    int solveWithMemo(vector<vector<vector<int>>>& dp, int N, int R, int C) {
+        // Edge case: No number exists outside of the keypad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
+        if(R < 0 || C < 0 || R == rowLimit || C == colLimit || keypad[R][C] == -1)
             return 0;
             
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
+        // Edge case: If you've correctly dialed a phone number of len N then you've one valid way
         if(N == 0)
             return 1;
         
-        // Memoization table: If the current state is already computed then return the computed value
-        if(memory[N][R][C] != -1)
-            return memory[N][R][C];
+        if(dp[N][R][C] != -1)
+            return dp[N][R][C];
             
-        // Stores the result value
-        LL count = 0;
+        int count = 0;
         
-        // Explore all the 5 directions one by one from the cell (R, C) and dial the phone numbers 
         for(auto& dir : directions) {
-            int reachRow = R + dir[0];
-            int reachCol = C + dir[1];
-            count += solveWithMemo(memory, N - 1, reachRow, reachCol);
+            int newR = R + dir[0];
+            int newC = C + dir[1];
+            count += solveWithMemo(dp, N - 1, newR, newC);
         }
         
-        // Store the result value to the memoization table and then return it
-        return memory[N][R][C] = count;
+        return dp[N][R][C] = count;
     }
     
 public:
-    // Method to find how many distinct phone numbers of length N you can dial, using recursion with memoization - O(N) & O(N)
-    LL getCount(int N) {
-        LL result = 0;
-        
-        // 3D memoization table
-        vector<vector<vector<LL>>> memory(N, vector<vector<LL>>(4, vector<LL>(3, -1)));
+    // Method to count total distinct phone numbers of length N you can dial, using recursion with memoization - O(N) & O(N)
+    int getCount(int N) {
+        vector<vector<vector<int>>> dp(N, vector<vector<int>>(rowLimit, vector<int>(colLimit, -1)));
+        int result = 0;        
         
         // Start dialing from the cells which doesn't starts from '*' or '#'
-        for(int R = 0; R < 4; ++R) 
-            for(int C = 0; C < 3; ++C) 
-                if(numpad[R][C] != -1)
-                    result += solveWithMemo(memory, N - 1, R, C);
+        for(int R = 0; R < rowLimit; ++R) 
+            for(int C = 0; C < colLimit; ++C) 
+                if(keypad[R][C] != -1)
+                    result += solveWithMemo(dp, N - 1, R, C);
 
-        // Return the result value 
         return result;
     }
 };
@@ -85,49 +75,48 @@ public:
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    #define LL long long
+    #define rowLimit 4
+    #define colLimit 3
 
     vector<vector<int>> directions = {{0,0}, {-1,0}, {1,0}, {0,-1}, {0,1}};
-    vector<vector<int>> numpad     = {{1, 2, 3},
+    vector<vector<int>> keypad     = {{1, 2, 3},
                                       {4, 5, 6},
                                       {7, 8, 9},
                                       {-1, 0, -1}};
 public:
-    // Method to find how many distinct phone numbers of length N you can dial, using 3D tabulation - O(N*4*3*5) & O(N*4*3)
-    LL getCount(int N) {
-        LL result = 0;
-        
-        // 3D DP table
-        vector<vector<vector<LL>>> dp(N, vector<vector<LL>>(4, vector<LL>(3, 0)));
+    // O(N*4*3*5) & O(N*4*3)
+    int getCount(int N) {
+        vector<vector<vector<int>>> dp(N, vector<vector<int>>(rowLimit, vector<int>(colLimit, 0)));
 
-        // Initialize the second edge case: If you've correctly dialed a phone number of length N then you've one valid way
-        for(int R = 0; R < 4; ++R)
-            for(int C = 0; C < 3; ++C)
+        // Initialize second edge case
+        for(int R = 0; R < rowLimit; ++R)
+            for(int C = 0; C < colLimit; ++C)
                 dp[0][R][C] = 1;
 
-        // Fill the rest of the table
-        for(int length = 1; length < N; ++length) {
-            for(int R = 3; R >= 0; --R) {
-                for(int C = 2; C >= 0; --C) {
-                    if(numpad[R][C] != -1) {
-                        LL count = 0;
+        for(int len = 1; len < N; ++len) {
+            for(int R = rowLimit-1; R >= 0; --R) {
+                for(int C = colLimit-1; C >= 0; --C) {
+                    if(keypad[R][C] != -1) {
+                        int count = 0;
                         for(auto& dir : directions) {
-                            int reachRow = R + dir[0];
-                            int reachCol = C + dir[1];
-                            if(reachRow >= 0 && reachCol >= 0 && reachRow < 4 && reachCol < 3 && numpad[reachRow][reachCol] != -1) {
-                                count += dp[length - 1][reachRow][reachCol];
+                            int newR = R + dir[0];
+                            int newC = C + dir[1];
+                            if(newR >= 0 && newC >= 0 && newR < 4 && newC < 3 && keypad[newR][newC] != -1) {
+                                count += dp[len - 1][newR][newC];
                             }
                         }
-                        dp[length][R][C] = count; 
+                        dp[len][R][C] = count; 
                     }
                 }
             }
         }
 
+        int result = 0;        
+
         // Start dialing from the cells which doesn't starts from '*' or '#'
-        for(int R = 0; R < 4; ++R) 
-            for(int C = 0; C < 3; ++C) 
-                if(numpad[R][C] != -1)
+        for(int R = 0; R < rowLimit; ++R) 
+            for(int C = 0; C < colLimit; ++C) 
+                if(keypad[R][C] != -1)
                     result += dp[N - 1][R][C];
 
         // Return the result value 
