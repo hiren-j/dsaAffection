cbbf92fb455d5d0ce153480dc74153db8f97c007
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-07-04 16:15:56 +0530

    Update 29 - Mobile Numeric Keypad.cpp

--- Dynamic Programming/Multi Dimensional DP/29 - Mobile Numeric Keypad.cpp
+++ Dynamic Programming/Multi Dimensional DP/29 - Mobile Numeric Keypad.cpp
@@ -85,7 +85,7 @@ class BottomUp {
                                       {-1, 0, -1}};
 public:
     // O(N*4*3*5) & O(N*4*3)
-    int getCount(int N) {
+    int countUniqueSeqsOfLen(int N) {
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(rowLimit, vector<int>(colLimit, -1)));
 
         // Initialize second edge case
