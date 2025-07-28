51ea6edac1887e7d87448c9746c7c7d9b87bf308
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-07-29 00:52:38 +0530

    Update 35.2 - Cherry Pickup II.cpp

--- Dynamic Programming/Multi Dimensional DP/35.2 - Cherry Pickup II.cpp
+++ Dynamic Programming/Multi Dimensional DP/35.2 - Cherry Pickup II.cpp
@@ -3,125 +3,98 @@
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-    int N, M;
+    vector<vector<int>> directions = {{1,0}, {1,-1}, {1,1}};
+    int M, N;
 
-    // O(9^(N*M)) & O(N)
-    int solveWithoutMemo(vector<vector<int>>& grid, int R, int C1, int C2) {
-        // Edge case: When all the cells are exhausted then its not possible to collect more cherries
-        if(C1 < 0 || C2 < 0 || C1 == M || C2 == M || R == N)
-            return 0;
-
-        // If both the robots are not at same column then collect the cherries from both of them, else collect it from any one robot
-        int currCherries = (C1 != C2 ? grid[R][C1] + grid[R][C2] : grid[R][C1]);
-
-        // Stores the maximum number of cherries collected by any one robot
-        int maxFromAll = 0;
-
-        // Explore all the 3 possibilities of robot2 from each possibility of robot1
-        for(int dir1 = -1; dir1 <= 1; ++dir1) 
-            for(int dir2 = -1; dir2 <= 1; ++dir2) 
-                maxFromAll = max(maxFromAll, solveWithoutMemo(grid, R + 1, C1 + dir1, C2 + dir2));
-
-        // Return the result value
-        return maxFromAll + currCherries;
+    bool isValid(int R, int C) {
+        return R >= 0 && C >= 0 && R < M && C < N;
     }
 
-    // O(9*N*M*M) & O(N*M*M + N)
-    int solveWithMemo(vector<vector<vector<int>>>& memory, vector<vector<int>>& grid, int R, int C1, int C2) {
-        // Edge case: When all the cells are exhausted then its not possible to collect more cherries
-        if(C1 < 0 || C2 < 0 || C1 == M || C2 == M || R == N)
+    // O(9^(M*N)) & O(M)
+    int solveWithoutMemo(vector<vector<int>>& grid, int R1, int C1, int R2, int C2) {
+        if(!isValid(R1, C1) || !isValid(R2, C2))
             return 0;
 
-        // Memoization table: If the current state is already computed then return the computed value
-        if(memory[R][C1][C2] != -1)
-            return memory[R][C1][C2];
-
-        // If both the robots are not at same column then collect the cherries from both of them, else collect it from any one robot
-        int currCherries = (C1 != C2 ? grid[R][C1] + grid[R][C2] : grid[R][C1]);
-
-        // Stores the maximum number of cherries collected by any one robot
+        int cellCherry = (C1 == C2) ? grid[R1][C1] : grid[R1][C1] + grid[R2][C2];
         int maxFromAll = 0;
 
-        // Explore all the 3 possibilities of robot2 from each possibility of robot1
-        for(int dir1 = -1; dir1 <= 1; ++dir1) 
-            for(int dir2 = -1; dir2 <= 1; ++dir2) 
-                maxFromAll = max(maxFromAll, solveWithMemo(memory, grid, R + 1, C1 + dir1, C2 + dir2));
+        for(auto& dir1 : directions) { // Explore all 3 possibilities of robot2 from each possibility of robot1
+            int newR1 = R1 + dir1[0];
+            int newC1 = C1 + dir1[1];
+            
+            for(auto& dir2 : directions) {
+                int newR2 = R2 + dir2[0];
+                int newC2 = C2 + dir2[1];
+                int nextCherry = solveWithoutMemo(grid, newR1, newC1, newR2, newC2);
+                maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+            }
+        }
 
-        // Store the result value to the memoization table and then return it
-        return memory[R][C1][C2] = maxFromAll + currCherries;
+        return maxFromAll;
     }
 
+    // O(9*M*N*M*N) & O(M*N*M*N + M)
+    int solveWithMemo(vector<vector<vector<vector<int>>>>& dp, vector<vector<int>>& grid, int R1, int C1, int R2, int C2) {
+        if(!isValid(R1, C1) || !isValid(R2, C2))
+            return 0;
+
+        if(dp[R1][C1][R2][C2] != -1)
+            return dp[R1][C1][R2][C2];
+
+        int cellCherry = (C1 == C2) ? grid[R1][C1] : grid[R1][C1] + grid[R2][C2];
+        int maxFromAll = 0;
+
+        for(auto& dir1 : directions) { // Explore all 3 possibilities of robot2 from each possibility of robot1
+            int newR1 = R1 + dir1[0];
+            int newC1 = C1 + dir1[1];
+            
+            for(auto& dir2 : directions) {
+                int newR2 = R2 + dir2[0];
+                int newC2 = C2 + dir2[1];
+                int nextCherry = solveWithMemo(dp, grid, newR1, newC1, newR2, newC2);
+                maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+            }
+        }
+
+        return dp[R1][C1][R2][C2] = maxFromAll;
+    }
 
 public:
-    // Method to find the maximum number of cherries collection, using recursion with memoization - (N*M*M) & O(N*M*M)
+    // Method to find maximum cherries can collect, using recursion with memoization - O(M*N*M*N) & O(M*N*M*N)
     int cherryPickup(vector<vector<int>>& grid) {
-        N = grid.size(), M = grid[0].size();
-        vector<vector<vector<int>>> memory(N, vector<vector<int>>(M, vector<int>(M, -1)));
-        return solveWithMemo(memory, grid, 0, 0, M - 1);
+        M = grid.size(), N = grid[0].size();
+        vector<vector<vector<vector<int>>>> dp(M, vector<vector<vector<int>>>(N, vector<vector<int>>(M, vector<int>(N, -1))));
+        return solveWithMemo(dp, grid, 0, 0, 0, N-1);
     }
 };
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the maximum number of cherries collection, using 3D tabulation - (N*M*M) & O(N*M*M)
-    int cherryPickup_V1(vector<vector<int>>& grid) {
-        int N = grid.size(), M = grid[0].size();
+    vector<vector<int>> directions = {{1,0}, {1,-1}, {1,1}};
+    int M, N;
 
-        // 3D DP table
-        vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(M + 2, vector<int>(M + 2, 0)));
-
-        // Fill the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C1 = M; C1 >= 1; --C1) {
-                for(int C2 = 1; C2 <= M; ++C2) {
-                    int currCherries = (C1 != C2 ? grid[R][C1 - 1] + grid[R][C2 - 1] : grid[R][C1 - 1]);
-                    int maxFromAll = 0;
-                    for(int dir1 = -1; dir1 <= 1; ++dir1) {
-                        for(int dir2 = -1; dir2 <= 1; ++dir2) {
-                            maxFromAll = max(maxFromAll, dp[R + 1][C1 + dir1][C2 + dir2]);
-                        } 
-                    }
-                    dp[R][C1][C2] = maxFromAll + currCherries;
-                }
-            }
-        }
-
-        // Return the result value
-        return dp[0][1][M];
+    bool isValid(int R, int C) {
+        return R >= 0 && C >= 0 && R < M && C < N;
     }
 
-    // #2 Method to find the maximum number of cherries collection, using 2D tabulation - (N*M*M) & O(M*M)
-    int cherryPickup_V2(vector<vector<int>>& grid) {
-        int N = grid.size(), M = grid[0].size();
+    // O(M*N*M*N) & O(M*N*M*N)
+    int solveWith4DTable(vector<vector<int>>& grid) {
+        vector<vector<vector<vector<int>>>> dp(M, vector<vector<vector<int>>>(N, vector<vector<int>>(M, vector<int>(N, -1))));
 
-        // 2D DP tables
-        vector<vector<int>> nextRow(M + 2, vector<int>(M + 2, 0)), idealRow(M + 2, vector<int>(M + 2, 0));
+        for(int R1 = M-1; R1 >= 0; --R1) {
+            for(int C1 = N-1; C1 >= 0; --C1) {
+                for(int R2 = M-1; R2 >= 0; --R2) {
+                    for(int C2 = 0; C2 <= N-1; ++C2) {
+                        int cellCherry = (C1 == C2) ? grid[R1][C1] : grid[R1][C1] + grid[R2][C2];
+                        int maxFromAll = 0;
 
-        // Fill the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C1 = M; C1 >= 1; --C1) {
-                for(int C2 = 1; C2 <= M; ++C2) {
-                    int currCherries = (C1 != C2 ? grid[R][C1 - 1] + grid[R][C2 - 1] : grid[R][C1 - 1]);
-                    int maxFromAll = 0;
-                    for(int dir1 = -1; dir1 <= 1; ++dir1) {
-                        for(int dir2 = -1; dir2 <= 1; ++dir2) {
-                            maxFromAll = max(maxFromAll, nextRow[C1 + dir1][C2 + dir2]);
-                        } 
-                    }
-                    idealRow[C1][C2] = maxFromAll + currCherries;
-                }
-            }
-            nextRow = idealRow;
-        }
+                        for(auto& dir1 : directions) {
+                            int newR1 = R1 + dir1[0];
+                            int newC1 = C1 + dir1[1];
 
-        // Return the result value
-        return idealRow[1][M];
-    }
-};
-
------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-Topics: Array | Dynamic Programming | Matrix
-Link  : https://leetcode.com/problems/cherry-pickup-ii/description/
+                            for(auto& dir2 : directions) {
+                                int newR2 = R2 + dir2[0];
+                                int newC2 = C2 + dir2[1];
+                                int nextCherry = (!isValid(newR1, newC1) || !isValid(newR2, newC2)) ? 0 : dp[newR1][newC1][newR2][newC2];
+              
