a363c758c07a10c445db787391df5b3a46d1812f
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-09-04 23:20:21 +0530

    Update 33.1 - Longest Increasing Path In A Matrix.cpp

--- Dynamic Programming/Multi Dimensional DP/33.1 - Longest Increasing Path In A Matrix.cpp
+++ Dynamic Programming/Multi Dimensional DP/33.1 - Longest Increasing Path In A Matrix.cpp
@@ -1,18 +1,18 @@
 // Code to find the length of the longest increasing path in matrix. From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed) ~ coded by Hiren
 
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
-DON'T IGNORE MUST READ: As you work on this problem, you'll notice that there isn't a bottom-up solution provided. 
-                        This is one of the problem in the multi-dimensional folder where I couldn't get the bottom-up approach to pass all the test cases. 
-                        The issue wasn't with time limits but with incorrect results. So, I've only included the top-down (memoized) solution for this problem. 
-                        There are only 2 problem in the entire DP series without bottom-up solution. 
-                        Creating a correct bottom-up solution for this problem is not possible because of how cells are visited and marked during recursion. Even if you convert the memoized (top-down) solution to bottom-up, it won’t work properly. 
-                        The issue comes from the fact that, in the recursive solution, cells are marked as "visited" and "unvisited" at different stages, which can't be handled in a bottom-up approach. In many cases, even memoization fails when handling these visited/unvisited states. 
-                        However, for this specific problem, memoization works fine, but the bottom-up approach still cannot manage the visited/unvisited states correctly.
+    DON'T IGNORE MUST READ: As you work on this problem, you'll notice that there isn't a bottom-up solution provided. 
+                            This is one of the problem in the multi-dimensional folder where I couldn't get the bottom-up approach to pass all the test cases. 
+                            The issue wasn't with time limits but with incorrect results. So, I've only included the top-down (memoized) solution for this problem. 
+                            There are only 2 problem in the entire DP series without bottom-up solution. 
+                            Creating a correct bottom-up solution for this problem is not possible because of how cells are visited and marked during recursion. Even if you convert the memoized (top-down) solution to bottom-up, it won’t work properly. 
+                            The issue comes from the fact that, in the recursive solution, cells are marked as "visited" and "unvisited" at different stages, which can't be handled in a bottom-up approach. In many cases, even memoization fails when handling these visited/unvisited states. 
+                            However, for this specific problem, memoization works fine, but the bottom-up approach still cannot manage the visited/unvisited states correctly.
 */
   
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
     vector<vector<int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
@@ -97,7 +97,7 @@ public:
     }
 };
 
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Topics: Array | Dynamic Programming | Depth-First Search | Breadth-First Search | Graph | Topological Sort | Memoization | Matrix
 Link  : https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
