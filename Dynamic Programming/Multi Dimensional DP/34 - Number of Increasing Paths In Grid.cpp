1438d9d881b4f3c3e29a6c405ea2ea332a6f4aa5
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-09-04 23:03:29 +0530

    Update 34 - Number of Increasing Paths In Grid.cpp

--- Dynamic Programming/Multi Dimensional DP/34 - Number of Increasing Paths In Grid.cpp
+++ Dynamic Programming/Multi Dimensional DP/34 - Number of Increasing Paths In Grid.cpp
@@ -1,1 +1,41 @@
-hiren
+class TopDown {
+    const vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    const int MOD = 1e9 + 7;
+    int M, N;
+
+    bool isValid(int R, int C) {
+        return R >= 0 && C >= 0 && R < M && C < N;
+    }
+
+    int solveWithMemo(vector<vector<int>>& dp, const vector<vector<int>>& grid, int R, int C) {
+        if(dp[R][C] != -1)
+            return dp[R][C];
+
+        int count = 1;
+
+        for(const auto& D : dirs) {
+            int newR = R + D[0];
+            int newC = C + D[1];
+            if(isValid(newR, newC) && grid[newR][newC] > grid[R][C]) {
+                count = (count + solveWithMemo(dp, grid, newR, newC)) % MOD;
+            }
+        }
+
+        return dp[R][C] = count;
+    }
+
+public:
+    // O(M*N) & O(M*N)
+    int countPaths(vector<vector<int>>& grid) {
+        M = grid.size(), N = grid[0].size();
+        int count = 0;
+
+        vector<vector<int>> dp(M, vector<int>(N, -1));
+
+        for(int R = 0; R < M; ++R)
+            for(int C = 0; C < N; ++C)
+                count = (count + solveWithMemo(dp, grid, R, C)) % MOD;
+
+        return count;
+    }
+};
