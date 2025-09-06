cb2903b9c62b669770698469585ada6b69c82646
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-09-07 01:03:27 +0530

    Update 29 - Soup Servings.cpp

--- Dynamic Programming/Multi Dimensional DP/29 - Soup Servings.cpp
+++ Dynamic Programming/Multi Dimensional DP/29 - Soup Servings.cpp
@@ -1,7 +1,33 @@
-class Solution {
+// Code to find the probability that A is used up before B plus half the probability that both soups are used up in the same turn ~ coded by Hiren
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class TopDown {
     const vector<vector<int>> operations = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
 
-    // O(N^2) & O(N^2)
+    // O(4^(N+N)) & O(N+N)
+    double solveWithoutMemo(int A, int B) {
+        if(A <= 0 && B <= 0)
+            return 0.5;
+
+        if(A <= 0)
+            return 1.0;
+
+        if(B <= 0)
+            return 0.0;
+
+        double prob = 0.0;
+
+        for(const auto& op : operations) {
+            int newA = A - op[0];
+            int newB = B - op[1];
+            prob += solveWithoutMemo(newA, newB);
+        }
+
+        return 0.25 * prob;
+    }
+
+    // O(4*N*N) & O(N*N + N+N)
     double solveWithMemo(vector<vector<double>>& dp, int A, int B) {
         if(A <= 0 && B <= 0)
             return 0.5;
@@ -25,6 +51,20 @@ class Solution {
 
         return dp[A][B] = 0.25 * prob;
     }
+
+public:
+    // Method to find specified probability, using recursion with memoization - O(N^2) & O(N^2)
+    double soupServings(int n) {
+        if(n >= 6000)
+            return 1.0;
+        return solveWith2DTable(n);
+    }
+};
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class BottomUp {
+    const vector<vector<int>> operations = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
 
     // O(N^2) & O(N^2)
     double solveWith2DTable(int n) {
@@ -57,3 +97,8 @@ public:
         return solveWith2DTable(n);
     }
 };
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Topics: Math | Dynamic Programming | Probability and Statistics | Weekly Contest 78
+Link  : https://leetcode.com/problems/soup-servings/description/
