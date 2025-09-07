b5342a5a7b0ec03e44e00f860c8be16f617c4c17
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-09-07 19:57:31 +0530

    Update 38 - Find a Safe Walk Through a Grid.cpp

--- Dynamic Programming/Multi Dimensional DP/38 - Find a Safe Walk Through a Grid.cpp
+++ Dynamic Programming/Multi Dimensional DP/38 - Find a Safe Walk Through a Grid.cpp
@@ -6,6 +6,7 @@
   DON'T IGNORE MUST READ: Creating a correct bottom-up solution for this problem is not possible because of how cells are visited and marked during recursion. Even if you convert the memoized (top-down) solution to bottom-up, it won’t work properly. 
                           The issue comes from the fact that, in the recursive solution, cells are marked as "visited" and "unvisited" at different stages, which can't be handled in a bottom-up approach. In many cases, even memoization fails when handling these visited/unvisited states. 
                           However, for this specific problem, memoization works fine, but the bottom-up approach still cannot manage the visited/unvisited states correctly, the thing is grid state is also changing each and every time, if we try to memoize the grid too the we'll end up using a lot of space, more clearly MLE.
+                          And I would say there are only 3 problems in the whole series which doesn't contain the bottom-up solution due to this reason. These 3 problems lies in this folder (Multi-Dimensional DP Folder).
 */
   
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
