5555a714452b927c9a45b7d1d378d2f5fdde95ec
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-09-07 16:00:46 +0530

    Update 9 - Dungeon Game.cpp

--- Dynamic Programming/Multi Dimensional DP/9 - Dungeon Game.cpp
+++ Dynamic Programming/Multi Dimensional DP/9 - Dungeon Game.cpp
@@ -3,33 +3,45 @@
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-public:
-    // Method to find the knight's minimum initial health, using recursion with memoization - O(N*M) & O(N*M)
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int N = dungeon.size(), M = dungeon[0].size();
-        vector<vector<int>> memory(N, vector<int>(M, -1));
-        return solveWithMemo(memory, dungeon, N, M, 0, 0);
-    }
-    
-private:
-    // O(2*N*M) & O(N*M + N+M)
-    int solveWithMemo(vector<vector<int>>& memory, vector<vector<int>>& dungeon, int N, int M, int R, int C) {
+    int M, N;
+
+    // O(2^(M*N)) & O(M+N)
+    int solveWithoutMemo(vector<vector<int>>& dungeon, int R, int C) {
         // Edge case: If all the cells are exhausted then its not possible to play the game anymore
-        if(R == N || C == M)
-            // Requires to handle the minimum health correctly
-            return INT_MAX;
+        if(R == M || C == N)
+            return INT_MAX; // Requires to handle minimum health correctly
 
         // Edge case: If reached the bottom-right corner then rescue the princess. Calcuate and return the minimum health required for the knight to defeat the demon of the current room or not a demon then he requires minimum of single health to live
-        if(R == N-1 && C == M-1)
+        if(R == M-1 && C == N-1)
             return (dungeon[R][C] <= 0) ? -dungeon[R][C] + 1 : 1;
 
-        // Memoization table: If the current state is already computed then return the computed value
+        // There are always two possibilities the knight can perform at each room
+        int moveRight = solveWithoutMemo(dungeon, R, C+1); // Is to move right
+        int moveDown  = solveWithoutMemo(dungeon, R+1, C); // Is to move down
+
+        // Stores the minimum health required for the knight to live at the current room
+        int minHealthToLive = min(moveRight, moveDown) - dungeon[R][C];
+
+        // Ensure the knight dies or not, if dies then he must need single health to live at the current room 
+        return (minHealthToLive <= 0) ? 1 : minHealthToLive;
+    }
+
+    // O(2*M*N) & O(M*N + M+N)
+    int solveWithMemo(vector<vector<int>>& memory, vector<vector<int>>& dungeon, int R, int C) {
+        // Edge case: If all cells are exhausted then its not possible to play the game anymore
+        if(R == M || C == N)
+            return INT_MAX; // Requires to handle minimum health correctly
+
+        // Edge case: If reached the bottom-right corner then rescue the princess. Calcuate and return the minimum health required for the knight to defeat the demon of the current room or not a demon then he requires minimum of single health to live
+        if(R == M-1 && C == N-1)
+            return (dungeon[R][C] <= 0) ? -dungeon[R][C] + 1 : 1;
+
         if(memory[R][C] != -1)
             return memory[R][C];
 
         // There are always two possibilities the knight can perform at each room
-        int moveRight = solveWithMemo(memory, dungeon, N, M, R, C+1); // Is to move right
-        int moveDown  = solveWithMemo(memory, dungeon, N, M, R+1, C); // Is to move down
+        int moveRight = solveWithMemo(memory, dungeon, R, C+1); // Is to move right
+        int moveDown  = solveWithMemo(memory, dungeon, R+1, C); // Is to move down
 
         // Stores the minimum health required for the knight to live at the current room
         int minHealthToLive = min(moveRight, moveDown) - dungeon[R][C];
@@ -38,47 +50,30 @@ private:
         return memory[R][C] = (minHealthToLive <= 0) ? 1 : minHealthToLive;
     }
 
-    // O(2^(N*M)) & O(N+M)
-    int solveWithoutMemo(vector<vector<int>>& dungeon, int N, int M, int R, int C) {
-        // Edge case: If all the cells are exhausted then its not possible to play the game anymore
-        if(R == N || C == M)
-            // Requires to handle the minimum health correctly
-            return INT_MAX;
-
-        // Edge case: If reached the bottom-right corner then rescue the princess. Calcuate and return the minimum health required for the knight to defeat the demon of the current room or not a demon then he requires minimum of single health to live
-        if(R == N-1 && C == M-1)
-            return (dungeon[R][C] <= 0) ? -dungeon[R][C] + 1 : 1;
-
-        // There are always two possibilities the knight can perform at each room
-        int moveRight = solveWithoutMemo(dungeon, N, M, R, C+1); // Is to move right
-        int moveDown  = solveWithoutMemo(dungeon, N, M, R+1, C); // Is to move down
-
-        // Stores the minimum health required for the knight to live at the current room
-        int minHealthToLive = min(moveRight, moveDown) - dungeon[R][C];
-
-        // Ensure the knight dies or not, if dies then he must need single health to live at the current room 
-        return (minHealthToLive <= 0) ? 1 : minHealthToLive;
+public:
+    // Method to find knight's minimum initial health, using recursion with memoization - O(M*N) & O(M*N)
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        M = dungeon.size(), N = dungeon[0].size();
+        vector<vector<int>> memory(M, vector<int>(N, -1));
+        return solveWithMemo(memory, dungeon, 0, 0);
     }
 };
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class BottomUp {
-public:
-    // #1 Method to find the knight's minimum initial health, using 2D tabulation - O(N*M) & O(N*M)
-    int calculateMinimumHP_V1(vector<vector<int>>& dungeon) {
-        int N = dungeon.size(), M = dungeon[0].size();
+class Solution {
+    int M, N;
 
-        // 2D DP table
-        vector<vector<int>> dp(N+1, vector<int>(M+1, INT_MAX));
+    // O(M*N) & O(M*N)
+    int solveWith2DTable(vector<vector<int>>& dungeon) {
+        vector<vector<int>> dp(M+1, vector<int>(N+1, INT_MAX));
 
-        // Initialize the second edge case
-        dp[N-1][M-1] = (dungeon[N-1][M-1] <= 0) ? -dungeon[N-1][M-1] + 1 : 1;
+        // Initialize second edge case
+        dp[M-1][N-1] = (dungeon[M-1][N-1] <= 0) ? -dungeon[M-1][N-1] + 1 : 1;
 
-        // Fill the rest of the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = M-1; C >= 0; --C) {
-                if(R == N-1 && C == M-1)
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if(R == M-1 && C == N-1)
                     continue;
                 int moveRight = dp[R][C+1]; 
                 int moveDown  = dp[R+1][C]; 
@@ -87,58 +82,54 @@ public:
             }
         }
 
-        // Return the result value
         return dp[0][0];
     }
 
-    // #2 Method to find the knight's minimum initial health, using 1D tabulation - O(N*M) & O(M)
-    int calculateMinimumHP_V2(vector<vector<int>>& dungeon) {
-        int N = dungeon.size(), M = dungeon[0].size();
+    // O(M*N) & O(2*N)
+    int solveWith1DTable(vector<vector<int>>& dungeon) {
+        vector<int> nextRow(N+1, INT_MAX), currRow(N+1, INT_MAX);
 
-        // 1D DP tables
-        vector<int> nextRow(M+1, INT_MAX), currRow(M+1, INT_MAX);
+        // Initialize second edge case
+        currRow[N-1] = (dungeon[M-1][N-1] <= 0) ? -dungeon[M-1][N-1] + 1 : 1;
 
-        // Initialize the second edge case
-        currRow[M-1] = (dungeon[N-1][M-1] <= 0) ? -dungeon[N-1][M-1] + 1 : 1;
-
-        // Fill the rest of the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = M-1; C >= 0; --C) {
-                if(R == N-1 && C == M-1)
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if(R == M-1 && C == N-1)
                     continue;
                 int moveRight = currRow[C+1]; 
                 int moveDown  = nextRow[C]; 
                 int minHealthToLive = min(moveRight, moveDown) - dungeon[R][C];
                 currRow[C] = (minHealthToLive <= 0) ? 1 : minHealthToLive;   
             }
-            nextRow = currRow;
+            swap(nextRow, currRow);
         }
 
-        // Return the result value
         return nextRow[0];
     }
 
-    // #3 Method to find the knight's minimum initial health, using constant auxiliary space - O(N*M) & O(1)
-    int calculateMinimumHP_V3(vector<vector<int>>& dungeon) {
-        int N = dungeon.size(), M = dungeon[0].size();
+    // O(M*N) & O(1)
+    int solveWithoutTable(vector<vector<int>>& dungeon) {
+        // Initialize second edge case
+        dungeon[M-1][N-1] = (dungeon[M-1][N-1] <= 0) ? -dungeon[M-1][N-1] + 1 : 1;
 
-        // Initialize the second edge case
-        dungeon[N-1][M-1] = (dungeon[N-1][M-1] <= 0) ? -dungeon[N-1][M-1] + 1 : 1;
-
-        // Fill the rest of the table
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = M-1; C >= 0; --C) {
-                if(R == N-1 && C == M-1)
+        for(int R = M-1; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                if(R == M-1 && C == N-1)
                     continue;
-                int moveRight = (C+1 < M) ? dungeon[R][C+1] : INT_MAX; 
-                int moveDown  = (R+1 < N) ? dungeon[R+1][C] : INT_MAX; 
+                int moveRight = (C+1 < N) ? dungeon[R][C+1] : INT_MAX; 
+                int moveDown  = (R+1 < M) ? dungeon[R+1][C] : INT_MAX; 
                 int minHealthToLive = min(moveRight, moveDown) - dungeon[R][C];
                 dungeon[R][C] = (minHealthToLive <= 0) ? 1 : minHealthToLive;   
             }
         }
 
-        // Return the result value
         return dungeon[0][0];
+    }
+
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        M = dungeon.size(), N = dungeon[0].size();
+        return solveWithoutTable(dungeon);
     }
 };
 
