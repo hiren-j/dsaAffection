4dd489e820cd95aff7b6205d40779898ef3632a1
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-11-08 21:40:37 +0530

    Update Climbing Stairs.cpp

--- Dynamic Programming/1D DP/6 - Climbing Stairs/Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/6 - Climbing Stairs/Climbing Stairs.cpp
@@ -5,27 +5,27 @@
 class TopDown {
     // O(2^N) & O(N)
     int solveWithoutMemo(int n) {
-        if(n <= 0)
-            return (n == 0);
+        if(n <= 1)
+            return 1;
 
-        int climb1Step = solveWithMemo(n - 1);
-        int climb2Step = solveWithMemo(n - 2);
+        int jump1StepDown = solveWithoutMemo(n - 1);
+        int jump2StepDown = solveWithoutMemo(n - 2);
 
-        return climb1Step + climb2Step;
+        return jump1StepDown + jump2StepDown;
     }
 
     // O(2*N) & O(2*N)
     int solveWithMemo(vector<int>& memory, int n) {
-        if(n <= 0)
-            return (n == 0);
+        if(n <= 1)
+            return 1;
 
         if(memory[n] != -1)
             return memory[n];
 
-        int climb1Step = solveWithMemo(memory, n - 1);
-        int climb2Step = solveWithMemo(memory, n - 2);
+        int jump1StepDown = solveWithMemo(memory, n - 1);
+        int jump2StepDown = solveWithMemo(memory, n - 2);
 
-        return memory[n] = (climb1Step + climb2Step);
+        return memory[n] = jump1StepDown + jump2StepDown;
     }
 
 public:
@@ -42,29 +42,32 @@ class BottomUp {
     int solveWith1DTable(int n) {
         vector<int> dp(n + 1, -1);
         dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i = 1; i <= n; ++i) {
-            int climb1Step = dp[i - 1];
-            int climb2Step = (i - 2 >= 0) ? dp[i - 2] : 0;
-            dp[i] = climb1Step + climb2Step;
+        for(int i = 2; i <= n; ++i) {
+            int jump1StepDown = dp[i - 1];
+            int jump2StepDown = dp[i - 2];
+            dp[i] = jump1StepDown + jump2StepDown;
         }
-        
+
         return dp[n];
     }
 
     // O(1*N) & O(1)
     int solveWithoutTable(int n) {
-        int climb1Step = 1;
-        int climb2Step = 0;
-        int numWays    = 0;
+        int dp_i_2 = 1;
+        int dp_i_1 = 1;
+        int dp_i = 1; // For when n = 1, because loop won't work for it
 
-        for(int i = 1; i <= n; ++i) {
-            numWays    = climb1Step + climb2Step;
-            climb2Step = climb1Step;
-            climb1Step = numWays;
+        for(int i = 2; i <= n; ++i) {
+            int jump1StepDown = dp_i_1;
+            int jump2StepDown = dp_i_2;
+            dp_i = jump1StepDown + jump2StepDown;
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
         }
-        
-        return numWays;
+
+        return dp_i;
     }
 
 public:
