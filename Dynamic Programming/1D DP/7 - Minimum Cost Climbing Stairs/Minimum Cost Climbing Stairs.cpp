e52140597ade3c9a6bd3fd713779c3cef3f28eac
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-11-09 21:28:23 +0530

    Update Minimum Cost Climbing Stairs.cpp

--- Dynamic Programming/1D DP/7 - Minimum Cost Climbing Stairs/Minimum Cost Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/7 - Minimum Cost Climbing Stairs/Minimum Cost Climbing Stairs.cpp
@@ -5,40 +5,37 @@
 class TopDown {
     int n;
 
-    // O(2^N) & O(N) : Where N = total steps
-    int solveWithoutMemo(vector<int>& cost, int step) {
-        if(step >= n)
+    // O(2^N) & O(N)
+    int solveWithoutMemo(const vector<int>& cost, int idx) {
+        if(idx >= n)
             return 0;
-        
-        int climb1Step = solveWithoutMemo(cost, step + 1);
-        int climb2Step = solveWithoutMemo(cost, step + 2);
 
-        return cost[step] + min(climb1Step, climb2Step);
-    }
+        int climb1Step = solveWithoutMemo(cost, idx + 1);
+        int climb2Step = solveWithoutMemo(cost, idx + 2);
 
+        return cost[idx] + min(climb1Step, climb2Step);
+    }    
+    
     // O(2*N) & O(2*N)
-    int solveWithMemo(vector<int>& memory, vector<int>& cost, int step) {
-        if(step >= n)
+    int solveWithMemo(vector<int>& dp, const vector<int>& cost, int idx) {
+        if(idx >= n)
             return 0;
 
-        if(memory[step] != -1)
-            return memory[step];
-        
-        int climb1Step = solveWithMemo(memory, cost, step + 1);
-        int climb2Step = solveWithMemo(memory, cost, step + 2);
+        if(dp[idx] != -1)
+            return dp[idx];
 
-        return memory[step] = cost[step] + min(climb1Step, climb2Step);
-    }
+        int climb1Step = solveWithMemo(dp, cost, idx + 1);
+        int climb2Step = solveWithMemo(dp, cost, idx + 2);
+
+        return dp[idx] = cost[idx] + min(climb1Step, climb2Step);
+    }    
 
 public:
     int minCostToReachTop(vector<int>& cost) {
         n = cost.size();
-
-        vector<int> memory1(n, -1), memory2(n, -1);
-        
-        int startFrom0 = solveWithMemo(memory1, cost, 0);
-        int startFrom1 = solveWithMemo(memory2, cost, 1);
-
+        vector<int> dp1(n, -1), dp2(n, -1);
+        int startFrom0 = solveWithMemo(dp1, cost, 0);
+        int startFrom1 = solveWithMemo(dp2, cost, 1);
         return min(startFrom0, startFrom1);
     }
 };
@@ -49,41 +46,42 @@ class BottomUp {
     int n;
 
     // O(1*N) & O(1*N)
-    int solveWith1DTable(vector<int>& cost, int startPoint) {
+    int solveWith1DTable(const vector<int>& cost, int start) {
         vector<int> dp(n + 2, -1);
-        dp[n] = dp[n + 1] = 0;
+        dp[n] = 0;
+        dp[n + 1] = 0;
 
-        for(int step = n-1; step >= startPoint; --step) {
-            int climb1Step = dp[step + 1];
-            int climb2Step = dp[step + 2];
-            dp[step] = cost[step] + min(climb1Step, climb2Step);
+        for(int idx = n - 1; idx >= start; --idx) {
+            int climb1Step = dp[idx + 1];
+            int climb2Step = dp[idx + 2];
+            dp[idx] = cost[idx] + min(climb1Step, climb2Step);
         }
 
-        return dp[startPoint];
+        return dp[start];
     }
 
     // O(1*N) & O(1)
-    int solveWithoutTable(vector<int>& cost, int startPoint) {
-        int climb1Step = 0;
-        int climb2Step = 0;
-        int minCost;
+    int solveWithoutTable(const vector<int>& cost, int start) {
+        int dp_idx_1 = 0;
+        int dp_idx_2 = 0;
+        int dp_idx   = 0;
 
-        for(int step = n-1; step >= startPoint; --step) {
-            minCost    = cost[step] + min(climb1Step, climb2Step);
-            climb2Step = climb1Step;
-            climb1Step = minCost;
+        for(int idx = n - 1; idx >= start; --idx) {
+            int climb1Step = dp_idx_1;
+            int climb2Step = dp_idx_2;
+            dp_idx = cost[idx] + min(climb1Step, climb2Step);
+            dp_idx_2 = dp_idx_1;
+            dp_idx_1 = dp_idx;
         }
 
-        return minCost;
+        return dp_idx;
     }
 
 public:
     int minCostToReachTop(vector<int>& cost) {
         n = cost.size();
-
         int startFrom0 = solveWithoutTable(cost, 0);
         int startFrom1 = solveWithoutTable(cost, 1);
-
         return min(startFrom0, startFrom1);
     }
 };
