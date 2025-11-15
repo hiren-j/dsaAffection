c0abb7610f0cbf4c4fc250ab25c89502aef4778c
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-11-15 19:18:25 +0530

    Update Maximize The Cut Segments.cpp

--- Dynamic Programming/1D DP/13 - Maximize The Cut Segments/Maximize The Cut Segments.cpp
+++ Dynamic Programming/1D DP/13 - Maximize The Cut Segments/Maximize The Cut Segments.cpp
@@ -1,53 +1,54 @@
-// Code to find the maximum number of cut segments possible. Given an integer n denoting the Length of a line segment. You need to cut the line segment in such a way that the cut length of a line segment each time is either x , y or z. Here x, y, and z are integers. After performing all the cut operations, your total number of cut segments must be maximum ~ coded by Hiren
+// Code to find the maximum number of cut segments possible. Given an integer n denoting the Length of a line segment. You need to cut the line segment in such a way that the cut len of a line segment each time is either x , y or z. Here x, y, and z are integers. After performing all the cut operations, your total number of cut segments must be maximum ~ coded by Hiren
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
 class TopDown {
     // O(3^N) & O(N)
     int solveWithoutMemo(int n, int x, int y, int z) {
-        if(n == 0) // Edge case: If you've cut the whole line segment then return 0 as an indication
+        if(n == 0) // Edge case: If you've cut the whole line segment then return 0 as indication
             return 0;
 
-        if(n < 0) // Edge case: If you've can't cut the whole line segment then return INT_MIN as an indication
+        if(n < 0) // Edge case: If you've can't cut the whole line segment then return INT_MIN as indication
             return INT_MIN;
 
         if(memory[n] != -1)
             return memory[n];
 
         // There are three cut operations to perform
-        int cutInLenX = solveWithoutMemo(n - x, x, y, z); // Is to cut line segment in length x
-        int cutInLenY = solveWithoutMemo(n - y, x, y, z); // Is to cut line segment in length y
-        int cutInLenZ = solveWithoutMemo(n - z, x, y, z); // Is to cut line segment in length z
+        int cutInLenX = solveWithoutMemo(n - x, x, y, z); // Is to cut line segment in len x
+        int cutInLenY = solveWithoutMemo(n - y, x, y, z); // Is to cut line segment in len y
+        int cutInLenZ = solveWithoutMemo(n - z, x, y, z); // Is to cut line segment in len z
 
-        int maxCuts = max({cutInLenX, cutInLenY, cutInLenZ});                
-        return (maxCuts == INT_MIN) ? INT_MIN : maxCuts + 1;
+        int maxSegments = max({cutInLenX, cutInLenY, cutInLenZ});                
+        return (maxSegments == INT_MIN) ? INT_MIN : maxSegments + 1;
     }
 
     // O(3*N) & O(2*N)
     int solveWithMemo(vector<int>& memory, int n, int x, int y, int z) {
-        if(n == 0) // Edge case: If you've cut the whole line segment then return 0 as an indication
+        if(n == 0) // Edge case: If you've cut the whole line segment then return 0 as indication
             return 0;
 
-        if(n < 0) // Edge case: If you've can't cut the whole line segment then return INT_MIN as an indication
+        if(n < 0) // Edge case: If you've can't cut the whole line segment then return INT_MIN as indication
             return INT_MIN;
 
         if(memory[n] != -1)
             return memory[n];
 
         // There are three cut operations to perform
-        int cutInLenX = solveWithMemo(memory, n - x, x, y, z); // Is to cut line segment in length x
-        int cutInLenY = solveWithMemo(memory, n - y, x, y, z); // Is to cut line segment in length y
-        int cutInLenZ = solveWithMemo(memory, n - z, x, y, z); // Is to cut line segment in length z
+        int cutInLenX = solveWithMemo(memory, n - x, x, y, z); // Is to cut line segment in len x
+        int cutInLenY = solveWithMemo(memory, n - y, x, y, z); // Is to cut line segment in len y
+        int cutInLenZ = solveWithMemo(memory, n - z, x, y, z); // Is to cut line segment in len z
 
-        int maxCuts = max({cutInLenX, cutInLenY, cutInLenZ});                
-        return memory[n] = (maxCuts == INT_MIN) ? INT_MIN : maxCuts + 1;
+        int maxSegments = max({cutInLenX, cutInLenY, cutInLenZ});                
+        return memory[n] = (maxSegments == INT_MIN) ? INT_MIN : maxSegments + 1;
     }
 
 public:
+    // Method to find maximum segments can cut, using recursion with memoization - O(N) & O(N)
     int maximizeTheCuts(int n, int x, int y, int z) {
         vector<int> memory(n + 1, -1);
-        int maxCutSegments = solveWithMemo(memory, n, x, y, z);
-        return (maxCutSegments == INT_MIN) ? 0 : maxCutSegments;
+        int result = solveWithMemo(memory, n, x, y, z);
+        return (result == INT_MIN) ? 0 : result;
     }
 };
 
@@ -60,16 +61,16 @@ public:
         vector<int> dp(n + 1, -1);
         dp[0] = 0; // Initialize first edge case
         
-        for(int length = 1; length <= n; ++length) {
-            int cutInLenX = (length - x >= 0) ? dp[length - x] : INT_MIN;
-            int cutInLenY = (length - y >= 0) ? dp[length - y] : INT_MIN;
-            int cutInLenZ = (length - z >= 0) ? dp[length - z] : INT_MIN;
-            int maxCuts   = max({cutInLenX, cutInLenY, cutInLenZ});
-            dp[length] = (maxCuts == INT_MIN) ? INT_MIN : maxCuts + 1;
+        for(int len = 1; len <= n; ++len) {
+            int cutInLenX = (len - x < 0) ? INT_MIN : dp[len - x];
+            int cutInLenY = (len - y < 0) ? INT_MIN : dp[len - y];
+            int cutInLenZ = (len - z < 0) ? INT_MIN : dp[len - z];
+            int maxSegments = max({cutInLenX, cutInLenY, cutInLenZ});
+            dp[len] = (maxSegments == INT_MIN) ? INT_MIN : maxSegments + 1;
         }
         
-        int maxCutSegments = dp[n];
-        return (maxCutSegments == INT_MIN) ? 0 : maxCutSegments;
+        int result = dp[n];
+        return (result == INT_MIN) ? 0 : result;
     }
 };
 
