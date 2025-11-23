09471eb52557f28e96f454a84ea4d2e714031193
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-11-23 10:25:00 +0530

    Update Dice Combinations.cpp

--- Dynamic Programming/1D DP/26 - Dice Combinations/Dice Combinations.cpp
+++ Dynamic Programming/1D DP/26 - Dice Combinations/Dice Combinations.cpp
@@ -3,7 +3,7 @@
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-    const int MOD = 1e9+7;
+    const int MOD = 1e9 + 7;
 
     // O(6^N) & O(N)
     int solveWithoutMemo(int n) {
@@ -12,7 +12,7 @@ class TopDown {
         
         int count = 0;
  
-        for(int outcome = 1; (outcome <= 6 && outcome <= n); ++outcome) 
+        for(int outcome = 1; (outcome <= 6 && n - outcome >= 0); ++outcome) 
             count = (count + solveWithoutMemo(n - outcome)) % MOD;
  
         return count;
@@ -28,7 +28,7 @@ class TopDown {
     
         int count = 0;
  
-        for(int outcome = 1; (outcome <= 6 && outcome <= n); ++outcome) 
+        for(int outcome = 1; (outcome <= 6 && n - outcome >= 0); ++outcome) 
             count = (count + solveWithMemo(dp, n - outcome)) % MOD;
  
         return dp[n] = count;
@@ -44,18 +44,17 @@ public:
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    const int MOD = 1e9+7;
+    const int MOD = 1e9 + 7;
  
-public:
     // O(N*6) & O(1*N) : Where N = given_n
-    int numWays(int given_n) {
+    int solveWith1DTable(int n) {
         vector<int> dp(given_n + 1, -1);
         dp[0] = 1;
  
         for(int n = 1; n <= given_n; ++n) {
             int count = 0;
 
-            for(int outcome = 1; (outcome <= 6 && outcome <= n); ++outcome) {
+            for(int outcome = 1; (outcome <= 6 && n - outcome >= 0); ++outcome) {
                 count = (count + dp[n - outcome]) % MOD;
             }
  
@@ -63,6 +62,42 @@ public:
         }
  
         return dp[given_n];
+    }
+    
+    // O(N*6) & O(1) : Where N = given_n
+    int solveWithoutTable(int given_n) {
+        int dp_n_1 = 1; // dp[n - 1]
+        int dp_n_2 = 0; // dp[n - 2]
+        int dp_n_3 = 0; // dp[n - 3]
+        int dp_n_4 = 0; // dp[n - 4]
+        int dp_n_5 = 0; // dp[n - 5]
+        int dp_n_6 = 0; // dp[n - 6]
+        int dp_n   = 1; // dp[n]
+
+        for(int n = 1; n <= given_n; ++n) {
+            int count = 0;
+            count = (count + dp_n_1) % MOD;
+            count = (count + dp_n_2) % MOD;
+            count = (count + dp_n_3) % MOD;
+            count = (count + dp_n_4) % MOD;
+            count = (count + dp_n_5) % MOD;
+            count = (count + dp_n_6) % MOD;
+            dp_n  = count;
+
+            dp_n_6 = dp_n_5;
+            dp_n_5 = dp_n_4;
+            dp_n_4 = dp_n_3;
+            dp_n_3 = dp_n_2;
+            dp_n_2 = dp_n_1;
+            dp_n_1 = dp_n;
+        }
+
+        return dp_n;
+    }
+
+public:
+    int numWays(int n) {
+        return solveWith1DTable(n);
     }
 };
 
