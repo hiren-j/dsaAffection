9bbc28df4ac6548aed70bdcf18c95cb53fde39e3
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-11-25 22:39:58 +0530

    Update Decode Ways.cpp

--- Dynamic Programming/1D DP/30 - Decode Ways/Decode Ways.cpp
+++ Dynamic Programming/1D DP/30 - Decode Ways/Decode Ways.cpp
@@ -6,46 +6,46 @@ class TopDown {
     int n;
 
     // O(2^N) & O(N)
-    int solveWithoutMemo(const string& s, int index) {
+    int solveWithoutMemo(const string& s, int i) {
         // Edge case: If all the characters are exhausted then you've decoded the whole string hence you've 1 way
-        if(index == n)
+        if(i == n)
             return 1;
 
         // Edge case: If the character is '0' then you've can't decode it any way
-        if(s[index] == '0')
+        if(s[i] == '0')
             return 0;
 
         // If you're here then it's guaranteed that the character lies within '1' to '9' which can be decoded hence consider it and move to the next character
-        int numWays = solveWithoutMemo(s, index + 1);
+        int numWays = solveWithoutMemo(s, i + 1);
 
         // If you're here then there are two possible ways of decoding: When the current character is '1' then it's guaranteed that the next character will be within '0' to '9'. If the current character is '2' then you can only decode when the next character lies within '0' to '6'
-        if(index + 1 < n && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6')))
-            numWays += solveWithoutMemo(s, index + 2);
+        if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+            numWays += solveWithoutMemo(s, i + 2);
 
         return numWays;
     }
 
     // O(2*N) & O(2*N)
-    int solveWithMemo(vector<int>& dp, const string& s, int index) {
+    int solveWithMemo(vector<int>& dp, const string& s, int i) {
         // Edge case: If all the characters are exhausted then you've decoded the whole string hence you've 1 way
-        if(index == n)
+        if(i == n)
             return 1;
 
         // Edge case: If the character is '0' then you've can't decode it in any way
-        if(s[index] == '0')
+        if(s[i] == '0')
             return 0;
 
-        if(dp[index] != -1)
-            return dp[index];
+        if(dp[i] != -1)
+            return dp[i];
 
         // If you're here then it's guaranteed that the character lies within '1' to '9' which can be decoded hence consider it and move to the next character
-        int numWays = solveWithMemo(dp, s, index + 1);
+        int numWays = solveWithMemo(dp, s, i + 1);
 
         // If you're here then there are two possible ways of decoding: When the current character is '1' then it's guaranteed that the next character will be within '0' to '9'. If the current character is '2' then you can only decode when the next character lies within '0' to '6'
-        if(index + 1 < n && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6')))
-            numWays += solveWithMemo(dp, s, index + 2);
+        if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+            numWays += solveWithMemo(dp, s, i + 2);
         
-        return dp[index] = numWays;
+        return dp[i] = numWays;
     }
 
 public:
@@ -66,16 +66,18 @@ class BottomUp {
         vector<int> dp(n + 1, -1);
         dp[n] = 1; // Init first edge case
 
-        for(int index = n-1; index >= 0; --index) {
-            if(s[index] == '0') { // Handle second edge case
-                dp[index] = 0;
+        for(int i = n - 1; i >= 0; --i) {
+            if(s[i] == '0') { // Handle second edge case
+                dp[i] = 0;
             }
             else {
-                int numWays = dp[index + 1];
-                if(index + 1 < n && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6'))) {
-                    numWays += dp[index + 2];
+                int numWays = dp[i + 1];
+
+                if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
+                    numWays += dp[i + 2];
                 }
-                dp[index] = numWays;
+
+                dp[i] = numWays;
             }
         }
 
@@ -84,26 +86,28 @@ class BottomUp {
 
     // O(1*N) & O(1)
     int solveWithoutTable(const string& s) {
-        int dpIndex1 = 1; // Init first edge case
-        int dpIndex2 = 0; 
-        int dpIndex = 0;
+        int dp_i_1 = 1; // Init first edge case
+        int dp_i_2 = 0; 
+        int dp_i   = 0;
 
-        for(int index = n-1; index >= 0; --index) {
-            if(s[index] == '0') { // Handle second edge case
-                dpIndex = 0;
+        for(int i = n - 1; i >= 0; --i) {
+            if(s[i] == '0') { // Handle second edge case
+                dp_i = 0;
             }
             else {
-                int numWays = dpIndex1;
-                if(index + 1 < n && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6'))) {
-                    numWays += dpIndex2;
+                int numWays = dp_i_1;
+
+                if(i + 1 < n && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
+                    numWays += dp_i_2;
                 }
-                dpIndex = numWays;
+
+                dp_i = numWays;
             }
-            dpIndex2 = dpIndex1;
-            dpIndex1 = dpIndex;
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
         }
 
-        return dpIndex;
+        return dp_i;
     }
 
 public:
