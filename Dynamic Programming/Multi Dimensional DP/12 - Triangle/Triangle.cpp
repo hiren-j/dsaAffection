fa6b6bfe8fdfdb2d938e041f4f0b064bc9a9c4db
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-12-17 11:42:58 +0530

    Update Triangle.cpp

--- Dynamic Programming/Multi Dimensional DP/12 - Triangle/Triangle.cpp
+++ Dynamic Programming/Multi Dimensional DP/12 - Triangle/Triangle.cpp
@@ -1,29 +1,29 @@
-// Code to find the minimum path sum from top to bottom in a triangle ~ coded by Hiren
+// Code to find the minimum path sum from top to bottom in a triangle ~ coded by vHiren
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
-    int N;
+    int M;
 
-    // O(2^(N*N)) & O(N)
-    int solveWithoutMemo(vector<vector<int>>& triangle, int R, int C) {
-        if(R == N)
-            return 0;
-        
+    // O(2^(M*M)) & O(M)
+    int solveWithoutMemo(const vector<vector<int>>& triangle, int R, int C) {
+        if(R == M-1)
+            return triangle[R][C];
+
         int moveToSameCol = solveWithoutMemo(triangle, R+1, C);
         int moveToNextCol = solveWithoutMemo(triangle, R+1, C+1);
 
         return min(moveToSameCol, moveToNextCol) + triangle[R][C];
     }
+    
+    // O(2*M*M) & O(M*M + M)
+    int solveWithMemo(vector<vector<int>>& memory, const vector<vector<int>>& triangle, int R, int C) {
+        if(R == M-1)
+            return triangle[R][C];
 
-    // O(2*N*N) & O(N*N + N)
-    int solveWithMemo(vector<vector<int>>& memory, vector<vector<int>>& triangle, int R, int C) {
-        if(R == N)
-            return 0;
-
-        if(memory[R][C] != -1)
+        if(memory[R][C] != INT_MIN)
             return memory[R][C];
-        
+
         int moveToSameCol = solveWithMemo(memory, triangle, R+1, C);
         int moveToNextCol = solveWithMemo(memory, triangle, R+1, C+1);
 
@@ -31,10 +31,9 @@ class TopDown {
     }
 
 public:
-    // Method to find minimum path sum from top to bottom, using recursion with memoization - O(N*N) & O(N*N)
     int minimumTotal(vector<vector<int>>& triangle) {
-        N = triangle.size();
-        vector<vector<int>> memory(N, vector<int>(N, -1));
+        M = triangle.size();
+        vector<vector<int>> memory(M, vector<int>(M, INT_MIN));
         return solveWithMemo(memory, triangle, 0, 0);
     }
 };
@@ -42,94 +41,101 @@ public:
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
 class BottomUp {
-    int N;
+    int M;
 
-    // O(N*N) & O(N*N)
-    int solveWith2DTable(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(N, vector<int>(N, -1));
+    // O(M*M) & O(M*M)
+    int solveWith2DTable(const vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(M, vector<int>(M, INT_MIN));
 
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = R; C >= 0; --C) {
-                int moveToSameCol = (R+1 < N) ? dp[R+1][C]   : 0;
-                int moveToNextCol = (R+1 < N) ? dp[R+1][C+1] : 0;
-                dp[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];  
-            }
-        }
+        for(int C = 0; C < M; ++C)
+            dp[M-1][C] = triangle[M-1][C];
 
-        return dp[0][0];
-    }
-
-    // O(N*N) & O(N*N)
-    int solveWith2DEnhanced(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(N+1, vector<int>(N+1, 0));
-
-        for(int R = N-1; R >= 0; --R) {
+        for(int R = M-2; R >= 0; --R) {
             for(int C = R; C >= 0; --C) {
                 int moveToSameCol = dp[R+1][C];
                 int moveToNextCol = dp[R+1][C+1];
-                dp[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];  
+                dp[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];
             }
         }
 
         return dp[0][0];
     }
 
-    // O(N*N) & O(2*N)
-    int solveWith1DTable(vector<vector<int>>& triangle) {
-        vector<int> nextRow(N+1, 0), idealRow(N+1, 0);
+    // O(M*M) & O(2*M)
+    int solveWith1DTable(const vector<vector<int>>& triangle) {
+        vector<int> nextRow(M, INT_MIN); // R + 1th row
 
-        for(int R = N-1; R >= 0; --R) {
+        for(int C = 0; C < M; ++C)
+            nextRow[C] = triangle[M-1][C];
+
+        for(int R = M-2; R >= 0; --R) {
+            vector<int> currRow(M, INT_MIN); // Rth row
+
             for(int C = R; C >= 0; --C) {
                 int moveToSameCol = nextRow[C];
                 int moveToNextCol = nextRow[C+1];
-                idealRow[C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];  
+                currRow[C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];
             }
-            nextRow = idealRow;
+
+            swap(nextRow, currRow);
         }
 
         return nextRow[0];
     }
 
-    // O(N*N) & O(N*N)
-    int solveWith2DLessBlocks(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(N);
+    // O(M*M) & O(M*M)
+    int solveWith2DLessBlocks(const vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(M);
+        
+        dp[M-1].resize(M, INT_MIN);
+        for(int C = 0; C < M; ++C)
+            dp[M-1][C] = triangle[M-1][C];
 
-        for(int R = N-1; R >= 0; --R) {
-            dp[R] = vector<int>(R+1, 0);
+        for(int R = M-2; R >= 0; --R) {
+            dp[R].resize(R+1, INT_MIN);
+
             for(int C = R; C >= 0; --C) {
-                int moveToSameCol = (R+1 < N) ? dp[R+1][C]   : 0;
-                int moveToNextCol = (R+1 < N) ? dp[R+1][C+1] : 0;
-                dp[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];  
+                int moveToSameCol = dp[R+1][C];
+                int moveToNextCol = dp[R+1][C+1];
+                dp[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];
             }
         }
 
         return dp[0][0];
     }
 
-    // O(N*N) & O(2*N)
-    int solveWith1DLessBlocks(vector<vector<int>>& triangle) {
-        vector<int> nextRow(N+1, 0);
+    // O(M*M) & O(2*M)
+    int solveWith1DLessBlocks(const vector<vector<int>>& triangle) {
+        vector<int> nextRow(M, INT_MIN); // R + 1th row
 
-        for(int R = N-1; R >= 0; --R) {
-            vector<int> idealRow(R+1, 0);
+        for(int C = 0; C < M; ++C)
+            nextRow[C] = triangle[M-1][C];
+
+        for(int R = M-2; R >= 0; --R) {
+            vector<int> currRow(R+1, INT_MIN); // Rth row
+
             for(int C = R; C >= 0; --C) {
                 int moveToSameCol = nextRow[C];
                 int moveToNextCol = nextRow[C+1];
-                idealRow[C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];  
+                currRow[C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];
             }
-            nextRow = idealRow;
+
+            swap(nextRow, currRow);
         }
 
         return nextRow[0];
     }
 
-    // O(N*N) & O(1)
+    // O(M*M) & O(1)
     int solveWithoutTable(vector<vector<int>>& triangle) {
-        for(int R = N-2; R >= 0; --R) {
+        for(int C = 0; C < M; ++C)
+            triangle[M-1][C] = triangle[M-1][C];
+
+        for(int R = M-2; R >= 0; --R) {
             for(int C = R; C >= 0; --C) {
                 int moveToSameCol = triangle[R+1][C];
                 int moveToNextCol = triangle[R+1][C+1];
-                triangle[R][C] += min(moveToSameCol, moveToNextCol);  
+                triangle[R][C] = min(moveToSameCol, moveToNextCol) + triangle[R][C];
             }
         }
 
@@ -138,7 +144,7 @@ class BottomUp {
 
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        N = triangle.size();
+        M = triangle.size();
         return solveWithoutTable(triangle);
     }
 };
