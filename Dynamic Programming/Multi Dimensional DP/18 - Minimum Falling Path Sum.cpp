365f932c8a217636bf96f816d27457696e2bd052
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-12-22 17:25:46 +0530

    Update 18 - Minimum Falling Path Sum.cpp

--- Dynamic Programming/Multi Dimensional DP/18 - Minimum Falling Path Sum.cpp
+++ Dynamic Programming/Multi Dimensional DP/18 - Minimum Falling Path Sum.cpp
@@ -19,197 +19,287 @@
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
+    const vector<vector<int>> directions = {{1, 0}, {1, -1}, {1, 1}};
     int N;
 
     // O(N * 3^(N*N)) & O(N)
-    int solveWithoutMemo(vector<vector<int>>& grid, int R, int C) {
+    int solveWithoutMemo(const vector<vector<int>>& grid, int R, int C) {
         if(C < 0 || C == N || R == N)
             return INT_MAX;
 
-        int moveToSameCol = solveWithoutMemo(grid, R+1, C);   
-        int moveToPrevCol = solveWithoutMemo(grid, R+1, C-1); 
-        int moveToNextCol = solveWithoutMemo(grid, R+1, C+1); 
+        if(R == N-1)
+            return grid[R][C];
 
-        int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
+        int moveDown      = solveWithoutMemo(grid, R+1, C); 
+        int moveDownLeft  = solveWithoutMemo(grid, R+1, C-1); 
+        int moveDownRight = solveWithoutMemo(grid, R+1, C+1);
 
-        return grid[R][C] + (minElement == INT_MAX ? 0 : minElement);
+        int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+        return (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C];
     }
-
+    
     // O(N + 3*N*N) & O(N*N + N)
-    int solveWithMemo(vector<vector<int>>& dp, vector<vector<int>>& grid, int R, int C) {
+    int solveWithMemo(vector<vector<int>>& dp, const vector<vector<int>>& grid, int R, int C) {
         if(C < 0 || C == N || R == N)
             return INT_MAX;
+
+        if(R == N-1)
+            return grid[R][C];
 
         if(dp[R][C] != INT_MAX)
             return dp[R][C];
 
-        int moveToSameCol = solveWithMemo(dp, grid, R+1, C);   
-        int moveToPrevCol = solveWithMemo(dp, grid, R+1, C-1); 
-        int moveToNextCol = solveWithMemo(dp, grid, R+1, C+1); 
+        int moveDown      = solveWithMemo(dp, grid, R+1, C); 
+        int moveDownLeft  = solveWithMemo(dp, grid, R+1, C-1); 
+        int moveDownRight = solveWithMemo(dp, grid, R+1, C+1);
 
-        int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
+        int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+        return dp[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C];
+    }
 
-        return dp[R][C] = grid[R][C] + (minElement == INT_MAX ? 0 : minElement);
+    // O(N + 3*N*N) & O(N*N + N)
+    int solveWithMemoLoop(vector<vector<int>>& dp, const vector<vector<int>>& grid, int R, int C) {
+        if(C < 0 || C == N || R == N)
+            return INT_MAX;
+
+        if(R == N-1)
+            return grid[R][C];
+
+        if(dp[R][C] != INT_MAX)
+            return dp[R][C];
+
+        int minPathSum = INT_MAX;
+
+        for(const auto& dir : directions) {
+            const int newR = R + dir[0];
+            const int newC = C + dir[1];
+            minPathSum = min(minPathSum, solveWithMemoLoop(dp, grid, newR, newC));
+        }
+
+        return dp[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C];
     }
 
 public:
-    // Method to find the minimum sum of a falling path, using recursion with memoization - O(N*N) & O(N*N)
     int minFallingPathSum(vector<vector<int>>& grid) {
         N = grid.size();
-        
+
         vector<vector<int>> dp(N, vector<int>(N, INT_MAX));
-        
-        int minPathSum = INT_MAX;
-        for(int C = 0; C < N; ++C) // Collect minimum sum from each start point and update result by minimum among all
-            minPathSum = min(minPathSum, solveWithMemo(dp, grid, 0, C));
-        
-        return minPathSum;
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = solveWithMemoLoop(dp, grid, 0, C);
+            result = min(result, currPathSum);
+        }
+
+        return result;
     }
 };
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class BottomUp {
+class Solution {
     int N;
 
     // O(N*N) & O(N*N)
-    int solveWith2DTable(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(N, vector<int>(N, INT_MAX));
-
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = N-1; C >= 0; --C) {
-                int moveToSameCol = (R+1 < N) ? dp[R+1][C] : INT_MAX; 
-                int moveToPrevCol = (R+1 < N && C-1 >= 0) ? dp[R+1][C-1] : INT_MAX;
-                int moveToNextCol = (R+1 < N && C+1 < N)  ? dp[R+1][C+1] : INT_MAX;
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                dp[R][C] = grid[R][C] + (minElement == INT_MAX ? 0 : minElement);
-            }
-        }
-
-        int minPathSum = INT_MAX;
-        for(int C = 0; C < N; ++C) 
-            minPathSum = min(minPathSum, dp[0][C]);
-        
-        return minPathSum;
-    }
-    
-    // O(N*N) & O(N*N)
-    int solveWith2DEnhanced(vector<vector<int>>& grid) {
+    int solveBy2DShifting(const vector<vector<int>>& grid) {
         vector<vector<int>> dp(N+1, vector<int>(N+2, INT_MAX));
 
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = N; C >= 1; --C) {
-                int moveToSameCol = dp[R+1][C]; 
-                int moveToPrevCol = dp[R+1][C-1];
-                int moveToNextCol = dp[R+1][C+1];
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                dp[R][C] = grid[R][C-1] + (minElement == INT_MAX ? 0 : minElement);
+        for(int R = 0; R <= N; ++R)
+            dp[R][-1+1] = INT_MAX;
+        for(int R = 0; R <= N; ++R)
+            dp[R][N+1] = INT_MAX;
+        for(int C = 0; C <= N; ++C)
+            dp[N][C+1] = INT_MAX;
+    
+        for(int C = 0; C < N; ++C)
+            dp[N-1][C+1] = grid[N-1][C];
+
+        for(int R = N-2; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                int moveDown      = dp[R+1][C+1]; 
+                int moveDownLeft  = dp[R+1][C-1+1]; 
+                int moveDownRight = dp[R+1][C+1+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                dp[R][C+1] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
             }
         }
 
-        int minPathSum = INT_MAX;
-        for(int C = 0; C < N; ++C) 
-            minPathSum = min(minPathSum, dp[0][C+1]);
-        
-        return minPathSum;
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = dp[0][C+1];
+            result = min(result, currPathSum);
+        }
+
+        return result;
+    }
+
+    // O(N*N) & O(N*N)
+    int solveBy2DTable_V1(const vector<vector<int>>& grid) {
+        vector<vector<int>> dp(N+1, vector<int>(N+1, INT_MAX));
+
+        for(int R = 0; R <= N; ++R)
+            dp[R][N] = INT_MAX;
+        for(int C = 0; C <= N; ++C)
+            dp[N][C] = INT_MAX;
+    
+        for(int C = 0; C < N; ++C)
+            dp[N-1][C] = grid[N-1][C];
+
+        for(int R = N-2; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                int moveDown      = dp[R+1][C]; 
+                int moveDownLeft  = (C-1 < 0) ? INT_MAX : dp[R+1][C-1]; 
+                int moveDownRight = dp[R+1][C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                dp[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
+            }
+        }
+
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = dp[0][C];
+            result = min(result, currPathSum);
+        }
+
+        return result;
+    }
+
+    // O(N*N) & O(N*N)
+    int solveBy2DTable_V2(const vector<vector<int>>& grid) {
+        vector<vector<int>> dp(N+1, vector<int>(N+1, INT_MAX));
+    
+        for(int C = 0; C < N; ++C)
+            dp[N-1][C] = grid[N-1][C];
+
+        for(int R = N-2; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                int moveDown      = (R+1 == N) ? INT_MAX : dp[R+1][C]; 
+                int moveDownLeft  = (R+1 == N || C-1 < 0)  ? INT_MAX : dp[R+1][C-1]; 
+                int moveDownRight = (R+1 == N || C+1 == N) ? INT_MAX : dp[R+1][C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                dp[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
+            }
+        }
+
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = dp[0][C];
+            result = min(result, currPathSum);
+        }
+
+        return result;
+    }
+
+    // O(N*N) & O(N*N)
+    int solveBy2DEnhanced(const vector<vector<int>>& grid) {
+        vector<vector<int>> dp(N+1, vector<int>(N+1, INT_MAX));
+    
+        for(int C = 0; C < N; ++C)
+            dp[N-1][C] = grid[N-1][C];
+
+        for(int R = N-2; R >= 0; --R) {
+            for(int C = N-1; C >= 0; --C) {
+                int moveDown      = dp[R+1][C]; 
+                int moveDownLeft  = (C-1 < 0) ? INT_MAX : dp[R+1][C-1]; 
+                int moveDownRight = dp[R+1][C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                dp[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
+            }
+        }
+
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = dp[0][C];
+            result = min(result, currPathSum);
+        }
+
+        return result;
     }
 
     // O(N*N) & O(2*N)
-    int solveWith1DTable(vector<vector<int>>& grid) {
-        vector<int> nextRow(N+2, INT_MAX), idealRow(N+2, INT_MAX);
+    int solveBy1DTable(const vector<vector<int>>& grid) {
+        vector<int> nextRow(N+1, INT_MAX); // R + 1th row
+    
+        for(int C = 0; C < N; ++C)
+            nextRow[C] = grid[N-1][C];
 
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = N; C >= 1; --C) {
-                int moveToSameCol = nextRow[C]; 
-                int moveToPrevCol = nextRow[C-1];
-                int moveToNextCol = nextRow[C+1];
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                idealRow[C] = grid[R][C-1] + (minElement == INT_MAX ? 0 : minElement);
+        for(int R = N-2; R >= 0; --R) {
+            vector<int> currRow(N+1, INT_MAX); // Rth row
+
+            for(int C = N-1; C >= 0; --C) {
+                int moveDown      = nextRow[C]; 
+                int moveDownLeft  = (C-1 < 0) ? INT_MAX : nextRow[C-1]; 
+                int moveDownRight = nextRow[C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                currRow[C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
             }
-            nextRow = idealRow;
+
+            swap(nextRow, currRow);
         }
 
-        int minPathSum = INT_MAX;
-        for(int C = 0; C < N; ++C) 
-            minPathSum = min(minPathSum, nextRow[C+1]);
-        
-        return minPathSum;
+        int result = INT_MAX;
+
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = nextRow[C];
+            result = min(result, currPathSum);
+        }
+
+        return result;
     }
 
     // O(N*N) & O(1)
     int solveWithoutTable(vector<vector<int>>& grid) {
-        for(int R = N-1; R >= 0; --R) {
+        for(int C = 0; C < N; ++C)
+            grid[N-1][C] = grid[N-1][C];
+
+        for(int R = N-2; R >= 0; --R) {
             for(int C = N-1; C >= 0; --C) {
-                int moveToSameCol = (R+1 < N) ? grid[R+1][C] : INT_MAX; 
-                int moveToPrevCol = (R+1 < N && C-1 >= 0) ? grid[R+1][C-1] : INT_MAX;
-                int moveToNextCol = (R+1 < N && C+1 < N)  ? grid[R+1][C+1] : INT_MAX;
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                grid[R][C] += (minElement == INT_MAX ? 0 : minElement);
+                int moveDown      = (R+1 == N) ? INT_MAX : grid[R+1][C]; 
+                int moveDownLeft  = (R+1 == N || C-1 < 0)  ? INT_MAX : grid[R+1][C-1]; 
+                int moveDownRight = (R+1 == N || C+1 == N) ? INT_MAX : grid[R+1][C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                grid[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C]; 
             }
         }
 
-        int minPathSum = INT_MAX;
-        for(int C = 0; C < N; ++C) 
-            minPathSum = min(minPathSum, grid[0][C]);
-        
-        return minPathSum;
-    }
+        int result = INT_MAX;
 
-    // O(N*N) & O(N*N)
-    int solveWith2DConcise(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(N+1, vector<int>(N+2, INT_MAX));
-        int minPathSum = INT_MAX;
-
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = N; C >= 1; --C) {
-                int moveToSameCol = dp[R+1][C]; 
-                int moveToPrevCol = dp[R+1][C-1];
-                int moveToNextCol = dp[R+1][C+1];
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                dp[R][C] = grid[R][C-1] + (minElement == INT_MAX ? 0 : minElement);
-                if(R == 0) minPathSum = min(minPathSum, dp[0][C]);
-            }
+        for(int C = 0; C < N; ++C) {
+            int currPathSum = grid[0][C];
+            result = min(result, currPathSum);
         }
-        
-        return minPathSum;
+
+        return result;
     }
-
-    // O(N*N) & O(2*N)
-    int solveWith1DConcise(vector<vector<int>>& grid) {
-        vector<int> nextRow(N+2, INT_MAX), idealRow(N+2, INT_MAX);
-        int minPathSum = INT_MAX;
-
-        for(int R = N-1; R >= 0; --R) {
-            for(int C = N; C >= 1; --C) {
-                int moveToSameCol = nextRow[C]; 
-                int moveToPrevCol = nextRow[C-1];
-                int moveToNextCol = nextRow[C+1];
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                idealRow[C] = grid[R][C-1] + (minElement == INT_MAX ? 0 : minElement);
-                if(R == 0) minPathSum = min(minPathSum, idealRow[C]);
-            }
-            nextRow = idealRow;
-        }
-        
-        return minPathSum;
-    }
-
+    
     // O(N*N) & O(1)
     int solveWithoutTableConcise(vector<vector<int>>& grid) {
-        int minPathSum = INT_MAX;
+        int result = INT_MAX;
 
-        for(int R = N-1; R >= 0; --R) {
+        for(int C = 0; C < N; ++C) {
+            grid[N-1][C] = grid[N-1][C];
+            int currPathSum = grid[0][C];
+            if(N-1 == 0) result = min(result, currPathSum); 
+        }
+
+        for(int R = N-2; R >= 0; --R) {
             for(int C = N-1; C >= 0; --C) {
-                int moveToSameCol = (R+1 < N) ? grid[R+1][C] : INT_MAX; 
-                int moveToPrevCol = (R+1 < N && C-1 >= 0) ? grid[R+1][C-1] : INT_MAX;
-                int moveToNextCol = (R+1 < N && C+1 < N)  ? grid[R+1][C+1] : INT_MAX;
-                int minElement = min({moveToSameCol, moveToPrevCol, moveToNextCol});
-                grid[R][C] += (minElement == INT_MAX ? 0 : minElement);
-                if(R == 0) minPathSum = min(minPathSum, grid[0][C]);
+                int moveDown      = (R+1 == N) ? INT_MAX : grid[R+1][C]; 
+                int moveDownLeft  = (R+1 == N || C-1 < 0)  ? INT_MAX : grid[R+1][C-1]; 
+                int moveDownRight = (R+1 == N || C+1 == N) ? INT_MAX : grid[R+1][C+1];
+                int minPathSum = min({moveDown, moveDownLeft, moveDownRight});
+                grid[R][C] = (minPathSum == INT_MAX) ? INT_MAX : minPathSum + grid[R][C];
+
+                int currPathSum = grid[0][C];
+                if(R == 0) result = min(result, currPathSum); 
             }
         }
 
-        return minPathSum;
+        return result;
     }
 
 public:
@@ -229,10 +319,10 @@ public:
         
         for(int R = N-2; R >= 0; --R) {
             for(int C = N-1; C >= 0; --C) {
-                int moveToSameCol = grid[R+1][C];   
-                int moveToPrevCol = (C-1 >= 0) ? grid[R+1][C-1] : INT_MAX; 
-                int moveToNextCol = (C+1 < N)  ? grid[R+1][C+1] : INT_MAX; 
-                grid[R][C] += min({moveToSameCol, moveToPrevCol, moveToNextCol});
+                int moveDown      = grid[R+1][C];   
+                int moveDownLeft  = (C-1 >= 0) ? grid[R+1][C-1] : INT_MAX; 
+                int moveDownRight = (C+1 < N)  ? grid[R+1][C+1] : INT_MAX; 
+                grid[R][C] += min({moveDown, moveDownLeft, moveDownRight});
             }
         }
 
