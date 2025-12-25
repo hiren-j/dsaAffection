808a55826c4fa760d9c3eb78a79ce74035171891
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-12-25 11:05:51 +0530

    Update 2 - Cherry Pickup II.cpp

--- Dynamic Programming/Multi Dimensional DP/40 - Cherry Pickup II/2 - Cherry Pickup II.cpp
+++ Dynamic Programming/Multi Dimensional DP/40 - Cherry Pickup II/2 - Cherry Pickup II.cpp
@@ -2,71 +2,67 @@
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-/*
-    NOTE: In the previous solution If you notice then R1 and R2 are doing the same thing, we're using them to move to the next row, so instead of taking two variables, 
-          Let's just maintain one variable R. 
-*/
-    
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 class TopDown {
-    vector<vector<int>> directions = {{1,0}, {1,-1}, {1,1}};
+    const vector<vector<int>> directions = {{1, -1}, {1, 0}, {1, 1}};
     int M, N;
 
-    bool isValid(int R, int C) {
-        return R >= 0 && C >= 0 && R < M && C < N;
-    }
-
     // O(9^(M*N)) & O(M)
-    int solveWithoutMemo(vector<vector<int>>& grid, int R, int C1, int C2) {
-        if(!isValid(R, C1) || !isValid(R, C2))
+    int solveWithoutMemo(const vector<vector<int>>& grid, int R, int C1, int C2) {
+        if(R == M || C1 < 0 || C1 == N || C2 < 0 || C2 == N)
             return 0;
 
-        int cellCherry = (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
-        int maxFromAll = 0;
+        int maxPathSum = 0;
 
-        for(auto& dir1 : directions) {
-            int newR = R + 1;
-            for(auto& dir2 : directions) {
-                int newC1 = C1 + dir1[1];
-                int newC2 = C2 + dir2[1];
-                int nextCherry = solveWithoutMemo(grid, newR, newC1, newC2);
-                maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+        for(const auto& dir1 : directions) {
+            const int newR  = R + dir1[0];
+            const int newC1 = C1 + dir1[1];
+
+            for(const auto& dir2 : directions) {
+                const int newC2 = C2 + dir2[1];
+                int nextSideSum = solveWithoutMemo(grid, newR, newC1, newC2);
+                maxPathSum = max(maxPathSum, nextSideSum);
             }
         }
 
-        return maxFromAll;
+        maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+        return maxPathSum;
     }
 
     // O(9*M*N*N) & O(M*N*N + M)
-    int solveWithMemo(vector<vector<vector<int>>>& dp, vector<vector<int>>& grid, int R, int C1, int C2) {
-        if(!isValid(R, C1) || !isValid(R, C2))
+    int solveWithMemo(vector<vector<vector<int>>>& dp, const vector<vector<int>>& grid, int R, int C1, int C2) {
+        if(R == M || C1 < 0 || C1 == N || C2 < 0 || C2 == N)
             return 0;
 
         if(dp[R][C1][C2] != -1)
             return dp[R][C1][C2];
 
-        int cellCherry = (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
-        int maxFromAll = 0;
+        int maxPathSum = 0;
 
-        for(auto& dir1 : directions) {
-            int newR = R + 1;
-            for(auto& dir2 : directions) {
-                int newC1 = C1 + dir1[1];
-                int newC2 = C2 + dir2[1];
-                int nextCherry = solveWithMemo(dp, grid, newR, newC1, newC2);
-                maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+        for(const auto& dir1 : directions) {
+            const int newR = R + dir1[0];
+            const int newC1 = C1 + dir1[1];
+
+            for(const auto& dir2 : directions) {
+                const int newR2 = R + dir2[0];
+                const int newC2 = C2 + dir2[1];
+                int nextSideSum = solveWithMemo(dp, grid, newR, newC1, newC2);
+                maxPathSum = max(maxPathSum, nextSideSum);
             }
         }
 
-        return dp[R][C1][C2] = maxFromAll;
+        maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+        return dp[R][C1][C2] = maxPathSum;
     }
 
 public:
     // Method to find maximum cherries can collect, using recursion with memoization - O(M*N*N) & O(M*N*N)
     int cherryPickup(vector<vector<int>>& grid) {
         M = grid.size(), N = grid[0].size();
-        vector<vector<vector<int>>> dp(M, vector<vector<int>>(N, vector<int>(N, -1)));
+        
+        vector<vector<vector<int>>> dp(M, 
+                vector<vector<int>>(N, 
+                        vector<int>(N, -1)));
+
         return solveWithMemo(dp, grid, 0, 0, N-1);
     }
 };
@@ -74,34 +70,136 @@ public:
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    vector<vector<int>> directions = {{1,0}, {1,-1}, {1,1}};
+    const vector<vector<int>> directions = {{1, -1}, {1, 0}, {1, 1}};
     int M, N;
 
-    bool isValid(int R, int C) {
-        return R >= 0 && C >= 0 && R < M && C < N;
-    }
-
     // O(M*N*N) & O(M*N*N)
-    int solveWith3DTable(vector<vector<int>>& grid) {
-        vector<vector<vector<int>>> dp(M, vector<vector<int>>(N, vector<int>(N, -1)));
+    int solveBy3DShifting(const vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(M+1, 
+                vector<vector<int>>(N+2, 
+                        vector<int>(N+2, -1)));
+        // R == M
+        for(int C1 = 0; C1 <= N; ++C1)
+            for(int C2 = 0; C2 <= N; ++C2)
+                dp[M][C1+1][C2+1] = 0;
+        
+        // C1 < 0
+        for(int R = 0; R <= M; ++R)
+            for(int C2 = 0; C2 <= N; ++C2)
+                dp[R][-1+1][C2+1] = 0;
+
+        // C1 == N
+        for(int R = 0; R <= M; ++R)
+            for(int C2 = 0; C2 <= N; ++C2)
+                dp[R][N+1][C2+1] = 0;
+
+        // C2 == N
+        for(int R = 0; R <= M; ++R)
+            for(int C1 = 0; C1 <= N; ++C1)
+                dp[R][C1+1][N+1] = 0;
+        
+        // C2 < 0
+        for(int R = 0; R <= M; ++R)
+            for(int C1 = 0; C1 <= N; ++C1)
+                dp[R][C1+1][-1+1] = 0;
 
         for(int R = M-1; R >= 0; --R) {
             for(int C1 = N-1; C1 >= 0; --C1) {
                 for(int C2 = 0; C2 <= N-1; ++C2) {
-                    int cellCherry = (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
-                    int maxFromAll = 0;
+                    int maxPathSum = 0;
 
-                    for(auto& dir1 : directions) {
-                        int newR = R + 1;
-                        for(auto& dir2 : directions) {
-                            int newC1 = C1 + dir1[1];
-                            int newC2 = C2 + dir2[1];
-                            int nextCherry = (!isValid(newR, newC1) || !isValid(newR, newC2)) ? 0 : dp[newR][newC1][newC2];
-                            maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+                    for(const auto& dir1 : directions) {
+                        const int newR = R + dir1[0];
+                        const int newC1 = C1 + dir1[1];
+
+                        for(const auto& dir2 : directions) {
+                            const int newR2 = R + dir2[0];
+                            const int newC2 = C2 + dir2[1];
+                            int nextSideSum = dp[newR][newC1 + 1][newC2 + 1];
+                            maxPathSum = max(maxPathSum, nextSideSum);
                         }
                     }
 
-                    dp[R][C1][C2] = maxFromAll;
+                    maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+                    dp[R][C1+1][C2+1] = maxPathSum;
+                }
+            }
+        }
+
+        return dp[0][0+1][N-1+1];
+    }
+
+    // O(M*N*N) & O(M*N*N)
+    int solveBy3DTable(const vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(M+1, 
+                vector<vector<int>>(N+1, 
+                        vector<int>(N+1, -1)));
+        // R == M
+        for(int C1 = 0; C1 <= N; ++C1)
+            for(int C2 = 0; C2 <= N; ++C2)
+                dp[M][C1][C2] = 0;
+        
+        // C1 == N
+        for(int R = 0; R <= M; ++R)
+            for(int C2 = 0; C2 <= N; ++C2)
+                dp[R][N][C2] = 0;
+
+        // C2 == N
+        for(int R = 0; R <= M; ++R)
+            for(int C1 = 0; C1 <= N; ++C1)
+                dp[R][C1][N] = 0;
+        
+        for(int R = M-1; R >= 0; --R) {
+            for(int C1 = N-1; C1 >= 0; --C1) {
+                for(int C2 = 0; C2 <= N-1; ++C2) {
+                    int maxPathSum = 0;
+
+                    for(const auto& dir1 : directions) {
+                        const int newR = R + dir1[0];
+                        const int newC1 = C1 + dir1[1];
+
+                        for(const auto& dir2 : directions) {
+                            const int newR2 = R + dir2[0];
+                            const int newC2 = C2 + dir2[1];
+                            int nextSideSum = (newC1 < 0 || newC2 < 0) ? 0 : dp[newR][newC1][newC2];
+                            maxPathSum = max(maxPathSum, nextSideSum);
+                        }
+                    }
+
+                    maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+                    dp[R][C1][C2] = maxPathSum;
+                }
+            }
+        }
+
+        return dp[0][0][N-1];
+    }
+
+    // O(M*N*N) & O(M*N*N)
+    int solveBy3DEnhanced(const vector<vector<int>>& grid) {
+        vector<vector<vector<int>>> dp(M+1, 
+                vector<vector<int>>(N+1, 
+                        vector<int>(N+1, 0)));
+                        
+        for(int R = M-1; R >= 0; --R) {
+            for(int C1 = N-1; C1 >= 0; --C1) {
+                for(int C2 = 0; C2 <= N-1; ++C2) {
+                    int maxPathSum = 0;
+
+                    for(const auto& dir1 : directions) {
+                        const int newR = R + dir1[0];
+                        const int newC1 = C1 + dir1[1];
+
+                        for(const auto& dir2 : directions) {
+                            const int newR2 = R + dir2[0];
+                            const int newC2 = C2 + dir2[1];
+                            int nextSideSum = (newC1 < 0 || newC2 < 0) ? 0 : dp[newR][newC1][newC2];
+                            maxPathSum = max(maxPathSum, nextSideSum);
+                        }
+                    }
+
+                    maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+                    dp[R][C1][C2] = maxPathSum;
                 }
             }
         }
@@ -110,38 +208,43 @@ class BottomUp {
     }
 
     // O(M*N*N) & O(2*N*N)
-    int solveWith2DTable(vector<vector<int>>& grid) {
-        vector<vector<int>> nextRow(N, vector<int>(N, -1)), idealRow(N, vector<int>(N, -1));        
-
+    int solveBy2DTable(const vector<vector<int>>& grid) { 
+        vector<vector<int>> next(N+1, vector<int>(N+1, 0)); // R + 1th table
+                        
         for(int R = M-1; R >= 0; --R) {
+            vector<vector<int>> curr(N+1, vector<int>(N+1, 0)); // Rth table
+
             for(int C1 = N-1; C1 >= 0; --C1) {
                 for(int C2 = 0; C2 <= N-1; ++C2) {
-                    int cellCherry = (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
-                    int maxFromAll = 0;
-                    
-                    for(auto& dir1 : directions) {
-                        int newR = R + 1;
-                        for(auto& dir2 : directions) {
-                            int newC1 = C1 + dir1[1];
-                            int newC2 = C2 + dir2[1];
-                            int nextCherry = (!isValid(newR, newC1) || !isValid(newR, newC2)) ? 0 : nextRow[newC1][newC2];
-                            maxFromAll = max(maxFromAll, nextCherry + cellCherry);
+                    int maxPathSum = 0;
+
+                    for(const auto& dir1 : directions) {
+                        const int newR = R + dir1[0];
+                        const int newC1 = C1 + dir1[1];
+
+                        for(const auto& dir2 : directions) {
+                            const int newR2 = R + dir2[0];
+                            const int newC2 = C2 + dir2[1];
+                            int nextSideSum = (newC1 < 0 || newC2 < 0) ? 0 : next[newC1][newC2];
+                            maxPathSum = max(maxPathSum, nextSideSum);
                         }
                     }
 
-                    idealRow[C1][C2] = maxFromAll;
+                    maxPathSum += (C1 == C2) ? grid[R][C1] : grid[R][C1] + grid[R][C2];
+                    curr[C1][C2] = maxPathSum;
                 }
             }
-            nextRow = idealRow;
+
+            swap(next, curr);
         }
 
-        return nextRow[0][N-1];
+        return next[0][N-1];
     }
 
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         M = grid.size(), N = grid[0].size();
-        return solveWith2DTable(grid);
+        return solveBy2DTable(grid);
     }
 };
 
