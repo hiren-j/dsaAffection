604d2584839d48174f992c603594628269180776
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2025-12-30 09:41:30 +0530

    Update 30 - Ways To Express an Integer as Sum of Powers.cpp

--- Dynamic Programming/Multi Dimensional DP/30 - Ways To Express an Integer as Sum of Powers.cpp
+++ Dynamic Programming/Multi Dimensional DP/30 - Ways To Express an Integer as Sum of Powers.cpp
@@ -1,65 +1,60 @@
-// Code to find the number of ways n can be expressed as the sum of the xth power of unique positive integers ~ coded by Hiren
-
------------------------------------------------------------------------------------------------------------------------------------------------------
-
-/*
-    DON'T FORGET, REGARDING INTUITION BEHIND THE PRECOMPUTATION OF POWERS:
-        : xthPower[i] represents pow(i, x) where i <= n.
-        : Brute forcely we could use pow(i, x) but this will lead to TLE, due to log(x) more increament in time.
-        : So It would be beneficial to precomute the xth power of every possible number (i <= n).
-*/
+// Code to find the number of ways n can be expressed as the sum of the xth power of unique positive integers ~ coded by vHiren
     
 -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
     const int MOD = 1e9 + 7;
-    vector<int> xthPower;
+    vector<long long> myPower;
 
-    void precomputeXthPower(int n, int x) {
-        xthPower.resize(n + 1);
+    // O(NLogX) Time
+    void precomputePowerExponentX(int n, int x) {
+        myPower.resize(n + 1);
 
-        for(int i = 1; i <= n; ++i) {
-            xthPower[i] = (long long)pow(i, x);
+        for(int v = 1; v <= n; ++v) {
+            myPower[v] = pow(v, x);
         }
     }
 
-    // O(N^N) & O(2*N)
-    int solveWithoutMemo(int n, int start, int x) {
+    // O(N^N) & O(N)
+    int solveWithoutMemo(int n, int x, int start) {
         if(n == 0)
             return 1;
-
+        
         int count = 0;
 
-        for(int i = start; (i <= n && xthPower[i] <= n); ++i) {
-            count = (count + solveWithoutMemo(n - xthPower[i], i + 1, x)) % MOD;
+        for(int v = start; (v <= n && myPower[v] <= n); ++v) {
+            int next_n    = n - myPower[v];
+            int nextCount = solveWithoutMemo(next_n, x, v + 1);
+            count = (count + nextCount) % MOD;
         }
 
         return count;
     }
 
-    // O(N*N*N) & O(N*N + 2*N)
-    int solveWithMemo(vector<vector<int>>& dp, int n, int start, int x) {
+    // O(N^3) & O(N*N + N)
+    int solveWithMemo(vector<vector<int>>& dp, int n, int x, int start) {
         if(n == 0)
             return 1;
 
         if(dp[n][start] != -1)
             return dp[n][start];
-
+        
         int count = 0;
 
-        for(int i = start; (i <= n && xthPower[i] <= n); ++i) {
-            count = (count + solveWithMemo(dp, n - xthPower[i], i + 1, x)) % MOD;
+        for(int v = start; (v <= n && myPower[v] <= n); ++v) {
+            int next_n    = n - myPower[v];
+            int nextCount = solveWithMemo(dp, next_n, x, v + 1);
+            count = (count + nextCount) % MOD;
         }
 
         return dp[n][start] = count;
     }
 
 public:
-    // Method to find number of ways, using recursion with memoization - O(N^3) & O(N^2)
     int numberOfWays(int n, int x) {
-        precomputeXthPower(n, x);
+        precomputePowerExponentX(n, x);
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return solveWithMemo(dp, n, 1, x);
+        return solveWithMemo(dp, n, x, 1);
     }
 };
 
@@ -67,45 +62,44 @@ public:
 
 class BottomUp {
     const int MOD = 1e9 + 7;
-    vector<int> xthPower;
+    vector<long long> myPower;
 
-    void precomputeXthPower(int n, int x) {
-        xthPower.resize(n + 1);
+    // O(NLogX) Time
+    void precomputePowerExponentX(int n, int x) {
+        myPower.resize(n + 1);
 
-        for(int i = 1; i <= n; ++i) {
-            xthPower[i] = (long long)pow(i, x);
+        for(int v = 1; v <= n; ++v) {
+            myPower[v] = pow(v, x);
         }
     }
 
-    // O(N^3) & O(N^2)
-    int solveWith2DTable(int n, int x) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 2, 0));
+    int solveBy2DTable(int given_n, int x) {
+        vector<vector<int>> dp(given_n + 1, vector<int>(given_n + 2, -1));
 
-        for(int start = 0; start <= n + 1; ++start)
+        for(int start = 0; start <= given_n + 1; ++start)
             dp[0][start] = 1;
 
-        for(int num = 1; num <= n; ++num) {
-            for(int start = n; start >= 1; --start) {
+        for(int n = 1; n <= given_n; ++n) {
+            for(int start = given_n; start >= 1; --start) {
                 int count = 0;
 
-                for(int i = start; (i <= num && xthPower[i] <= num); ++i) {
-                    int idx = num - xthPower[i];
-                    if(idx >= 0 && idx <= n) {
-                        count = (count + dp[idx][i + 1]) % MOD;
-                    }
+                for(int v = start; (v <= n && myPower[v] <= n); ++v) {
+                    int next_n    = n - myPower[v];
+                    int nextCount = dp[next_n][v + 1];
+                    count = (count + nextCount) % MOD;
                 }
 
-                dp[num][start] = count;
+                dp[n][start] = count;
             }
-        }
+        } 
 
-        return dp[n][1];
+        return dp[given_n][1];
     }
 
 public:
     int numberOfWays(int n, int x) {
-        precomputeXthPower(n, x);
-        return solveWith2DTable(n, x);
+        precomputePowerExponentX(n, x);
+        return solveBy2DTable(n, x);
     }
 };
 
