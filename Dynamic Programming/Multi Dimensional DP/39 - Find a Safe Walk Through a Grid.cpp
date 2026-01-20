077424aa1304204cbf887e712223b20569b59788
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-01-20 15:27:46 +0530

    Update 39 - Find a Safe Walk Through a Grid.cpp

--- Dynamic Programming/Multi Dimensional DP/39 - Find a Safe Walk Through a Grid.cpp
+++ Dynamic Programming/Multi Dimensional DP/39 - Find a Safe Walk Through a Grid.cpp
@@ -4,7 +4,8 @@
 
 /*
   DON'T IGNORE MUST READ: Creating a correct bottom-up solution for this problem is not possible and also memoization could also fail in such problems! Why? Watch The Video Lecture to know about it.
-                          And I would say there are only 3 problems in the whole series which doesn't contain the bottom-up solution. These 3 problems lies in this folder (Multi-Dimensional DP Folder).
+                          And I would say there are only 3 problems in the whole series which doesn't contain the bottom-up solution due to the reasons specified in Video Lecture. 
+                          These 3 problems lies in this folder (Multi-Dimensional DP Folder).
 */
   
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
