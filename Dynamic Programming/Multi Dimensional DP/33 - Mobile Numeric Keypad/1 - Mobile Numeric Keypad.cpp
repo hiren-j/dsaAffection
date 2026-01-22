df0bf7fdbf939959f972ec35504c92749148909d
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-01-22 11:11:06 +0530

    Update 1 - Mobile Numeric Keypad.cpp

--- Dynamic Programming/Multi Dimensional DP/33 - Mobile Numeric Keypad/1 - Mobile Numeric Keypad.cpp
+++ Dynamic Programming/Multi Dimensional DP/33 - Mobile Numeric Keypad/1 - Mobile Numeric Keypad.cpp
@@ -1,73 +1,69 @@
-// Code to find the number of possible unique sequences of len n that you can create by pressing buttons (You can start from any digit) ~ coded by Hiren
+// Code to find the number of possible unique sequences of len n that you can create by pressing buttons (You can start from any digit) ~ coded by vHiren
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {    
-    #define rowLimit 4
-    #define colLimit 3
+    const int rowLimit = 4, colLimit = 3;
+    const vector<vector<int>> dirs   = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    const vector<vector<int>> keypad = {{1, 2, 3}, 
+                                        {4, 5, 6}, 
+                                        {7, 8, 9}, 
+                                        {-1, 0, -1}};
 
-    vector<vector<int>> directions = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    vector<vector<int>> keypad     = {{1, 2, 3},
-                                      {4, 5, 6},
-                                      {7, 8, 9},
-                                      {-1, 0, -1}};
-    // O(10 * 5^N) & O(N) 
+    // O(RL*CL * 5^N) & O(N) : Where RL = rowLimit, CL = colLimit
     int solveWithoutMemo(int N, int R, int C) {
-        // Edge case: No number exists outside of the keypad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
         if(R < 0 || C < 0 || R == rowLimit || C == colLimit || keypad[R][C] == -1)
             return 0;
             
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
         if(N == 0)
             return 1;
-                    
+        
         int count = 0;
         
-        for(auto& dir : directions) {
-            int newR = R + dir[0];
-            int newC = C + dir[1];
-            count += solveWithoutMemo(N - 1, newR, newC);
+        for(const auto& D : dirs) {
+            int newR = R + D[0];
+            int newC = C + D[1];
+            int nextCount = solveWithoutMemo(N - 1, newR, newC);
+            count += nextCount;
         }
-
+        
         return count;
     }
-
-    // O(10 + 5*N*4*3) & O(N*4*3 + N)
+    
+    // O(RL*CL + 5*N*RL*CL) & O(N*RL*CL + N) : Where RL = rowLimit, CL = colLimit
     int solveWithMemo(vector<vector<vector<int>>>& dp, int N, int R, int C) {
-        // Edge case: No number exists outside of the keypad and if you try to look for it or you've pressed '*' or '#' then you've no valid way
         if(R < 0 || C < 0 || R == rowLimit || C == colLimit || keypad[R][C] == -1)
             return 0;
             
-        // Edge case: If you've correctly dialed a phone number of length N then you've one valid way
         if(N == 0)
             return 1;
-        
+            
         if(dp[N][R][C] != -1)
             return dp[N][R][C];
-            
+        
         int count = 0;
         
-        for(auto& dir : directions) {
-            int newR = R + dir[0];
-            int newC = C + dir[1];
-            count += solveWithMemo(dp, N - 1, newR, newC);
+        for(const auto& D : dirs) {
+            int newR = R + D[0];
+            int newC = C + D[1];
+            int nextCount = solveWithMemo(dp, N - 1, newR, newC);
+            count += nextCount;
         }
         
         return dp[N][R][C] = count;
     }
     
 public:
-    // Method to count total distinct phone numbers of length N you can dial, using recursion with memoization - O(N) & O(N)
+    // Method to count unique phone numbers of length N can dial, using recursion with memoization - O(N) & O(N)
     int countUniqueSeqsOfLen(int N) {
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(rowLimit, vector<int>(colLimit, -1)));
-        int result = 0;        
+        int result = 0;
         
-        // Start dialing from the cells which doesn't starts from '*' or '#'
-        for(int R = 0; R < rowLimit; ++R) 
-            for(int C = 0; C < colLimit; ++C) 
+        for(int R = 0; R < rowLimit; ++R)
+            for(int C = 0; C < colLimit; ++C)
                 if(keypad[R][C] != -1)
                     result += solveWithMemo(dp, N - 1, R, C);
-
+        
         return result;
     }
 };
@@ -75,51 +71,122 @@ public:
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    #define rowLimit 4
-    #define colLimit 3
-
-    vector<vector<int>> directions = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    vector<vector<int>> keypad     = {{1, 2, 3},
-                                      {4, 5, 6},
-                                      {7, 8, 9},
-                                      {-1, 0, -1}};
-public:
-    // O(N*4*3*5) & O(N*4*3)
-    int countUniqueSeqsOfLen(int N) {
-        vector<vector<vector<int>>> dp(N, vector<vector<int>>(rowLimit, vector<int>(colLimit, -1)));
-
-        // Initialize second edge case
-        for(int R = 0; R < rowLimit; ++R)
-            for(int C = 0; C < colLimit; ++C)
+    const int rowLimit = 4, colLimit = 3;
+    const vector<vector<int>> dirs   = {{0, 0}, {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    const vector<vector<int>> keypad = {{1, 2, 3}, 
+                                        {4, 5, 6}, 
+                                        {7, 8, 9}, 
+                                        {-1, 0, -1}};
+    
+    // O(GN*RL*CL*5) & O(GN*RL*CL) : Where GN = given_N, Where RL = rowLimit, CL = colLimit
+    int solveBy3DTable(int given_N) {
+        vector<vector<vector<int>>> dp(given_N + 1, vector<vector<int>>(rowLimit + 1, vector<int>(colLimit + 1, -1)));
+    
+        // N == 0
+        for(int R = 0; R <= rowLimit; ++R)
+            for(int C = 0; C <= colLimit; ++C)
                 dp[0][R][C] = 1;
-
-        for(int len = 1; len < N; ++len) {
-            for(int R = rowLimit-1; R >= 0; --R) {
-                for(int C = colLimit-1; C >= 0; --C) {
+                
+        for(int N = 1; N <= given_N - 1; ++N) {
+            for(int R = rowLimit - 1; R >= 0; --R) {
+                for(int C = colLimit - 1; C >= 0; --C) {
                     int count = 0;
                     
-                    for(auto& dir : directions) {
-                        int newR = R + dir[0];
-                        int newC = C + dir[1];
-                        if(newR >= 0 && newC >= 0 && newR < rowLimit && newC < colLimit && keypad[newR][newC] != -1) {
-                            count += dp[len - 1][newR][newC];
-                        }
+                    for(const auto& D : dirs) {
+                        int newR = R + D[0];
+                        int newC = C + D[1];
+                        int nextCount = (newR < 0 || newC < 0 || newR == rowLimit || newC == colLimit || keypad[newR][newC] == -1) 
+                                        ? 0 
+                                        : dp[N - 1][newR][newC];
+                        count += nextCount;
                     }
                     
-                    dp[len][R][C] = count; 
+                    dp[N][R][C] = count;
                 }
             }
         }
-
-        int result = 0;        
-
-        // Start dialing from the cells which doesn't starts from '*' or '#'
-        for(int R = 0; R < rowLimit; ++R) 
-            for(int C = 0; C < colLimit; ++C) 
+        
+        int result = 0;
+        
+        for(int R = 0; R < rowLimit; ++R)
+            for(int C = 0; C < colLimit; ++C)
                 if(keypad[R][C] != -1)
-                    result += dp[N - 1][R][C];
-
+                    result += dp[given_N - 1][R][C];
+        
         return result;
+    }
+    
+    // O(GN*RL*CL*5) & O(GN*RL*CL) : Where GN = given_N, Where RL = rowLimit, CL = colLimit
+    int solveBy3DEnhanced(int given_N) {
+        vector<vector<vector<int>>> dp(given_N + 1, vector<vector<int>>(rowLimit + 1, vector<int>(colLimit + 1, 1)));
+                
+        for(int N = 1; N <= given_N - 1; ++N) {
+            for(int R = rowLimit - 1; R >= 0; --R) {
+                for(int C = colLimit - 1; C >= 0; --C) {
+                    int count = 0;
+                    
+                    for(const auto& D : dirs) {
+                        int newR = R + D[0];
+                        int newC = C + D[1];
+                        int nextCount = (newR < 0 || newC < 0 || newR == rowLimit || newC == colLimit || keypad[newR][newC] == -1) 
+                                        ? 0 
+                                        : dp[N - 1][newR][newC];
+                        count += nextCount;
+                    }
+                    
+                    dp[N][R][C] = count;
+                }
+            }
+        }
+        
+        int result = 0;
+        
+        for(int R = 0; R < rowLimit; ++R)
+            for(int C = 0; C < colLimit; ++C)
+                if(keypad[R][C] != -1)
+                    result += dp[given_N - 1][R][C];
+        
+        return result;
+    }
+    
+    // O(GN*RL*CL*5) & O(2*RL*CL) : Where GN = given_N, Where RL = rowLimit, CL = colLimit
+    int solveBy2DTable(int given_N) {
+        vector<vector<int>> prev(rowLimit + 1, vector<int>(colLimit + 1, 1));  // N - 1th table
+        vector<vector<int>> curr(rowLimit + 1, vector<int>(colLimit + 1, -1)); // Nth table
+    
+        for(int N = 1; N <= given_N - 1; ++N) {
+            for(int R = rowLimit - 1; R >= 0; --R) {
+                for(int C = colLimit - 1; C >= 0; --C) {
+                    int count = 0;
+                    
+                    for(const auto& D : dirs) {
+                        int newR = R + D[0];
+                        int newC = C + D[1];
+                        int nextCount = (newR < 0 || newC < 0 || newR == rowLimit || newC == colLimit || keypad[newR][newC] == -1) 
+                                        ? 0 
+                                        : prev[newR][newC];
+                        count += nextCount;
+                    }
+                    
+                    curr[R][C] = count;
+                }
+            }
+            swap(prev, curr);
+        }
+        
+        int result = 0;
+        
+        for(int R = 0; R < rowLimit; ++R)
+            for(int C = 0; C < colLimit; ++C)
+                if(keypad[R][C] != -1)
+                    result += prev[R][C];
+        
+        return result;
+    }
+    
+public:
+    int countUniqueSeqsOfLen(int N) {
+        return solveBy2DTable(N);
     }
 };
 
