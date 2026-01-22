35ffde1c4cff1c60028f8bc9544007509a1853df
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-01-22 11:48:38 +0530

    Update Knight Dialer.cpp

--- Dynamic Programming/Multi Dimensional DP/34 - Knight Dialer/Knight Dialer.cpp
+++ Dynamic Programming/Multi Dimensional DP/34 - Knight Dialer/Knight Dialer.cpp
@@ -11,7 +11,7 @@ class TopDown {
                                         {7, 8, 9}, 
                                         {-1, 0, -1}};
 
-    // O((RL*CL) * (8^N)) & O(N)
+    // O(RL*CL * 8^N) & O(N)
     int solveWithoutMemo(int N, int R, int C) {
         if(R < 0 || C < 0 || R >= rowLimit || C >= colLimit || numpad[R][C] == -1)
             return 0;
@@ -31,7 +31,7 @@ class TopDown {
         return count;
     }
 
-    // O(N*RL*CL) & O(N*RL*CL)
+    // O(RL*CL + 8*N*RL*CL) & O(N*RL*CL)
     int solveWithMemo(vector<vector<vector<int>>>& dp, int N, int R, int C) {
         if(R < 0 || C < 0 || R >= rowLimit || C >= colLimit || numpad[R][C] == -1)
             return 0;
