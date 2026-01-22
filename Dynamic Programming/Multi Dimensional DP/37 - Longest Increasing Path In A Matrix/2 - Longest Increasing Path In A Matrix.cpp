d2a2242f9fc57e1e2cb2161da1f82c23f8976b5c
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-01-22 15:12:12 +0530

    Update 2 - Longest Increasing Path In A Matrix.cpp

--- Dynamic Programming/Multi Dimensional DP/37 - Longest Increasing Path In A Matrix/2 - Longest Increasing Path In A Matrix.cpp
+++ Dynamic Programming/Multi Dimensional DP/37 - Longest Increasing Path In A Matrix/2 - Longest Increasing Path In A Matrix.cpp
@@ -7,11 +7,6 @@
     DON'T IGNORE MUST READ: For the bottom-up solution of this problem, I created it and run it but it didn't passed all the testcases.
                             So I would say there are only 3 problems in the whole series which doesn't contain the bottom-up solution 
                             due to this reason. These 3 problems lies in this folder (Multi-Dimensional DP Folder).
-
-    REGARDING INTUITION OF THE SOLUTION: In the previous solution, we're doing backtracking, In this one we're not doing backtracking. Because If
-                                         you notice then It's not required, as we need to consider strictly increasing paths, which means from a cell
-                                         (R, C) we'll only move to cells (newR, newC) which contains strictly greater value, which means we'll never 
-                                         end up having a stack-overflow or endless recursion.
 */
     
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
