bcbe097ef1b1798b09fba4b5cc148b382de05c9b
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-02-11 20:17:23 +0530

    Update new_problem - check subarray sum k.cpp

--- Dynamic Programming/Subarrays DP/new_problem - check subarray sum k.cpp
+++ Dynamic Programming/Subarrays DP/new_problem - check subarray sum k.cpp
@@ -1,4 +1,4 @@
-// Code to check whether there exists a subarray with sum k ~ coded by vHiren
+// Code to check whether there exists a non-empty subarray with sum k ~ coded by vHiren
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,15 +8,15 @@ class TopDown {
 
     // O(2^N) & O(N)
     bool solveWithoutMemo(const vector<int>& nums, int i, bool prevPick, int k) {
-        if(k == 0)
-            return true;
-
-        if(k < 0 || i == n)
+        if(k < 0)
             return false;
+
+        if(i == n)
+            return (prevPick == true && k == 0) ? true : false;
 
         if(prevPick) {
             bool pickInSubarr = solveWithoutMemo(nums, i + 1, true, k - nums[i]);
-            bool stopHere = (k == 0) ? true : false;
+            bool stopHere = (prevPick == true && k == 0) ? true : false;
             return (pickInSubarr || stopHere);
         }
         else {
@@ -28,18 +28,18 @@ class TopDown {
 
     // O(N*K) & O(N*K)
     bool solveWithMemo(vector<vector<vector<int>>>& dp, const vector<int>& nums, int i, bool prevPick, int k) {
-        if(k == 0)
-            return true;
-
-        if(k < 0 || i == n)
+        if(k < 0)
             return false;
+
+        if(i == n)
+            return (prevPick == true && k == 0) ? true : false;
 
         if(dp[i][prevPick][k] != -1)
             return dp[i][prevPick][k];
 
         if(prevPick) {
             bool pickInSubarr = solveWithMemo(dp, nums, i + 1, true, k - nums[i]);
-            bool stopHere = (k == 0) ? true : false;
+            bool stopHere = (prevPick == true && k == 0) ? true : false;
             return dp[i][prevPick][k] = (pickInSubarr || stopHere);
         }
         else {
@@ -64,51 +64,16 @@ class BottomUp {
     bool solveBy3DTable(const vector<int>& nums, int given_k) {
         vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(given_k + 1, -1)));
         
-        // k == 0
-        for(int i = 0; i <= n; ++i)
-            for(int prevPick = 0; prevPick < 2; ++prevPick)
-                dp[i][prevPick][0] = true;
-
-        // i == n
         for(int prevPick = 0; prevPick < 2; ++prevPick)
             for(int k = 0; k <= given_k; ++k)
-                dp[n][prevPick][k] = false;
+                dp[n][prevPick][k] = (prevPick == true && k == 0) ? true : false;
 
         for(int i = n - 1; i >= 0; --i) {
             for(int prevPick = 1; prevPick >= 0; --prevPick) {
-                for(int k = 1; k <= given_k; ++k) {
+                for(int k = 0; k <= given_k; ++k) {
                     if(prevPick) {
                         bool pickInSubarr = (k - nums[i] < 0) ? false : dp[i + 1][true][k - nums[i]];
-                        bool stopHere = (k == 0) ? true : false;
-                        dp[i][prevPick][k] = (pickInSubarr || stopHere);
-                    }
-                    else {
-                        bool startNewFromCurr = (k - nums[i] < 0) ? false : dp[i + 1][true][k - nums[i]];
-                        bool startNewFromNext = dp[i + 1][false][k];
-                        dp[i][prevPick][k] = (startNewFromNext || startNewFromCurr);
-                    }
-                }
-            }
-        }
-
-        return dp[0][false][given_k];
-    }
-
-    // O(N*GK) & O(N*GK) : Where GK = given_k
-    bool solveBy3DEnhanced(const vector<int>& nums, int given_k) {
-        vector<vector<vector<bool>>> dp(n + 1, vector<vector<bool>>(2, vector<bool>(given_k + 1, false)));
-        
-        // k == 0
-        for(int i = 0; i <= n; ++i)
-            for(int prevPick = 0; prevPick < 2; ++prevPick)
-                dp[i][prevPick][0] = true;
-
-        for(int i = n - 1; i >= 0; --i) {
-            for(int prevPick = 1; prevPick >= 0; --prevPick) {
-                for(int k = 1; k <= given_k; ++k) {
-                    if(prevPick) {
-                        bool pickInSubarr = (k - nums[i] < 0) ? false : dp[i + 1][true][k - nums[i]];
-                        bool stopHere = (k == 0) ? true : false;
+                        bool stopHere = (prevPick == true && k == 0) ? true : false;
                         dp[i][prevPick][k] = (pickInSubarr || stopHere);
                     }
                     else {
@@ -125,20 +90,19 @@ class BottomUp {
 
     // O(N*K) & O(GK) : Where GK = given_k
     bool solveBy2DTable(const vector<int>& nums, int given_k) {
-        vector<vector<bool>> next(2, vector<bool>(given_k + 1, false));
-        vector<vector<bool>> curr(2, vector<bool>(given_k + 1, false));
-        
-        for(int prevPick = 0; prevPick < 2; ++prevPick) // Base case k == 0 for (i == n)
-            next[prevPick][0] = true;
+        vector<vector<int>> next(2, vector<int>(given_k + 1, -1));
+        vector<vector<int>> curr(2, vector<int>(given_k + 1, -1));
+
+        for(int prevPick = 0; prevPick < 2; ++prevPick)
+            for(int k = 0; k <= given_k; ++k)
+                next[prevPick][k] = (prevPick == true && k == 0) ? true : false;
 
         for(int i = n - 1; i >= 0; --i) {
             for(int prevPick = 1; prevPick >= 0; --prevPick) {
-                curr[prevPick][0] = true; // Base case k == 0 for (i >= 0 && i <= n - 1)
-
-                for(int k = 1; k <= given_k; ++k) {
+                for(int k = 0; k <= given_k; ++k) {
                     if(prevPick) {
                         bool pickInSubarr = (k - nums[i] < 0) ? false : next[true][k - nums[i]];
-                        bool stopHere = (k == 0) ? true : false;
+                        bool stopHere = (prevPick == true && k == 0) ? true : false;
                         curr[prevPick][k] = (pickInSubarr || stopHere);
                     }
                     else {
@@ -148,7 +112,6 @@ class BottomUp {
                     }
                 }
             }
-
             swap(next, curr);
         }
 
@@ -168,7 +131,7 @@ int main() {
     cin.tie(nullptr);
 
     vector<int> nums = {1, 4, 3, 7, 2};
-    const int k = 9;
+    const int k = 14;
 
     TopDown td;
     if(td.isSubarraySumK(nums, k)) {
