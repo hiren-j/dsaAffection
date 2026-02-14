e66035937e45c9da9d4fe7c02c4ca4d8435dcb8c
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-02-14 10:28:32 +0530

    Create Count Number of Subsequences.cpp

---
+++ Dynamic Programming/Subsequences DP/0 - Count Number of Subsequences/Count Number of Subsequences.cpp
@@ -0,0 +1,97 @@
+// Code to count total number of subsequences / subsets in an array ~ coded by vHiren
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class TopDown {
+	int n;
+
+	// O(2^N) & O(N)
+	int solveWithoutMemo(const vector<int>& nums, int i) {
+		if(i == n)
+			return 1;
+
+		int currTake = solveWithoutMemo(nums, i + 1);
+		int currSkip = solveWithoutMemo(nums, i + 1);
+
+		return (currSkip + currTake);
+	}
+
+	// O(2*N) & O(2*N)
+	int solveWithMemo(vector<int>& dp, const vector<int>& nums, int i) {
+		if(i == n)
+			return 1;
+
+		if(dp[i] != -1)
+			return dp[i];
+
+		int currTake = solveWithMemo(dp, nums, i + 1);
+		int currSkip = solveWithMemo(dp, nums, i + 1);
+
+		return dp[i] = (currSkip + currTake);
+	}
+
+public:
+	int countSubsequences(const vector<int>& nums) {
+		n = nums.size();
+		vector<int> dp(n, -1);
+		return solveWithMemo(dp, nums, 0);
+	}
+};
+
+class BottomUp {
+	int n;
+
+	// O(N) & O(N)
+	int solveWith1DTable(const vector<int>& nums) {
+		vector<int> dp(n + 1, -1);
+		dp[n] = 1;
+
+		for(int i = n - 1; i >= 0; --i) {
+			int currTake = dp[i + 1];
+			int currSkip = dp[i + 1];
+			dp[i] = (currSkip + currTake);
+		}
+
+		return dp[0];
+	}
+
+	// O(N) & O(1)
+	int solveWithoutTable(const vector<int>& nums) {
+		int next_val = 1; // dp[i + 1];
+		int curr_val = 1; // dp[i];
+
+		for(int i = n - 1; i >= 0; --i) {
+			int currTake = next_val;
+			int currSkip = next_val;
+			curr_val = (currSkip + currTake);
+			next_val = curr_val;
+		}
+
+		return next_val;
+	}
+
+public:
+	int countSubsequences(const vector<int>& nums) {
+		n = nums.size();
+		return solveWith1DTable(nums);
+	}
+};
+
+// Driver code
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(nullptr);
+
+	vector<int> nums = {1, 2, 3};
+	cout << "From Formula: " << pow(2, nums.size()) << '\n'; // calculate 2^N
+
+	TopDown td;
+	cout << "From Memoization: " << td.countSubsequences(nums) << '\n';
+
+	BottomUp bu;
+	cout << "From Bottom Up: " << bu.countSubsequences(nums) << '\n';
+
+	return 0;
+}
