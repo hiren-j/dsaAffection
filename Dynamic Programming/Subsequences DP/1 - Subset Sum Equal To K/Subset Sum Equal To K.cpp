148dfcfa08614eecd826f60552443d53e4c7fb7d
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-01 21:18:40 +0530

    Update Subset Sum Equal To K.cpp

--- Dynamic Programming/Subsequences DP/1 - Subset Sum Equal To K/Subset Sum Equal To K.cpp
+++ Dynamic Programming/Subsequences DP/1 - Subset Sum Equal To K/Subset Sum Equal To K.cpp
@@ -1,14 +1,4 @@
-// Code to determine if there is a subset of the given set with sum equal to the given sum or not ~ coded by Hiren
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-/*
-    DON'T IGNORE MUST READ: As I mentioned earlier, I'll be providing multiple ways of writing the top-down solutions. So, from now on, in this Subsequence DP pattern and the LIS DP pattern, you’ll find different approaches to crafting these solutions. 
-                            This will help you figure out multiple ways of writing a top-down solution. Again, I'll be doing it for both the Subsequence and LIS patterns to help you grasp various techniques and perspectives.
-                            In this Subsequence pattern, there are a total of 22 questions, and 12 of them contain multiple ways of writing the top-down solution. 
-                            If multiple ways are not provided, consider that I tried but couldn’t get them to work through all test cases. 
-                            Hope you’re enjoying the series!
-*/
+// Code to determine if there is a subset of the given set with sum equal to the given sum or not ~ coded by vHiren
   
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
