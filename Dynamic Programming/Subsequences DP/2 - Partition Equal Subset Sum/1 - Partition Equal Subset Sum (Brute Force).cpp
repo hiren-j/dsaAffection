0155f7394d9e453bd6e76d13d0349402b8d19d56
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-03 16:13:11 +0530

    Update 1 - Partition Equal Subset Sum (Brute Force).cpp

--- Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
+++ Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
@@ -2,11 +2,53 @@
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+class TopDown {
+    int n, arrSum;
+
+    bool solveWithMemo(vector<vector<int>>& dp, const vector<int>& nums, int i, int k) {
+        if(k < 0)
+            return false;
+        
+        if(i == n)
+            return (k == 0) ? true : false;
+            
+        if(dp[i][k] != -1)
+            return dp[i][k];
+
+        bool currTake = solveWithMemo(dp, nums, i + 1, k - nums[i]);
+        bool currSkip = solveWithMemo(dp, nums, i + 1, k);
+        
+        return dp[i][k] = (currTake || currSkip);
+    }
+
+public:
+    // O(AS*N*AS) & O(N*AS) : Where AS = arrSum
+    bool canPartition(vector<int>& nums) {
+        n = nums.size();
+        arrSum = accumulate(begin(nums), end(nums), 0);
+
+        vector<vector<int>> dp(n, vector<int>(arrSum / 2 + 1, -1));
+
+        for(int subset1Sum = 0; subset1Sum <= arrSum / 2; ++subset1Sum) {
+            bool isPresent = solveWithMemo(dp, nums, 0, subset1Sum);
+            
+            if(isPresent) { // If subset1 is present is array 'nums'
+                int subset2Sum = arrSum - subset1Sum;
+                if(subset1Sum == subset2Sum) return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
 class BottomUp {
     int n, arrSum;
 
     void precomputeSubsetSumK(vector<vector<int>>& dp, const vector<int>& nums) {
-        dp.resize(n + 1, vector<int>(arrSum + 1, -1));
+        dp.resize(n + 1, vector<int>(arrSum / 2 + 1, -1));
 
         for(int k = 0; k <= arrSum; ++k)
             dp[n][k] = (k == 0) ? true : false;
