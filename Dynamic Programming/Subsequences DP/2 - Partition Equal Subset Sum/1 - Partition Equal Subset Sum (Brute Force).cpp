e345037a5280d34756009878e917ab89264510b9
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-03 16:03:55 +0530

    Update 1 - Partition Equal Subset Sum (Brute Force).cpp

--- Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
+++ Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
@@ -2,102 +2,42 @@
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class TopDown {
-    int n;
-    
-    bool solveWithMemo(vector<vector<int>>& dp, vector<int>& nums, int index, int sum) {
-        if(sum == 0)
-            return true;
-            
-        if(sum < 0 || index == n)
-            return false;
-            
-        if(dp[index][sum] != -1)
-            return dp[index][sum];
-            
-        bool currSkip = solveWithMemo(dp, nums, index + 1, sum);
-        bool currTake = solveWithMemo(dp, nums, index + 1, sum - nums[index]);
+class BottomUp {
+    int n, arrSum;
+
+    void precomputeSubsetSumK(vector<vector<int>>& dp, const vector<int>& nums) {
+        dp.resize(n + 1, vector<int>(arrSum + 1, -1));
+
+        for(int k = 0; k <= arrSum; ++k)
+            dp[n][k] = (k == 0) ? true : false;
         
-        return dp[index][sum] = (currSkip || currTake); 
+        for(int i = n - 1; i >= 0; --i) {
+            for(int k = 0; k <= arrSum; ++k) {
+                bool currTake = (k - nums[i] < 0) ? false : dp[i + 1][k - nums[i]];
+                bool currSkip = dp[i + 1][k];
+                dp[i][k] = (currTake || currSkip);  
+            }
+        }
     }
-    
+
 public:
-    // O(N*TS) & O(N*TS) : Where TS = totalSum
+    // O(N*AS) & O(N*AS) : Where AS = arrSum
     bool canPartition(vector<int>& nums) {
         n = nums.size();
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        
-        vector<vector<int>> dp(n, vector<int>(totalSum + 1, -1));
-        
-        for(int subset1Sum = 1; subset1Sum <= totalSum; ++subset1Sum) {
-            bool isSubsetExist = solveWithMemo(dp, nums, 0, subset1Sum);
-            if(!isSubsetExist) continue;
-            int subset2Sum = totalSum - subset1Sum;
-            if(subset1Sum == subset2Sum) return true;
-        }
-    
-        return false;
-    }
-};
+        arrSum = accumulate(begin(nums), end(nums), 0);
 
---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        vector<vector<int>> dp;
+        precomputeSubsetSumK(dp, nums);
 
-class BottomUp {
-public:
-    // O(N*TS) & O(N*TS)
-    bool canPartition_V1(vector<int>& nums) {
-        int n = nums.size();
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        
-        vector<vector<bool>> dp(n + 1, vector<bool>(totalSum + 1, false));
-        
-        for(int index = 0; index <= n; ++index)
-            dp[index][0] = true;
+        for(int subset1Sum = 0; subset1Sum <= arrSum / 2; ++subset1Sum) {
+            bool isPresent = dp[0][subset1Sum];
             
-        for(int index = n-1; index >= 0; --index) {
-            for(int sum = 1; sum <= totalSum; ++sum) {
-                bool currSkip = dp[index + 1][sum];
-                bool currTake = (sum - nums[index] >= 0) ? dp[index + 1][sum - nums[index]] : false;
-                dp[index][sum] = (currSkip || currTake); 
+            if(isPresent) { // If subset1 is present is array 'nums'
+                int subset2Sum = arrSum - subset1Sum;
+                if(subset1Sum == subset2Sum) return true;
             }
         }
-        
-        for(int subset1Sum = totalSum/2; subset1Sum <= totalSum; ++subset1Sum) {
-            bool isSubsetExist = dp[0][subset1Sum];
-            if(!isSubsetExist) continue;
-            int subset2Sum = totalSum - subset1Sum;
-            if(subset1Sum == subset2Sum) return true;
-        }
-    
-        return false;
-    }
 
-    // O(N*TS) & O(TS)
-    bool canPartition_V2(vector<int>& nums) {
-        int n = nums.size();
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        
-        vector<bool> nextRow(totalSum + 1, false);
-        nextRow[0] = true;
-            
-        for(int index = n-1; index >= 0; --index) {
-            vector<bool> idealRow(totalSum + 1, false);
-            idealRow[0] = true;
-            for(int sum = 1; sum <= totalSum; ++sum) {
-                bool currSkip = nextRow[sum];
-                bool currTake = (sum - nums[index] >= 0) ? nextRow[sum - nums[index]] : false;
-                idealRow[sum] = (currSkip || currTake); 
-            }
-            nextRow = idealRow;
-        }
-        
-        for(int subset1Sum = totalSum/2; subset1Sum <= totalSum; ++subset1Sum) {
-            bool isSubsetExist = nextRow[subset1Sum];
-            if(!isSubsetExist) continue;
-            int subset2Sum = totalSum - subset1Sum;
-            if(subset1Sum == subset2Sum) return true;
-        }
-    
         return false;
     }
 };
