82ee5121b87ac7274c6bb57c59325860944ce5fa
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-03 16:42:29 +0530

    Update 1 - Partition Equal Subset Sum (Brute Force).cpp

--- Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
+++ Dynamic Programming/Subsequences DP/2 - Partition Equal Subset Sum/1 - Partition Equal Subset Sum (Brute Force).cpp
@@ -22,14 +22,14 @@ class TopDown {
     }
 
 public:
-    // O(AS*N*AS) & O(N*AS) : Where AS = arrSum
+    // O(N*AS) & O(N*AS) : Where AS = arrSum
     bool canPartition(vector<int>& nums) {
         n = nums.size();
         arrSum = accumulate(begin(nums), end(nums), 0);
 
         vector<vector<int>> dp(n, vector<int>(arrSum / 2 + 1, -1));
 
-        for(int subset1Sum = 0; subset1Sum <= arrSum / 2; ++subset1Sum) {
+        for(int subset1Sum = arrSum / 2; subset1Sum >= 0; --subset1Sum) {
             if(solveWithMemo(dp, nums, 0, subset1Sum)) { // If subset1 is present is array 'nums'
                 int subset2Sum = arrSum - subset1Sum;
                 if(subset1Sum == subset2Sum) return true;
@@ -69,7 +69,7 @@ public:
         vector<vector<int>> dp;
         precomputeSubsetSumK(dp, nums);
 
-        for(int subset1Sum = 0; subset1Sum <= arrSum / 2; ++subset1Sum) {
+        for(int subset1Sum = arrSum / 2; subset1Sum >= 0; --subset1Sum) {
             if(dp[0][subset1Sum]) { // If subset1 is present is array 'nums'
                 int subset2Sum = arrSum - subset1Sum;
                 if(subset1Sum == subset2Sum) return true;
