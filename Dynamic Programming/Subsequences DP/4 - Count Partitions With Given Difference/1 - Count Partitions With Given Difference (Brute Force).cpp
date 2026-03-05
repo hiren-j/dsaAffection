f9cdb57b6cfc552db878d445ec1b90cef2454743
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-05 21:20:08 +0530

    Update 1 - Count Partitions With Given Difference (Brute Force).cpp

--- Dynamic Programming/Subsequences DP/4 - Count Partitions With Given Difference/1 - Count Partitions With Given Difference (Brute Force).cpp
+++ Dynamic Programming/Subsequences DP/4 - Count Partitions With Given Difference/1 - Count Partitions With Given Difference (Brute Force).cpp
@@ -31,7 +31,7 @@ public:
         vector<vector<int>> dp(n, vector<int>(arrSum + 1, -1));
         int result = 0;
         
-        for(int subset1Sum = arrSum; subset1Sum >= arrSum / 2; --subset1Sum) {
+        for(int subset1Sum = arrSum / 2; subset1Sum <= arrSum; ++subset1Sum) {
             int countSubsets = solveWithMemo(dp, nums, 0, subset1Sum);
             int subset2Sum   = arrSum - subset1Sum;
             if(subset1Sum - subset2Sum == D) {
@@ -74,7 +74,7 @@ public:
         
         int result = 0;
         
-        for(int subset1Sum = arrSum; subset1Sum >= arrSum / 2; --subset1Sum) {
+        for(int subset1Sum = arrSum / 2; subset1Sum <= arrSum; ++subset1Sum) {
             int countSubsets = dp[0][subset1Sum];
             int subset2Sum   = arrSum - subset1Sum;
             if(subset1Sum - subset2Sum == D) {
