50b001911e0dddff31bd42751b35fb01f98092f7
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-08 17:53:20 +0530

    Update 2 - Ones And Zeros (Optimized).cpp

--- Dynamic Programming/Subsequences DP/13 - Ones And Zeroes/2 - Ones And Zeros (Optimized).cpp
+++ Dynamic Programming/Subsequences DP/13 - Ones And Zeroes/2 - Ones And Zeros (Optimized).cpp
@@ -128,7 +128,7 @@ class BottomUp {
     // O(AS*GN*GM) & O(GN*GM) : Where AS = arrSize, GN = given_n, GM = given_m
     int solveBy2DTable(const vector<string>& arr, int given_n, int given_m) {
         vector<vector<int>> next(given_n + 1, vector<int>(given_m + 1, -1)); // i + 1th table
-        vector<vector<int>> curr(given_n + 1, vector<int>(given_m + 1, -1)); // i + 1th table
+        vector<vector<int>> curr(given_n + 1, vector<int>(given_m + 1, -1)); // ith table
 
         for(int n = 0; n <= given_n; ++n)
             for(int m = 0; m <= given_m; ++m)
