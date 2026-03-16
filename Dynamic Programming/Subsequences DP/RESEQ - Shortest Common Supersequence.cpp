88c99fc31af718fd4faff7b5bdc272ad6ac31f6e
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-16 11:06:49 +0530

    Update and rename 15 - Shortest Common Supersequence.cpp to RESEQ - Shortest Common Supersequence.cpp

--- Dynamic Programming/Subsequences DP/RESEQ - Shortest Common Supersequence.cpp
+++ Dynamic Programming/Subsequences DP/RESEQ - Shortest Common Supersequence.cpp
@@ -4,66 +4,62 @@
 
 class BottomUp {
 public:
-    // Method to find the shortest common supersequence, using 2D tabulation - O(N*M) & O(N*M)
+    // O(N*M) & O(N*M)
     string shortestCommonSupersequence(string& s1, string& s2) {
-        int n = s1.size(), m = s2.size();
+        const int n = s1.size(), m = s2.size();
 
-        // 2D DP table
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        // Find the length of the longest common subsequence of both the strings
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else {
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-                }
+        // Find length of LCS
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + dp[i + 1][j + 1]
+                                : 0;
+                int currSkip1 = dp[i + 1][j];
+                int currSkip2 = dp[i][j + 1];
+                dp[i][j] = max({currTake, currSkip1, currSkip2});
             }
         }
 
         string answer;
 
-        int i = n, j = m;
+        int i = 0, j = 0;
 
-        while(i > 0 && j > 0) {
+        while(i < n && j < m) {
             // If both the letters match
-            if(s1[i - 1] == s2[j - 1]) {
+            if(s1[i] == s2[j]) {
                 // Take only 1 letter
-                answer.push_back(s1[i - 1]);
-                i--;
-                j--;
+                answer.push_back(s1[i]);
+                i++;
+                j++;
             }
             // Else when both the letters don't match
             else {
                 // If it's maximum then string1 will shrink by size 1. So before shrink take that dropped letter of string1
-                if(dp[i - 1][j] > dp[i][j - 1]) {
-                    answer.push_back(s1[i - 1]);
-                    i--;
+                if(dp[i + 1][j] > dp[i][j + 1]) {
+                    answer.push_back(s1[i]);
+                    i++;
                 }   
                 // Else if it's maximum then string2 will shrink by size 1. So before shrink take that dropped letter of string2
                 else {
-                    answer.push_back(s2[j - 1]);
-                    j--;
+                    answer.push_back(s2[j]);
+                    j++;
                 }
             }
         }
 
         // Store the remaining letters of string1 to the answer
-        while(i > 0) {
-            answer.push_back(s1[i - 1]);
-            i--;
+        while(i < n) {
+            answer.push_back(s1[i]);
+            i++;
         }
 
         // Store the remaining letters of string2 to the answer
-        while(j > 0) {
-            answer.push_back(s2[j - 1]);
-            j--;
+        while(j < m) {
+            answer.push_back(s2[j]);
+            j++;
         }
-
-        // Reverse the answer array to get the actual order
-        reverse(begin(answer), end(answer));
 
         return answer;
     }
