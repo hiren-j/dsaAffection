8ce97af6604106e27a4802a065c119d1b4d98182
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-16 11:15:55 +0530

    Update RESEQ - Print Longest Palindromic Subsequence.cpp

--- Dynamic Programming/Subsequences DP/RESEQ - Print Longest Palindromic Subsequence.cpp
+++ Dynamic Programming/Subsequences DP/RESEQ - Print Longest Palindromic Subsequence.cpp
@@ -4,52 +4,47 @@
 
 class BottomUp {
 public:
-    // Method to find the LPS of given string, using 2D tabulation - O(N*M) & O(N*M)
-    int getLPS(string& s1) {
-        int n = s1.size();
+    // O(N*M) & O(N*M)
+    int printLongestPalindromicSubseq(string& s1) {
         string s2 = s1;
         reverse(begin(s2), end(s2));
 
-        // 2D DP table
+        const int n = s1.size();
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-        // Find the length of the LCS of s1 and s2
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                if(s1[i - 1] == s2[j - 1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else 
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        // Find length of LCS
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = n - 1; j >= 0; --j) {
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + dp[i + 1][j + 1]
+                                : 0;
+                int currSkip1 = dp[i + 1][j];
+                int currSkip2 = dp[i][j + 1];
+                dp[i][j] = max({currTake, currSkip1, currSkip2});
             }
         }
         
-        // Stores the result string
         string LPS;
-
-        int i = n, j = n;
+        int i = 0, j = 0;
 
         while(i > 0 && j > 0) {
             // If both the letters match
-            if(s1[i - 1] == s2[j - 1]) {
-                LPS.push_back(s1[i - 1]);
-                i = i - 1;
-                j = j - 1;
+            if(s1[i] == s2[j]) {
+                LPS.push_back(s1[i1]);
+                i = i + 1;
+                j = j + 1;
             }
             // Else when both the letters don't match
             else {
-                if(dp[i - 1][j] > dp[i][j - 1]) {
-                    i = i - 1;
+                if(dp[i + 1][j] > dp[i][j + 1]) {
+                    i = i + 1;
                 }
                 else {
-                    j = j - 1;
+                    j = j + 1;
                 }
             }
         }
 
-        // Reverse the string to get the actual order of the LPS
-        reverse(begin(LPS), end(LPS));
-
-        // Return the result string
         return LPS;
     }
 };
@@ -58,4 +53,5 @@ public:
     
 Topics: String | Dynamic Programming
 Links : https://leetcode.com/problems/longest-palindromic-subsequence/description/
-        https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/String%20DP/Longest%20Palindromic%20Subsequence.cpp
+        https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/Subsequences%20DP/RESEQ%20-%20Longest%20Palindromic%20Subsequence.cpp
+        https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/Subsequences%20DP/RESEQ%20-%20Longest%20Common%20Subsequence.cpp
