e8e39739032cb17f39e0fe478bfd84401c54e592
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-16 09:54:57 +0530

    Update 1 - Longest Common Subsequence.cpp

--- Dynamic Programming/String DP/1 - Longest Common Subsequence.cpp
+++ Dynamic Programming/String DP/1 - Longest Common Subsequence.cpp
@@ -1,96 +1,117 @@
-// Code to find the length of the longest common subsequence of the strings s1 and s2 ~ coded by Hiren
+// Code to find the length of the longest common subsequence of the strings s1 and s2 ~ coded by vHiren
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class TopDown {
     int n, m;
 
-    // O(2^(N+M)) & O(N+M)
-    int solveWithoutMemo(string& s1, string& s2, int i, int j) {
-        // Edge case: If any of the string gets exhausted then we've found a common subsequence
-        if(i < 0 || j < 0)
+    int solveWithoutMemo(const string& s1, const string& s2, int i, int j) {
+        if(i == n || j == m)
             return 0;
 
-        // If the letters of both the string matches then we've got a common subsequence of length 1 
-        if(s1[i] == s2[j])
-            return 1 + solveWithoutMemo(s1, s2, i-1, j-1);
-        
-        // Else when the letters doesn't match then it's possible that the jth letter could be found at the left side of the ith letter and vice versa. So, that's why explore both the possibilities and as we're striving for the LCS hence return the maximum value
-        return max(solveWithoutMemo(s1, s2, i-1, j), solveWithoutMemo(s1, s2, i, j-1));
+        int currSkip1 = solveWithoutMemo(s1, s2, i + 1, j);
+        int currSkip2 = solveWithoutMemo(s1, s2, i, j + 1);
+        int currTake  = s1[i] == s2[j] 
+                        ? 1 + solveWithoutMemo(s1, s2, i + 1, j + 1)
+                        : 0;
+
+        return max({currTake, currSkip1, currSkip2});
     }
-
-    // O(2*N*M) & O(N*M + N+M)
-    int solveWithMemo(vector<vector<int>>& memory, string& s1, string& s2, int i, int j) {
-        // Edge case: If any of the string gets exhausted then we've found a common subsequence
-        if(i < 0 || j < 0)
+    
+    int solveWithMemo(vector<vector<int>>& dp, const string& s1, const string& s2, int i, int j) {
+        if(i == n || j == m)
             return 0;
 
-        // Memoization table: If the current state is already computed then return the computed value
-        if(memory[i][j] != -1)
-            return memory[i][j];
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
-        // If the letters of both the string matches then we've got a common subsequence of length 1 
-        if(s1[i] == s2[j])
-            return memory[i][j] = 1 + solveWithMemo(memory, s1, s2, i-1, j-1);
-        
-        // Else when the letters doesn't match then it's possible that the jth letter could be found at the left side of the ith letter and vice versa. So, that's why explore both the possibilities and as we're striving for the LCS hence store the maximum value to the memoization table and then return it
-        return memory[i][j] = max(solveWithMemo(memory, s1, s2, i-1, j), solveWithMemo(memory, s1, s2, i, j-1));
+        int currSkip1 = solveWithMemo(dp, s1, s2, i + 1, j);
+        int currSkip2 = solveWithMemo(dp, s1, s2, i, j + 1);
+        int currTake  = s1[i] == s2[j] 
+                        ? 1 + solveWithMemo(dp, s1, s2, i + 1, j + 1)
+                        : 0;
+
+        return dp[i][j] = max({currTake, currSkip1, currSkip2});
     }
 
 public:
-    // Method to find the length of the LCS, using recursion with memoization - O(N*M) & O(N*M)
     int longestCommonSubsequence(string& s1, string& s2) {
         n = s1.size(), m = s2.size();
-        vector<vector<int>> memory(n, vector<int>(m, -1));
-        return solveWithMemo(memory, s1, s2, n-1, m-1);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solveWithMemo(dp, s1, s2, 0, 0);
     }
 };
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the length of the LCS, using 2D tabulation - O(N*M) & O(N*M)
-    int longestCommonSubsequence_V1(string& s1, string& s2) {
-        int n = s1.size(), m = s2.size();
+    int n, m;
 
-        // 2D DP table
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int solveBy2DTable(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
 
-        // Fill the table
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s1[i - 1] == s2[j - 1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else 
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        for(int j = 0; j <= m; ++j)
+            dp[n][j] = 0;
+
+        for(int i = 0; i <= n; ++i)
+            dp[i][m] = 0;
+
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                int currSkip1 = dp[i + 1][j];
+                int currSkip2 = dp[i][j + 1];
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + dp[i + 1][j + 1]
+                                : 0;
+                dp[i][j] = max({currTake, currSkip1, currSkip2});
             }
         }
 
-        // Return the result value
-        return dp[n][m];
+        return dp[0][0];
     }
 
-    // #2 Method to find the length of the LCS, using 1D tabulation - O(N*M) & O(M)
-    int longestCommonSubsequence_V2(string& s1, string& s2) {
-        int n = s1.size(), m = s2.size();
+    int solveBy2DEnhanced(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        // 1D DP tables
-        vector<int> prevRow(m + 1), currRow(m + 1);
-
-        // Fill the table
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s1[i - 1] == s2[j - 1])
-                    currRow[j] = 1 + prevRow[j-1];
-                else 
-                    currRow[j] = max(prevRow[j], currRow[j-1]);
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                int currSkip1 = dp[i + 1][j];
+                int currSkip2 = dp[i][j + 1];
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + dp[i + 1][j + 1]
+                                : 0;
+                dp[i][j] = max({currTake, currSkip1, currSkip2});
             }
-            prevRow = currRow;
         }
 
-        // Return the result value
-        return prevRow[m];
+        return dp[0][0];
+    }
+
+    int solveBy1DTable(const string& s1, const string& s2) {
+        vector<int> nextRow(m + 1, 0); // i + 1th row
+
+        for(int i = n - 1; i >= 0; --i) {
+            vector<int> idealRow(m + 1, 0); // ith row
+
+            for(int j = m - 1; j >= 0; --j) {
+                int currSkip1 = nextRow[j];
+                int currSkip2 = idealRow[j + 1];
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + nextRow[j + 1]
+                                : 0;
+                idealRow[j] = max({currTake, currSkip1, currSkip2});
+            }
+
+            swap(nextRow, idealRow);
+        }
+
+        return nextRow[0];
+    }
+
+public:
+    int longestCommonSubsequence(string& s1, string& s2) {
+        n = s1.size(), m = s2.size();
+        return solveBy1DTable(s1, s2);
     }
 };
 
