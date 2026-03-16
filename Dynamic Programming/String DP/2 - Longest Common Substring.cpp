36be4b5a9c51fa186b610b4ca7c3f993b8533338
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-16 15:40:59 +0530

    Update 2 - Longest Common Substring.cpp

--- Dynamic Programming/String DP/2 - Longest Common Substring.cpp
+++ Dynamic Programming/String DP/2 - Longest Common Substring.cpp
@@ -50,54 +50,53 @@ public:
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // #1 Method to find the length of the longest common substring, using 2D tabulation - O(N*M) & O(N*M)
-    int longestCommonSubstring_V1(string& s1, string& s2, int n, int m) {
-        // 2D DP table
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
-        
-        int maxLength = 0;
-        
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                // If both the letters match
-                if(s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                // Else when both the letters don't match
-                else {
-                    dp[i][j] = 0;
-                }
-                maxLength = max(maxLength, dp[i][j]);
-            }
-        }
-        
-        return maxLength;
-    }
+    int n, m;
 
-    // #2 Method to find the length of the longest common substring, using 1D tabulation - O(N*M) & O(M)
-    int longestCommonSubstring_V2(string& s1, string& s2, int n, int m) {
-        // 1D DP tables
-        vector<int> prevRow(m + 1), currRow(m + 1);
-        
-        int maxLength = 0;
-        
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                // If both the letters match
-                if(s1[i - 1] == s2[j - 1]) {
-                    currRow[j] = 1 + prevRow[j - 1];
-                }
-                // Else when both the letters don't match
-                else {
-                    currRow[j] = 0;
-                }
-                maxLength = max(maxLength, currRow[j]);
+    // O(N*M) & O(N*M)
+    int solveBy2DTable(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); // Fill dp table with 0 so don't have to initialize both base cases seperately
+         
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                if(s1[i] == s2[j])
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = 0;
             }
-            prevRow = currRow;
         }
         
-        return maxLength;
+        int maxLen = 0;
+        
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                maxLen = max(maxLen, dp[i][j]);
+        
+        return maxLen;
+    }
+    
+    // O(N*M) & O(N*M)
+    int solveBy2DEnhanced(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); // Fill dp table with 0 so don't have to initialize both base cases seperately
+        int maxLen = 0;
+         
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                if(s1[i] == s2[j])
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                else
+                    dp[i][j] = 0;
+                
+                maxLen = max(maxLen, dp[i][j]);
+            }
+        }
+    
+        return maxLen;
+    }
+    
+public:
+    int longCommSubstr(string& s1, string& s2) {
+        n = s1.size(), m = s2.size();
+        return solveBy2DEnhanced(s1, s2);
     }
 };
 
