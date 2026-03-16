a74e2a49ecf6d0cedd7264ab8ef7c56f2c780955
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-16 15:35:09 +0530

    Update 2 - Longest Common Substring.cpp

--- Dynamic Programming/String DP/2 - Longest Common Substring.cpp
+++ Dynamic Programming/String DP/2 - Longest Common Substring.cpp
@@ -2,6 +2,53 @@
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+class TopDown {
+    int n, m;
+    
+    // O(N+M) & O(min(N,M))
+    int solveWithoutMemo(const string& s1, const string& s2, int i, int j) {
+        if(i == n || j == m)
+            return 0;
+        
+        if(s1[i] == s2[j])
+            return 1 + solveWithoutMemo(s1, s2, i + 1, j + 1);
+            
+        return 0;
+    }
+    // This function will be called N*M times, look below from the loop, hence overall Time considering this O(N*M * (N+M))
+    
+    // O(N*M) & O(N*M)
+    int solveWithMemo(vector<vector<int>>& dp, const string& s1, const string& s2, int i, int j) {
+        if(i == n || j == m)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(s1[i] == s2[j])
+            return dp[i][j] = 1 + solveWithMemo(dp, s1, s2, i + 1, j + 1);
+            
+        return dp[i][j] = 0;
+    }
+    // This function will be called N*M times, look below from the loop, hence overall Time considering this O(N*M + N*M) = O(2*N*M) = O(N*M)
+    
+public:
+    int longCommSubstr(string& s1, string& s2) {
+        n = s1.size(), m = s2.size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int maxLen = 0;
+        
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < m; ++j)
+                maxLen = max(maxLen, solveWithMemo(dp, s1, s2, i, j));
+        
+        return maxLen;
+    }
+};
+    
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class BottomUp {
 public:
     // #1 Method to find the length of the longest common substring, using 2D tabulation - O(N*M) & O(N*M)
