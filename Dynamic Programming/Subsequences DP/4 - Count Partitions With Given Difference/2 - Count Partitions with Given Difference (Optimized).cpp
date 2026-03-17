37b6cddea431d5431c5d33eca31de815cb33a354
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-03-17 09:57:25 +0530

    Update 2 - Count Partitions with Given Difference (Optimized).cpp

--- Dynamic Programming/Subsequences DP/4 - Count Partitions With Given Difference/2 - Count Partitions with Given Difference (Optimized).cpp
+++ Dynamic Programming/Subsequences DP/4 - Count Partitions With Given Difference/2 - Count Partitions with Given Difference (Optimized).cpp
@@ -16,10 +16,10 @@ class TopDown {
         if(dp[i][k] != -1)
             return dp[i][k];
             
-        int currTake = solveWithMemo(dp, nums, i + 1, k - nums[i]);
         int currSkip = solveWithMemo(dp, nums, i + 1, k);
+        int currTake = solveWithMemo(dp, nums, i + 1, k - nums[i]);
         
-        return dp[i][k] = (currTake + currSkip);
+        return dp[i][k] = (currSkip + currTake);
     }
     
 public:
@@ -52,9 +52,9 @@ class BottomUp {
         
         for(int i = n - 1; i >= 0; --i) {
             for(int k = 0; k <= arrSum; ++k) {
-                int currTake = (k - nums[i] < 0) ? 0 : dp[i + 1][k - nums[i]];
                 int currSkip = dp[i + 1][k];
-                dp[i][k] = (currTake + currSkip);
+                int currTake = (k - nums[i] < 0) ? 0 : dp[i + 1][k - nums[i]];
+                dp[i][k] = (currSkip + currTake);
             }
         }
     }
