671485b39c6555f273d924ca06cef5349b395f89
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-07 10:34:42 +0530

    Update House Robber II.cpp

--- Dynamic Programming/1D DP/9 - House Robber II/House Robber II.cpp
+++ Dynamic Programming/1D DP/9 - House Robber II/House Robber II.cpp
@@ -2,38 +2,38 @@
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class TopDown {
+class Solution {
     // O(2^N) & O(N)
-    int solveWithoutMemo(const vector<int>& nums, int start, const int end) {
-        if(start > end)
+    int solveWithoutMemo(const vector<int>& nums, int i, const int end) {
+        if(i > end)
             return 0;
 
-        int robHouse  = nums[start] + solveWithoutMemo(nums, start + 2, end);
-        int skipHouse = solveWithoutMemo(nums, start + 1, end);
-        
+        int robHouse  = nums[i] + solveWithoutMemo(nums, i + 2, end);
+        int skipHouse = solveWithoutMemo(nums, i + 1, end);
+
         return max(robHouse, skipHouse);
     }
 
     // O(2*N) & O(2*N)
-    int solveWithMemo(vector<int>& dp, const vector<int>& nums, int start, const int end) {
-        if(start > end)
+    int solveWithMemo(vector<int>& dp, const vector<int>& nums, int i, const int end) {
+        if(i > end)
             return 0;
 
-        if(dp[start] != -1)
-            return dp[start];
+        if(dp[i] != -1)
+            return dp[i];
 
-        int robHouse  = nums[start] + solveWithMemo(dp, nums, start + 2, end);
-        int skipHouse = solveWithMemo(dp, nums, start + 1, end);
-        
-        return dp[start] = max(robHouse, skipHouse);
+        int robHouse  = nums[i] + solveWithMemo(dp, nums, i + 2, end);
+        int skipHouse = solveWithMemo(dp, nums, i + 1, end);
+
+        return dp[i] = max(robHouse, skipHouse);
     }
 
 public:
-    // Methot to find maximum money can rob, using recursion with memoization - O(N) & O(N)
+    // Method to find maximum money can rob, using recursion with memoization - O(N) & O(N)
     int robMaxMoney(vector<int>& nums) {
         const int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> dp1(n - 1, -1), dp2(n, -1);
+        vector<int> dp1(n, -1), dp2(n, -1);
         int startFrom0 = solveWithMemo(dp1, nums, 0, n - 2);
         int startFrom1 = solveWithMemo(dp2, nums, 1, n - 1);
         return max(startFrom0, startFrom1);
@@ -43,36 +43,36 @@ public:
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    // O(1*N) & O(1*N)
-    int solveWith1DTable(const vector<int>& nums, int start, const int end) {
+    // O(N) & O(N)
+    int solveWith1DTable(const vector<int>& nums, const int start, const int end) {
         vector<int> dp(end + 3, -1);
         dp[end + 2] = 0;
         dp[end + 1] = 0;
 
-        for(int idx = end; idx >= start; --idx) {
-            int robHouse  = nums[idx] + dp[idx + 2];
-            int skipHouse = dp[idx + 1];
-            dp[idx] = max(robHouse, skipHouse);
+        for(int i = end; i >= start; --i) {
+            int robHouse  = nums[i] + dp[i + 2];
+            int skipHouse = dp[i + 1];
+            dp[i] = max(robHouse, skipHouse);
         }
 
         return dp[start];
     }
 
-    // O(1*N) & O(1)
-    int solveWithoutTable(const vector<int>& nums, int start, const int end) {
-        int dp_idx_2 = 0;
-        int dp_idx_1 = 0;
-        int dp_idx   = 0;
+    // O(N) & O(1)
+    int solveWithoutTable(const vector<int>& nums, const int start, const int end) {
+        int dp_i_plus_2 = 0; 
+        int dp_i_plus_1 = 0; 
+        int dp_i = -1;       
 
-        for(int idx = end; idx >= start; --idx) {
-            int robHouse  = nums[idx] + dp_idx_2;
-            int skipHouse = dp_idx_1;
-            dp_idx   = max(robHouse, skipHouse);
-            dp_idx_2 = dp_idx_1;
-            dp_idx_1 = dp_idx;
+        for(int i = end; i >= start; --i) {
+            int robHouse  = nums[i] + dp_i_plus_2;
+            int skipHouse = dp_i_plus_1;
+            dp_i = max(robHouse, skipHouse);
+            dp_i_plus_2 = dp_i_plus_1;
+            dp_i_plus_1 = dp_i;
         }
 
-        return dp_idx;
+        return dp_i;
     }
 
 public:
