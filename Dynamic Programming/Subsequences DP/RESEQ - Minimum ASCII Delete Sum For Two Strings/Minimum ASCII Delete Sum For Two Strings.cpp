13e68bd07db87bce873d8e3409c44c49c55f391f
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-07 22:23:09 +0530

    Update Minimum ASCII Delete Sum For Two Strings.cpp

--- Dynamic Programming/Subsequences DP/RESEQ - Minimum ASCII Delete Sum For Two Strings/Minimum ASCII Delete Sum For Two Strings.cpp
+++ Dynamic Programming/Subsequences DP/RESEQ - Minimum ASCII Delete Sum For Two Strings/Minimum ASCII Delete Sum For Two Strings.cpp
@@ -1,3 +1,7 @@
+// Code to find the lowest ASCII sum of deleted characters to make given two strings equal ~ coded by vHiren
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class TopDown {
     int n, m;
 
@@ -35,3 +39,54 @@ public:
         return res1 + res2;
     }
 };
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class BottomUp {
+    int n, m;
+
+    int stringSum(const string& s) {
+        int sum = 0;
+        for(const char ch : s) sum += ch;
+        return sum;
+    }
+
+    // O(N*M) & O(M)
+    int solveBy1DTable(const string& s1, const string& s2) {
+        vector<int> nextRow(m + 1, 0); // i + 1th row
+
+        for(int i = n - 1; i >= 0; --i) {
+            vector<int> idealRow(m + 1, 0); // ith row
+
+            for(int j = m - 1; j >= 0; --j) {
+                int currSkip1 = nextRow[j];
+                int currSkip2 = idealRow[j + 1];
+                int currTake  = s1[i] == s2[j] 
+                                ? nextRow[j + 1] + s1[i]
+                                : 0;
+                idealRow[j] = max({currSkip1, currSkip2, currTake});
+            }
+
+            swap(nextRow, idealRow);
+        }
+
+        return nextRow[0];
+    }
+    
+public:
+    // O(N*M) & O(M)
+    int minimumDeleteSum(string& s1, string& s2) {
+        n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int sumOfLCS = solveBy1DTable(s1, s2);
+        int res1 = stringSum(s1) - sumOfLCS;
+        int res2 = stringSum(s2) - sumOfLCS;
+        return res1 + res2;
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Topics: Senior Staff | String | Dynamic Programming
+Link  : https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/description/
+      : https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/Subsequences%20DP/RESEQ%20-%20Longest%20Common%20Subsequence/1%20-%20Longest%20Common%20Subsequence%20(Static%20Version).cpp
