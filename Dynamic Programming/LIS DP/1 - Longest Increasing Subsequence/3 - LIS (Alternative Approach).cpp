5ad2e178b2945d426621ec0679974e427f2f147d
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-08 22:22:35 +0530

    Update 3 - LIS (Alternative Approach).cpp

--- Dynamic Programming/LIS DP/1 - Longest Increasing Subsequence/3 - LIS (Alternative Approach).cpp
+++ Dynamic Programming/LIS DP/1 - Longest Increasing Subsequence/3 - LIS (Alternative Approach).cpp
@@ -2,6 +2,98 @@
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+class Solution {
+    // O(N*N) & O(N)
+    int solveWithMemo(vector<int>& LIS, const vector<int>& nums, int i) {
+        if(LIS[i] != -1)
+            return LIS[i];
+
+        int maxLen = 1;
+
+        for(int prev = 0; prev < i; ++prev) {
+            if(nums[i] > nums[prev]) {
+                int lisLenPrevSide = solveWithMemo(LIS, nums, prev) + 1;
+                maxLen = max(maxLen, lisLenPrevSide);
+            }
+        }
+
+        return LIS[i] = maxLen;
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) { 
+        int n = nums.size(), res = 0;
+        
+        vector<int> LIS(n, -1); // dp array
+
+        for(int i = n - 1; i >= 0; --i) {
+            res = max(res, solveWithMemo(LIS, nums, i));
+        }
+
+        return res;
+    }
+};
+    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class BottomUp {
+    int n;
+
+    // O(N*N) & O(N)
+    int solveBy1DTable(const vector<int>& nums) {
+        vector<int> LIS(n, -1);
+
+        for(int i = 0; i < n; ++i) {
+            int maxLen = 1;
+
+            for(int prev = 0; prev < i; ++prev) {
+                if(nums[i] > nums[prev]) {
+                    int lisLenPrevSide = LIS[prev] + 1;
+                    maxLen = max(maxLen, lisLenPrevSide);
+                }
+            }
+
+            LIS[i] = maxLen;
+        }
+
+        int res = 0;
+        for(int i = n - 1; i >= 0; --i) {
+            res = max(res, LIS[i]);
+        }
+        return res;
+    }
+
+    // O(N*N) & O(N)
+    int solveBy1DEnhanced(const vector<int>& nums) {
+        vector<int> LIS(n, -1);
+        int res = 0;
+
+        for(int i = 0; i < n; ++i) {
+            int maxLen = 1;
+
+            for(int prev = 0; prev < i; ++prev) {
+                if(nums[i] > nums[prev]) {
+                    int lisLenPrevSide = LIS[prev] + 1;
+                    maxLen = max(maxLen, lisLenPrevSide);
+                }
+            }
+
+            LIS[i] = maxLen;
+            res = max(res, LIS[i]);
+        }
+
+        return res;
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) { 
+        n = nums.size();
+        return solveBy1DEnhanced(nums);
+    }
+};
+    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class BottomUpCleanVersion {
 public:
     // O(N*N) & O(N)
