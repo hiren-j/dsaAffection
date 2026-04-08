b046daeea02b8f1fc16c931e2ab63ff0edd20f85
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-08 22:09:23 +0530

    Update 3 - LIS (Alternative Approach).cpp

--- Dynamic Programming/LIS DP/1 - Longest Increasing Subsequence/3 - LIS (Alternative Approach).cpp
+++ Dynamic Programming/LIS DP/1 - Longest Increasing Subsequence/3 - LIS (Alternative Approach).cpp
@@ -2,74 +2,24 @@
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class BottomUpBrute {
+class BottomUpCleanVersion {
 public:
     // O(N*N) & O(N)
     int lengthOfLIS(vector<int>& nums) {
-        const int n = nums.size();
-        int maxLen = 0;
-        
-        vector<int> dp(n, 1);
+        int n = nums.size(), maxLen = 0;
 
-        for(int i = 0; i < n; ++i) {
-            for(int prev = 0; prev < i; ++prev) {
-                if(nums[i] > nums[prev]) {
-                    dp[i] = max(dp[i], dp[prev] + 1);
+        vector<int> LIS(n, 1);
+
+        for(int index = 0; index < n; ++index) {
+            for(int prevIndex = 0; prevIndex < index; ++prevIndex) {
+                if(nums[index] > nums[prevIndex]) {
+                    LIS[index] = max(LIS[index], LIS[prevIndex] + 1);
                 }
             }
-            maxLen = max(maxLen, dp[i]);
+            maxLen = max(maxLen, LIS[index]);
         }
 
         return maxLen;
-    }
-};
-    
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-class BinarySearch {
-    // O(NLogN) & O(N)
-    int solveWith1DTable(vector<int>& nums) {
-        const int n = nums.size();
-
-        // This array doesn't necessarily store the values of the LIS in the correct temp. 
-        // This is due to updates made during the binary search. 
-        // However, it will correctly provide the length of the actual LIS
-        vector<int> temp; 
-
-        for(const int val : nums) {
-            if(temp.empty() || temp.back() < val) {
-                temp.push_back(val);
-            }
-            else {
-                int i  = lower_bound(begin(temp), end(temp), val) - begin(temp);
-                temp[i] = val;
-            }
-        }
-
-        return temp.size();
-    }
-
-    // O(NLogN) & O(1)
-    int solveWithoutTable(vector<int>& nums) {
-        int len = 0;
-
-        for(const int val : nums) {
-            if(len == 0 || nums[len - 1] < val) {
-                nums[len] = val;
-                len++;
-            }
-            else {
-                int i   = lower_bound(begin(nums), begin(nums) + len, val) - begin(nums);
-                nums[i] = val;
-            }
-        }
-
-        return len;
-    }
-
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        return solveWithoutTable(nums);
     }
 };
     
