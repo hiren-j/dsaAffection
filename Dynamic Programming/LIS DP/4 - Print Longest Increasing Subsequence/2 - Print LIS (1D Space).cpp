b3d7690564d1035b9e7b57146b8eda11ac4ad486
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-10 23:07:36 +0530

    Update 2 - Print LIS (1D Space).cpp

--- Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence/2 - Print LIS (1D Space).cpp
+++ Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence/2 - Print LIS (1D Space).cpp
@@ -4,50 +4,44 @@
 
 class BottomUp {
 public:
-    // Method to find the longest increasing subsequence, using 1D tabulation - O(N*N) & O(N) 
+    // O(N*N) & O(N) 
     vector<int> getLIS(vector<int>& nums) {
-        int n = nums.size();
+        const int n = nums.size();
 
-        vector<int> dp(n, 1);    // dp[index] represents the length of the LIS ending at that index 
-        vector<int> temp(n, -1); // temp[index] represents the index of the recently seen value which is considered as the part of the LIS ending at that index
+        vector<int> LIS(n, 1); 
+        vector<int> sequence(n, -1); // sequence[i] represents the index of the recently seen value which is considered as the part of the LIS ending at that index
 
-        // Find the length of the LIS ending at each index
-        for(int index = 0; index < n; ++index) {   
-            temp[index] = index;
-            for(int prevIndex = 0; prevIndex < index; ++prevIndex) {
-                if(nums[prevIndex] < nums[index] && 1 + dp[prevIndex] > dp[index]) {
-                    dp[index]   = max(dp[index], 1 + dp[prevIndex]);
-                    temp[index] = prevIndex;
+        for(int i = 0; i < n; ++i) {   
+            sequence[i] = i;
+
+            for(int prev = 0; prev < i; ++prev) {
+                if(nums[prev] < nums[i] && LIS[prev] + 1 > LIS[i]) {
+                    LIS[i]      = max(LIS[i], LIS[prev] + 1);
+                    sequence[i] = prev;
                 }
             }
         }
 
-        int resultIndex = -1; // Stores the index of the value which has to be taken (result value)
-        int maxLength   = -1; // Stores the length of the LIS
+        // Find LIS length and index where its stored
+        int lenOfLIS = -1; 
+        int idx = -1; 
 
-        // Find the length of the LIS 
-        for(int index = 0; index < n; ++index) {
-            if(dp[index] > maxLength) {
-                maxLength   = dp[index];
-                resultIndex = index;
+        for(int i = 0; i < n; ++i) {
+            if(LIS[i] > lenOfLIS) {
+                lenOfLIS = LIS[i];
+                idx = i;
             }
         }
 
-        // Stores the result values
-        vector<int> LIS;
+        vector<int> answer;
 
-        // Iterate and store the result values
-        while(resultIndex != temp[resultIndex]) {
-            LIS.push_back(nums[resultIndex]);
-            resultIndex = temp[resultIndex];
+        while(idx != sequence[idx]) {
+            answer.push_back(nums[idx]);
+            idx = sequence[idx];
         }
-        LIS.push_back(nums[resultIndex]);
-
-        // Reverse the result array to get the actual order
-        reverse(begin(LIS), end(LIS));
-        
-        // Return the result array
-        return LIS;
+        answer.push_back(nums[idx]);
+        reverse(begin(answer), end(answer));
+        return answer;
     }
 };
 
