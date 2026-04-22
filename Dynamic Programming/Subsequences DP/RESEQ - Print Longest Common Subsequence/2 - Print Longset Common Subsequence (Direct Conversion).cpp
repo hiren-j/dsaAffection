7895b287f29faace0af3626f0c7633c5bc0210a3
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-22 11:38:55 +0530

    Create 2 - Print Longset Common Subsequence (Direct Conversion).cpp

---
+++ Dynamic Programming/Subsequences DP/RESEQ - Print Longest Common Subsequence/2 - Print Longset Common Subsequence (Direct Conversion).cpp
@@ -0,0 +1,56 @@
+// Code to find the longest common subsequence of s1 and s2 ~ coded by vHiren
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class BottomUp {
+public:
+    // O(N*M) & O(N*M)
+    string findLCS(int n, int m, string& s1, string& s2){
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Find length of LCS
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = m - 1; j >= 0; --j) {
+                int currSkip1 = dp[i + 1][j];
+                int currSkip2 = dp[i][j + 1];
+                int currTake  = s1[i] == s2[j] 
+                                ? 1 + dp[i + 1][j + 1]
+                                : 0;
+                dp[i][j] = max({currTake, currSkip1, currSkip2});
+            }
+        }
+
+        int i = 0, j = 0;
+        string LCS;
+
+        while(i <= n - 1 && j <= m - 1) {
+            int currSkip1 = dp[i + 1][j];
+            int currSkip2 = dp[i][j + 1];
+            int currTake  = s1[i] == s2[j] 
+                            ? 1 + dp[i + 1][j + 1]
+                            : 0;
+            if(currSkip1 > max(currSkip2, currTake)) {
+                i++;
+            }
+            else if(currSkip2 > max(currSkip1, currTake)) {
+                j++;
+            }
+            else {
+                if(s1[i] == s2[j]) {
+                    LCS.push_back(s1[i]);
+                    j++;
+                }
+                i++;
+            }
+        }
+
+        return LCS;
+    }
+};
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Topics: String | Dynamic Programming
+Links : https://www.naukri.com/code360/problems/print-longest-common-subsequence_8416383?leftPanelTabValue=PROBLEM
+        https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/String%20DP/Longest%20Common%20Subsequence.cpp
+        https://cses.fi/problemset/task/3403
