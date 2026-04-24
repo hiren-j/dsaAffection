5d5356bce4f86b8699f7086ec5cb153d14275123
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-04-24 13:50:42 +0530

    Update 2 - Print LIS (1D Space).cpp

--- Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence/2 - Print LIS (1D Space).cpp
+++ Dynamic Programming/LIS DP/4 - Print Longest Increasing Subsequence/2 - Print LIS (1D Space).cpp
@@ -3,40 +3,39 @@
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-public:
-    // O(N*N) & O(N) 
+    // O(N*N) & O(N)
     vector<int> getLIS(vector<int>& nums) {
         const int n = nums.size();
 
-        vector<int> LIS(n, 1); 
-        vector<int> prevIndex(n, -1);
+        vector<int> LIS(n, 1);
+        vector<int> jumpIndex(n);
 
-        for(int i = 0; i < n; ++i) {   
-            prevIndex[i] = i;
+        int maxElement = -1;
+        int curr = -1;
 
+        for(int i = 0; i < n; ++i) {
+            jumpIndex[i] = i;
+            
             for(int prev = 0; prev < i; ++prev) {
-                if(nums[prev] < nums[i] && LIS[prev] + 1 > LIS[i]) {
-                    LIS[i]       = max(LIS[i], LIS[prev] + 1);
-                    prevIndex[i] = prev;
+                if(nums[i] > nums[prev]) {
+                    if(LIS[prev] + 1 > LIS[i]) {
+                        LIS[i] = LIS[prev] + 1;
+                        jumpIndex[i] = prev;
+                    }
                 }
             }
-        }
-
-        int arrayLIS = -1; 
-        int curr = -1; 
-        
-        for(int i = 0; i < n; ++i) {
-            if(LIS[i] > arrayLIS) {
-                arrayLIS = LIS[i];
+            
+            if(LIS[i] > maxElement) { // Find index of maxElement
+                maxElement = LIS[i];
                 curr = i;
             }
         }
-
+        
         vector<int> answer;
         
-        while(curr != prevIndex[curr]) {
+        while(curr != jumpIndex[curr]) {
             answer.push_back(nums[curr]);
-            curr = prevIndex[curr];
+            curr = jumpIndex[curr];
         }
         answer.push_back(nums[curr]);
         reverse(begin(answer), end(answer));
