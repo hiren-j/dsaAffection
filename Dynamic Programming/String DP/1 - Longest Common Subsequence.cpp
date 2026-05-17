d603b40bfbd098bf0d39f7aab76663d57ed4d380
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-05-17 17:03:26 +0530

    Update 1 - Longest Common Subsequence.cpp

--- Dynamic Programming/String DP/1 - Longest Common Subsequence.cpp
+++ Dynamic Programming/String DP/1 - Longest Common Subsequence.cpp
@@ -40,6 +40,72 @@ public:
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+class BottomUp {
+    int n, m;
+
+    // O(N*M) & O(N*M)
+    int solveBy2DTable(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        for(int j = 0; j <= m; ++j)
+            dp[n][j] = 0;
+        for(int i = 0; i <= n; ++i)
+            dp[i][m] = 0;
+
+        for(int i = n-1; i >= 0; --i) {
+            for(int j = m-1; j >= 0; --j) {
+                if(s1[i] == s2[j])
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);   
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    // O(N*M) & O(N*M)
+    int solveBy2DEnhanced(const string& s1, const string& s2) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = n-1; i >= 0; --i) {
+            for(int j = m-1; j >= 0; --j) {
+                if(s1[i] == s2[j])
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);   
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    // O(N*M) & O(M)
+    int solveBy1DTable(const string& s1, const string& s2) {
+        vector<int> nextRow(m + 1, 0), currRow(m + 1, 0);
+
+        for(int i = n-1; i >= 0; --i) {
+            for(int j = m-1; j >= 0; --j) {
+                if(s1[i] == s2[j])
+                    currRow[j] = 1 + nextRow[j+1];
+                else
+                    currRow[j] = max(nextRow[j], currRow[j+1]);   
+            }
+            swap(nextRow, currRow);
+        }
+
+        return nextRow[0];
+    }
+
+public:
+    int longestCommonSubsequence(string& s1, string& s2) {
+        n = s1.size(), m = s2.size();
+        return solveBy1DTable(s1, s2);
+    }
+};
+    
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Topics: String | Dynamic Programming
 Links : https://leetcode.com/problems/longest-palindromic-subsequence/description/
         https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/String%20DP/1%20-%20Longest%20Common%20Subsequence.cpp
