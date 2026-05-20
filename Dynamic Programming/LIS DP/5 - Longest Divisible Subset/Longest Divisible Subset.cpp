abc023fe7297d1360bec74db4f0ef444bd6ba763
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-05-20 22:19:04 +0530

    Update Longest Divisible Subset.cpp

--- Dynamic Programming/LIS DP/5 - Longest Divisible Subset/Longest Divisible Subset.cpp
+++ Dynamic Programming/LIS DP/5 - Longest Divisible Subset/Longest Divisible Subset.cpp
@@ -1,85 +1,100 @@
-// provide our new way to print it
-
 // Code to find the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies: answer[i] % answer[j] == 0, or answer[j] % answer[i] == 0 ~ coded by Hiren
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class BottomUp {
-    // O(N^2) & O(N^2)
+    int n;
+
+    // O(N*N) & O(N*N)
     vector<int> solveBy2DTable(vector<int>& nums) {
+        const int n = nums.size();
         sort(begin(nums), end(nums));
-        int n = nums.size();
-        int maxLenLDS = 0;
-        
-        vector<int> LDS(n, 1);            // LDS[i] represents the length of the LDS ending at that ith index 
-        vector<vector<int>> divSubset(n); // divSubset[i] stores the values of the longest divisible subset ending at that ith index
 
-        for(int j = 0; j < n; ++j) { // Find the length of longest divisible subset and the values of it
-            divSubset[j].push_back(nums[j]);
-            for(int i = 0; i < j; ++i) {
-                if(nums[j] % nums[i] == 0) {
-                    if(LDS[j] < LDS[i] + 1) {
-                        LDS[j] = LDS[i] + 1;
-                        divSubset[j] = divSubset[i]; 
-                        divSubset[j].push_back(nums[j]);
-                    }
-                }
-            }
-            maxLenLDS = max(maxLenLDS, LDS[j]);
-        }
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-        for(int j = 0; j < n; ++j) // Return the values of the longest divisible subset 
-            if(LDS[j] == maxLenLDS) 
-                return divSubset[j];
-                        
-        return {};
-    }
-    
-    // O(N^2) & O(N)
-    vector<int> solveBy1DTable(vector<int>& nums) {
-        int n = nums.size();
-        sort(begin(nums), end(nums));
-        
-        vector<int> LDS(n, 1);   // LDS[i] represents the length of the LDS ending at that ith index 
-        vector<int> temp(n, -1); // temp[i] represents the index of the recently seen value which is considered as the part of the LDS ending at that ith index
-        
-        // Find the length of the (LDS) longest divisible subset ending at index i
-        for(int i = 0; i < n; ++i) {
-            temp[i] = i;
-            for(int j = 0; j < i; ++j) {
-                if(nums[i] % nums[j] == 0) {
-                    if(1 + LDS[j] > LDS[i]) {
-                        LDS[i]  = 1 + LDS[j];
-                        temp[i] = j;
-                    }
+        for(int i = n - 1; i >= 0; --i) {
+            for(int j = 0; j <= n; ++j) {
+                int currSkip = dp[i + 1][j];
+                int currTake = 0;
+
+                if(j == n || (nums[i] % nums[j] == 0))
+                    currTake = dp[i + 1][i] + 1;
+                
+                if(currSkip > currTake) {
+                    dp[i][j] = currSkip;
+                }   
+                else {
+                    dp[i][j] = currTake;
                 }
             }
         }
         
-        int maxLenLDS = -1;
-        int maxLenIdx = -1;
+        int i = 0, j = n;
+        vector<int> answer;
         
-        for(int i = 0; i < n; ++i) { // Find the length of the longest divisible subset
-            if(LDS[i] > maxLenLDS) {
-                maxLenLDS = LDS[i];
-                maxLenIdx = i;
-            }
-        }
+        while(i <= n - 1 && j >= 0) {
+            int currSkip = dp[i + 1][j];
+            int currTake = 0;
             
-        vector<int> LDS;
-        while(maxLenIdx != temp[maxLenIdx]) { // Store the result values
-            LDS.push_back(nums[maxLenIdx]);
-            maxLenIdx = temp[maxLenIdx];
+            if(j == n || (nums[i] % nums[j] == 0))
+                currTake = dp[i + 1][i] + 1;
+            
+            if(currSkip > currTake) {
+                i = i + 1;
+                j = j;
+            }   
+            else {
+                if(j == n || (nums[i] % nums[j] == 0)) {
+                    answer.push_back(nums[i]);
+                    j = i;
+                }
+                i = i + 1;
+            }
         }
-        LDS.push_back(nums[maxLenIdx]);
         
-        reverse(begin(LDS), end(LDS)); // Reverse the result array to get the actual order
-        return LDS;
+        return answer;
     }
-    
+
+    // O(N*N) & O(N)
+    vector<int> solveBy1DTable(const vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        vector<int> LIS(n, 1);
+        vector<int> jumpIndex(n);
+
+        int maxElement = -1;
+        int curr = -1;
+
+        for(int i = 0; i < n; ++i) {
+            jumpIndex[i] = i;
+
+            for(int j = 0; j < i; ++j) {
+                if(nums[i] % nums[j] == 0 && LIS[j] + 1 > LIS[i]) {
+                    LIS[i] = LIS[j] + 1;
+                    jumpIndex[i] = j;
+                }
+            }
+            
+            if(LIS[i] > maxElement) { // Find index of maxElement
+                maxElement = LIS[i];
+                curr = i;
+            }
+        }
+        
+        vector<int> answer;
+        
+        while(curr != jumpIndex[curr]) {
+            answer.push_back(nums[curr]);
+            curr = jumpIndex[curr];
+        }
+        answer.push_back(nums[curr]);
+        reverse(begin(answer), end(answer));
+        return answer;
+    }
+
 public:
-    // Method to find the largest divisible subset, using tabulation :-
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        n = nums.size();
         return solveBy1DTable(nums);
     }
 };
