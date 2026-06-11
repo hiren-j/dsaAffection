60fd2bf2edfec1d3eb812004e29dda514f94d393
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-06-11 22:53:05 +0530

    Update Print Longest Palindromic Substring.cpp

--- Dynamic Programming/String DP/10 - Print Longest Palindromic Substring/Print Longest Palindromic Substring.cpp
+++ Dynamic Programming/String DP/10 - Print Longest Palindromic Substring/Print Longest Palindromic Substring.cpp
@@ -36,5 +36,35 @@ public:
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+class BottomUp {
+public:
+    // O(N*N) & O(N)
+    string longestPalindrome(string& s) {
+        const int n = s.size();
+        int start = -1, end = -1;
+        int maxLen = 0;
+
+        vector<int> nextRow(n, -1), currRow(n, -1);
+
+        for(int i = n-1; i >= 0; --i) {
+            for(int j = n-1; j >= i; --j) {
+                if(s[i] == s[j])
+                    currRow[j] = (i + 1 >= j || i >= j - 1) ? true : nextRow[j - 1];
+                else
+                    currRow[j] = false;  
+
+                if(currRow[j] && j - i + 1 > maxLen)
+                    maxLen = j - i + 1, start = i, end = j;
+            }
+            swap(nextRow, currRow);
+        }
+
+        return s.substr(start, end - start + 1);
+    }
+};
+    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Topics: Two Pointers | String | Dynamic Programming 
 Link  : https://leetcode.com/problems/longest-palindromic-substring/description/
+        https://github.com/hiren-j/dsaAffection/blob/C%2B%2B/Dynamic%20Programming/String%20DP/11%20-%20Count%20Palindromic%20Substrings/Count%20Palindromic%20Substrings.cpp
