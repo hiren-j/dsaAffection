c768025a88c32e565402ff9d0688f93b743d7821
Hiren Joshi (vHiren) <155013795+hiren-j@users.noreply.github.com>
2026-07-05 11:40:55 +0530

    Update and rename Dynamic Programming/String DP/RESEQ - Print LCS.cpp to Dynamic Programming/String DP/RESEQ - Print Longest Common Subsequence/Print LCS.cpp

--- Dynamic Programming/String DP/RESEQ - Print Longest Common Subsequence/Print LCS.cpp
+++ Dynamic Programming/String DP/RESEQ - Print Longest Common Subsequence/Print LCS.cpp
@@ -3,38 +3,36 @@
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 // O(N*M) & O(N*M)
-string findLCS(int n, int m, string& s1, string& s2) {
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+string printLCS(int n, int m, string &s1, string &s2){
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-    for(int i = n-1; i >= 0; --i) {
-        for(int j = m-1; j >= 0; --j) {
+    for(int i = n - 1; i >= 0; --i) {
+        for(int j = m - 1; j >= 0; --j) {
             if(s1[i] == s2[j])
-                dp[i][j] = 1 + dp[i+1][j+1];
+                dp[i][j] = 1 + dp[i + 1][j + 1];
             else
-                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);   
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);   
         }
     }
 
-	int i = 0, j = 0;
-	string answer;
+    int i = 0, j = 0;
+    string answer;
 
-	while(i <= n-1 && j <= m-1) {
+    while(i <= n - 1 && j <= m - 1) {
         if(s1[i] == s2[j]) {
             answer.push_back(s1[i]);
             i++;
             j++;
         }
-        else {
-            if(dp[i+1][j] > dp[i][j+1]) {
-                i++;
-            }
-            else {
-                j++;
-            }  
+        else if(dp[i][j + 1] > dp[i + 1][j]) {
+            j++;
         }
-	}
+        else {
+            i++;
+        }
+    }
 
-	return answer;
+    return answer;	
 }
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------
